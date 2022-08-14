aa6496af2f881ed97cb3a97cc3d1954c813ec05f
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-08-14 03:12:55 +0200

    Export types.

---
+++ packages/kartoffelgames.core.serializer/source/index.ts
@@ -0,0 +1,4 @@
+export { StatefullSerializeable } from './statefull_serialize/decorator/statefull-serializeable.decorator';
+export { StatefullDeserializer } from './statefull_serialize/statefull-deserializer';
+export { StatefullSerializeableClasses } from './statefull_serialize/statefull-serializeable-classes';
+export { StatefullSerializer } from './statefull_serialize/statefull-serializer';

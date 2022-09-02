496474c7f75f36a346321bad8222f49bdc573110
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-09-03 01:56:33 +0200

    Move eslint config to root. Remove old config files.

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "root": true,
     "parserOptions": {
-        "project": "../../tsconfig.json",
+        "project": "tsconfig.json",
         "tsconfigRootDir": __dirname
     },
     "parser": "@typescript-eslint/parser",

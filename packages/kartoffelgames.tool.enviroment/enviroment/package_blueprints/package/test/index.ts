27c31a233592abe94823fc5ad0687492d2b06a64
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-09-04 02:07:36 +0200

    Move enviroment to different repo.

--- packages/kartoffelgames.tool.enviroment/enviroment/package_blueprints/package/test/index.ts
+++
@@ -1,9 +0,0 @@
-module.exports = (()=>{
-    const lTestContext = (<any>require).context('.', true, /.test.ts$/);
-    lTestContext.keys().forEach(lTestContext);
-
-    const lComponentContext = (<any>require).context('../source', true, /.ts$/);
-    lComponentContext.keys().forEach(lComponentContext);
-
-    return lTestContext;
-})();

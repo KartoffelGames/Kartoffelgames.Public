2c2b9a44e896cbe42f206d9f423d2fe5738aae80
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2022-11-26 03:21:51 +0100

    Gamepad mapping in global scope.

--- packages/kartoffelgames.web.game_input/source/gampad/gamepad-connection.ts
+++ packages/kartoffelgames.web.game_input/source/gampad/gamepad-connection.ts
@@ -1,6 +1,6 @@
 import { Dictionary } from '@kartoffelgames/core.data';
-import { GamepadButtonType } from './enum/gamepad-button-type.enum';
-import { GamepadMapping } from './gamepad-mapping';
+import { InputConfiguration } from '../configuration/input-configuration';
+import { GamepadButtonMapping } from '../configuration/gamepad/gamepad-button-mapping';
 
 // TODO: Listener
 // TODO: Pressed state. Apply tolerance.
@@ -9,54 +9,15 @@ import { GamepadMapping } from './gamepad-mapping';
 
 export class GamepadConnection {
     private static mIntervalStarted: boolean = false;
-    private static readonly mMappingList: Array<MappingAssignment> = new Array<MappingAssignment>();
-    private static readonly mStandardMapping: GamepadMapping = ((): GamepadMapping => {
-        const lMapping = {
-            clusterButtonBottom: { type: GamepadButtonType.Button, index: 0 },
-            clusterButtonRight: { type: GamepadButtonType.Button, index: 1 },
-            clusterButtonLeft: { type: GamepadButtonType.Button, index: 2 },
-            clusterButtonTop: { type: GamepadButtonType.Button, index: 3 },
-            buttonLeft: { type: GamepadButtonType.Button, index: 4 },
-            buttonRight: { type: GamepadButtonType.Button, index: 5 },
-            triggerLeft: { type: GamepadButtonType.Button, index: 6 },
-            triggerRight: { type: GamepadButtonType.Button, index: 7 },
-            selectButton: { type: GamepadButtonType.Button, index: 8 },
-            startButton: { type: GamepadButtonType.Button, index: 9 },
-            homeButton: { type: GamepadButtonType.Button, index: 16 },
-            directionalPadTop: { type: GamepadButtonType.Button, index: 12 },
-            directionalPadBottom: { type: GamepadButtonType.Button, index: 13 },
-            directionalPadRight: { type: GamepadButtonType.Button, index: 15 },
-            directionalPadLeft: { type: GamepadButtonType.Button, index: 14 },
-            leftThumbStickButton: { type: GamepadButtonType.Button, index: 10 },
-            leftThumbStickXaxis: { type: GamepadButtonType.Axis, index: 0 },
-            leftThumbStickYaxis: { type: GamepadButtonType.Axis, index: 1 },
-            rightThumbStickButton: { type: GamepadButtonType.Button, index: 11 },
-            rightThumbStickXaxis: { type: GamepadButtonType.Axis, index: 2 },
-            rightThumbStickYaxis: { type: GamepadButtonType.Axis, index: 3 },
-        };
-
-        return new GamepadMapping(lMapping);
-    })();
-
-    /**
-     * Add gamepad mapping by id matching.
-     * @param pIdAssignment - Regex for assigning to matching gamepad ids.
-     * @param pMapping - Gamepad mapping.
-     */
-    public static addMapping(pIdAssignment: RegExp, pMapping: GamepadMapping): void {
-        this.mMappingList.push({ mapping: pMapping, idMatch: pIdAssignment });
-    }
 
     private readonly mGamepads: Dictionary<number, GamepadInformation>;
-    private readonly mGlobalConfig: GamepadConfiguration;
 
     /**
      * Constructor.
      * Initialize connecting and disconnecting gamepads.
      */
-    public constructor(pConfig: GamepadConfiguration) {
+    public constructor() {
         this.mGamepads = new Dictionary<number, GamepadInformation>();
-        this.mGlobalConfig = pConfig;
 
         // Init connected gamepads.
         globalThis.addEventListener('gamepadconnected', (pEvent: GamepadEvent) => {
@@ -118,23 +79,19 @@ export class GamepadConnection {
     private initGamepad(pGamepad: Gamepad): void {
         // TODO: Enable gamepad when allready created.
 
-        let lFoundMapping: GamepadMapping | null = null;
+        let lFoundMapping: GamepadButtonMapping | null = null;
 
         // Try to find mappig by id assignment.
-        const lGamepadId: string = pGamepad.id;
-        for (const lMappingAssignment of GamepadConnection.mMappingList) {
-            if (lMappingAssignment.idMatch.test(lGamepadId)) {
-                lFoundMapping = lMappingAssignment.mapping;
-                break;
-            }
-        }
+        lFoundMapping = InputConfiguration.gamepad.getMapping(pGamepad.id);
 
         // Fallback to gamepad mapping property.
-        if (pGamepad.mapping === 'standard') {
-            lFoundMapping = GamepadConnection.mStandardMapping;
-        } else {
-            // There are more mapping types, but ignored for now. :) hehe
-            lFoundMapping = GamepadConnection.mStandardMapping;
+        if (!lFoundMapping) {
+            if (pGamepad.mapping === 'standard') {
+                lFoundMapping = InputConfiguration.gamepad.standardMapping;
+            } else {
+                // There are more mapping types, but ignored for now. :) hehe
+                lFoundMapping = InputConfiguration.gamepad.standardMapping;
+            }
         }
 
         // Build general gamepad information.
@@ -150,17 +107,9 @@ export class GamepadConnection {
     }
 }
 
-type GamepadConfiguration = {
-    triggerTolerance: number;
-};
 
 type GamepadInformation = {
     index: number;
     id: string;
-    mapping: GamepadMapping;
-};
-
-type MappingAssignment = {
-    mapping: GamepadMapping,
-    idMatch: RegExp;
+    mapping: GamepadButtonMapping;
 };

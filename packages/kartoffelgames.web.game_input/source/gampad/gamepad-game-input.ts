a16951aa74c39c4f0a9aeb5e14f259702f5bf104
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2022-11-30 00:04:17 +0100

    Update gamepad game input button values.

--- packages/kartoffelgames.web.game_input/source/gampad/gamepad-game-input.ts
+++ packages/kartoffelgames.web.game_input/source/gampad/gamepad-game-input.ts
@@ -1,9 +1,12 @@
+import { EnumUtil } from '@kartoffelgames/core.data';
 import { GamepadButtonMapping } from '../configuration/gamepad/gamepad-button-mapping';
+import { InputConfiguration } from '../configuration/input-configuration';
+import { GamepadButton } from '../enum/gamepad-button.enum';
 import { InputDevice } from '../enum/input-device.enum';
 import { BaseGameInput } from '../game_input/base-game-input';
 
 export class GamepadGameInput extends BaseGameInput {
-    private readonly mGamepadId: string;
+    private readonly mGamepadInformation: GamepadGameInputInformation;
 
     /**
      * Constructor.
@@ -12,20 +15,41 @@ export class GamepadGameInput extends BaseGameInput {
     public constructor(pGamepad: GamepadGameInputInformation) {
         super(`gamepad_${pGamepad.id}`, InputDevice.Gamepad);
 
-        this.mGamepadId = pGamepad.id;
+        this.mGamepadInformation = pGamepad;
         this.startScanLoop();
     }
 
     private startScanLoop(): void {
+        // Get all gamepad buttons.
+        const lGamepadButtonList: Array<GamepadButton> = EnumUtil.enumValuesToArray<GamepadButton>(GamepadButton);
+
         const lLoop = () => {
             // Only scan on connected gamepads.
             if (this.connected) {
                 // Find connected gamepad.
                 const lGamepad = globalThis.navigator.getGamepads().find((pGamepad) => {
-                    return pGamepad?.id === this.mGamepadId;
+                    return pGamepad?.id === this.mGamepadInformation.id;
                 });
 
-                // TODO: Pressed state. Apply tolerance.
+                // On found gamepad.
+                if (lGamepad) {
+                    // Scan each gamepad button.
+                    for (const lButton of lGamepadButtonList) {
+                        // Read button value.
+                        let lButtonValue = this.mGamepadInformation.mapping.executeMapping(lButton, lGamepad);
+
+                        // Apply tolerance.
+                        if (lButtonValue < InputConfiguration.gamepad.triggerTolerance) {
+                            lButtonValue = 0;
+                        }
+
+                        // Set button value.
+                        this.setButtonState(lButton, lButtonValue);
+                    }
+                } else {
+                    // Disconnect gamepad when not found.
+                    this.connected = false;
+                }
             }
 
             window.requestAnimationFrame(lLoop);

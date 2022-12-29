43e9119f39182e07ad87c0992c59ae1691093cc6
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2022-12-29 05:01:37 +0100

    Setup keyboard and mouse capture events.

--- packages/kartoffelgames.web.game_input/source/keyboard_mouse/mouse-keyboard-game-input.ts
+++ packages/kartoffelgames.web.game_input/source/keyboard_mouse/mouse-keyboard-game-input.ts
@@ -1,4 +1,5 @@
 import { InputDevice } from '../enum/input-device.enum';
+import { KeyboardButton } from '../enum/keyboard-button.enum';
 import { MouseButton } from '../enum/mouse-button.enum';
 import { BaseGameInput } from '../game_input/base-game-input';
 
@@ -44,6 +45,9 @@ export class MouseKeyboardGameInput extends BaseGameInput {
         }
     }
 
+    /**
+     * Setup event listener for keyboard and mouse events.
+     */
     private setupCaptureListener(): void {
         // Capture mouse movement. Calculate to axis value by set base value to 10 pixels.
         document.addEventListener('mousemove', (pMouseEvent) => {
@@ -56,9 +60,17 @@ export class MouseKeyboardGameInput extends BaseGameInput {
             this.setMouseButtonValue(pMouseEvent.button, 0);
         });
         document.addEventListener('mousedown', (pMouseEvent) => {
-            this.setMouseButtonValue(pMouseEvent.button, 0);
+            this.setMouseButtonValue(pMouseEvent.button, 1);
         });
 
-        // TODO: Keboard event. UP and DOWN. Map with enum values.
+        // Keyboard event.
+        document.addEventListener('keydown', (pKeyboardEvent) => {
+            const lInputKey: KeyboardButton = <KeyboardButton>pKeyboardEvent.code;
+            this.setButtonState(lInputKey, 1);
+        });
+        document.addEventListener('keyup', (pKeyboardEvent) => {
+            const lInputKey: KeyboardButton = <KeyboardButton>pKeyboardEvent.code;
+            this.setButtonState(lInputKey, 0);
+        });
     }
 }

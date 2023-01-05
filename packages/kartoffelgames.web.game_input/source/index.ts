e05087132871439753ed2c8b5a385560e35a92e5
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2023-01-05 03:03:13 +0100

    Export types.

--- packages/kartoffelgames.web.game_input/source/index.ts
+++ packages/kartoffelgames.web.game_input/source/index.ts
@@ -1,4 +1,13 @@
+export { DeviceConfiguration } from './configuration/device-configuration';
 export { MouseKeyboardConnector } from './connector/mouse-keyboard-connector';
 export { GamepadConnector } from './connector/gamepad-connector';
 export { InputDevices } from './input-devices';
 export { InputConfiguration } from './configuration/input-configuration';
+export { BaseInputDevice } from './device/base-input-device';
+
+export { InputActionEvent } from './events/input-action-event';
+export { InputButtonEvent } from './events/input-button-event';
+
+export { KeyboardButton } from './enum/keyboard-button.enum';
+export { MouseButton } from './enum/mouse-button.enum';
+export { GamepadButton } from './enum/gamepad-button.enum';

d14948d387c0620e894717876b6da53e4c07b0f0
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2023-02-13 23:51:53 +0100

    Normed gpu object.

---
+++ packages/kartoffelgames.web.gpu/source/core/gpu-native-object.ts
@@ -0,0 +1,49 @@
+import { Gpu } from './gpu';
+
+/**
+ * Gpu native object.
+ */
+export abstract class GpuNativeObject<T> {
+    private readonly mGpu: Gpu;
+    private mNativeObject: T | null;
+
+    /**
+     * Get global gpu.
+     */
+    protected get gpu(): Gpu {
+        return this.mGpu;
+    }
+
+    /**
+     * Constructor.
+     * @param pGpu - Gpu object.
+     */
+    public constructor(pGpu: Gpu) {
+        this.mGpu = pGpu;
+        this.mNativeObject = null;
+    }
+
+    /**
+     * Get native object.
+     */
+    public async native(): Promise<T> {
+        // Generate new native object when not already created.
+        if (!this.mNativeObject) {
+            this.mNativeObject = await this.generate();
+        }
+
+        return this.mNativeObject;
+    }
+
+    /**
+     * Reset generated native object.
+     */
+    protected reset(): void {
+        this.mNativeObject = null;
+    }
+
+    /**
+     * Generate native object.
+     */
+    protected abstract generate(): Promise<T>;
+}

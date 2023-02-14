40bb44f298ec063f01fd6113835b6c915aceb9f4
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2023-02-14 23:33:03 +0100

    Cached adapter and devices on gpu creation.

--- packages/kartoffelgames.web.gpu/source/core/gpu.ts
+++ packages/kartoffelgames.web.gpu/source/core/gpu.ts
@@ -1,18 +1,27 @@
-import { Exception } from '@kartoffelgames/core.data';
+import { Dictionary, Exception } from '@kartoffelgames/core.data';
 
 export class Gpu {
+    private static readonly mAdapters: Dictionary<string, GPUAdapter> = new Dictionary<string, GPUAdapter>();
+    private static readonly mDevices: Dictionary<GPUAdapter, GPUDevice> = new Dictionary<GPUAdapter, GPUDevice>();
+
     /**
      * Create GPU device.
      * @param pMode - Prefered device mode.
      */
     public static async create(pMode: GPUPowerPreference): Promise<Gpu> {
-        const lAdapter: GPUAdapter | null = await window.navigator.gpu.requestAdapter({ powerPreference: pMode });
-        if (lAdapter === null) {
+        // Try to load cached adapter. When not cached, request new one.
+        const lAdapter: GPUAdapter | null = Gpu.mAdapters.get(pMode) ?? await window.navigator.gpu.requestAdapter({ powerPreference: pMode });
+        if (lAdapter) {
+            Gpu.mAdapters.set(pMode, lAdapter);
+        } else {
             throw new Exception('Error requesting GPU adapter', Gpu);
         }
 
-        const lDevice: GPUDevice | null = await lAdapter.requestDevice();
-        if (lDevice === null) {
+        // Try to load cached device. When not cached, request new one.
+        const lDevice: GPUDevice | null = Gpu.mDevices.get(lAdapter) ?? await lAdapter.requestDevice();
+        if (lAdapter) {
+            Gpu.mDevices.set(lAdapter, lDevice);
+        } else {
             throw new Exception('Error requesting GPU device', Gpu);
         }
 

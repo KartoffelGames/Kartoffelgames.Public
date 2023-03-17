5c26023b652715d928c24f42449fc13a5425727e
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2023-03-17 23:48:57 +0100

    Enum value utils.

--- packages/kartoffelgames.core.data/source/util/enum-util.ts
+++ packages/kartoffelgames.core.data/source/util/enum-util.ts
@@ -3,12 +3,35 @@
  */
 export class EnumUtil {
     /**
+     * Return enum of enum value.
+     * @param pEnum - typeof Enum object.
+     * @param pValue - Value of enum.
+     */
+    public static enumKeyByValue<T>(pEnum: object, pValue: any): T | undefined {
+        // Thats it... :)
+        if (EnumUtil.enumValueExists<T>(pEnum, pValue)) {
+            return pValue;
+        } else {
+            return undefined;
+        }
+    }
+
+    /**
      * Return all keys of an enum as array.
      * @param pEnum - typeof Enum object.
      */
     public static enumNamesToArray(pEnum: object): Array<string> {
         // Convert enum to key array.
         return Object.keys(pEnum).filter((pKey) => isNaN(Number(pKey)));
+    }
+
+    /**
+     * Check if value exists in enum object.
+     * @param pEnum - typeof Enum object.
+     * @param pValue - Value of enum.
+     */
+    public static enumValueExists<T>(pEnum: object, pValue: any): pValue is T {
+        return EnumUtil.enumValuesToArray(pEnum).includes(pValue);
     }
 
     /**
--- packages/kartoffelgames.core.data/test/unit/util/enum-util.test.ts
+++ packages/kartoffelgames.core.data/test/unit/util/enum-util.test.ts
@@ -33,4 +33,72 @@ describe('EnumUtil', () => {
         // Evaluation.
         expect(lValueArray).to.be.deep.equal([1, 2, '3', '5']);
     });
+
+    describe('Static Method: enumValueExists', () => {
+        it('-- Exists', () => {
+            // Setup.
+            enum TestEnum {
+                One = 1,
+                Two = 2,
+                Three = '3',
+                Five = '5',
+            }
+
+            // Process.
+            const lExists: boolean = EnumUtil.enumValueExists(TestEnum, 2);
+
+            // Evaluation.
+            expect(lExists).to.be.true;
+        });
+
+        it('-- Not Exists', () => {
+            // Setup.
+            enum TestEnum {
+                One = 1,
+                Two = 2,
+                Three = '3',
+                Five = '5',
+            }
+
+            // Process.
+            const lExists: boolean = EnumUtil.enumValueExists(TestEnum, 'Not__Exists');
+
+            // Evaluation.
+            expect(lExists).to.be.false;
+        });
+    });
+
+    describe('Static Method: enumKeyByValue', () => {
+        it('-- Exists', () => {
+            // Setup.
+            enum TestEnum {
+                One = 1,
+                Two = 2,
+                Three = '3',
+                Five = '5',
+            }
+
+            // Process.
+            const lEnum: TestEnum | undefined = EnumUtil.enumKeyByValue(TestEnum, 2);
+
+            // Evaluation.
+            expect(lEnum).to.equal(TestEnum.Two);
+        });
+
+        it('-- Not Exists', () => {
+            // Setup.
+            enum TestEnum {
+                One = 1,
+                Two = 2,
+                Three = '3',
+                Five = '5',
+            }
+
+            // Process.
+            const lEnum: TestEnum | undefined = EnumUtil.enumKeyByValue(TestEnum, 'Not__Exists');
+
+            // Evaluation.
+            expect(lEnum).to.be.undefined;
+        });
+    });
 });

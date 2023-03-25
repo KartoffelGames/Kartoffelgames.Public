af1e56395d12017af79ce56221336e7174bb3e76
Kartoffeleintopf <TetrodoToxinPotato@gmail.com>
2023-03-25 05:58:31 +0100

    Grouped wgsl type information.

--- packages/kartoffelgames.web.gpu/source/core/type_handler/type-information.ts
+++ packages/kartoffelgames.web.gpu/source/core/type_handler/type-information.ts
@@ -1,13 +1,26 @@
 import { Dictionary } from '@kartoffelgames/core.data';
+import { WgslEnum } from './wgsl-enum.enum';
 import { WgslType } from './wgsl-type.enum';
 
-export const WgslTypeFloatNumbers: Array<WgslType> = [WgslType.Float16, WgslType.Float32];
-export const WgslTypeIntegerNumbers: Array<WgslType> = [WgslType.Integer32, WgslType.UnsignedInteger32];
-export const WgslTypeNumbers: Array<WgslType> = [...WgslTypeFloatNumbers, ...WgslTypeIntegerNumbers];
-export const WgslTypeAccessModes: Array<WgslType> = [WgslType.AccessModeRead, WgslType.AccessModeWrite, WgslType.AccessModeReadWrite];
-export const WgslTypeAddressSpaces: Array<WgslType> = [WgslType.AddressSpaceFunction, WgslType.AddressSpacePrivate, WgslType.AddressSpaceStorage, WgslType.AddressSpaceUniform, WgslType.AddressSpaceWorkgroup];
-export const WgslTypeVectors: Array<WgslType> = [WgslType.Vector2, WgslType.Vector3, WgslType.Vector4];
-export const WgslTypeMatrix: Array<WgslType> = [WgslType.Matrix22, WgslType.Matrix23, WgslType.Matrix24, WgslType.Matrix32, WgslType.Matrix33, WgslType.Matrix34, WgslType.Matrix42, WgslType.Matrix43, WgslType.Matrix44];
+// Type collection.
+export const WgslTypeFloatNumbers: Array<WgslTypeFloatNumber> = [WgslType.Float16, WgslType.Float32];
+export const WgslTypeIntegerNumbers: Array<WgslTypeIntegerNumber> = [WgslType.Integer32, WgslType.UnsignedInteger32];
+export const WgslTypeNumbers: Array<WgslTypeNumber> = [...WgslTypeFloatNumbers, ...WgslTypeIntegerNumbers];
+export const WgslTypeVectors: Array<WgslTypeVector> = [WgslType.Vector2, WgslType.Vector3, WgslType.Vector4];
+export const WgslTypeMatrices: Array<WgslTypeMatrix> = [WgslType.Matrix22, WgslType.Matrix23, WgslType.Matrix24, WgslType.Matrix32, WgslType.Matrix33, WgslType.Matrix34, WgslType.Matrix42, WgslType.Matrix43, WgslType.Matrix44];
+export const WgslTypeStorageTextures: Array<WgslTypeStorageTexture> = [WgslType.TextureStorage1d, WgslType.TextureStorage2d, WgslType.TextureStorage2dArray, WgslType.TextureStorage3d];
+export const WgslTypeTextures: Array<WgslTypeTexture> = [WgslType.Texture1d, WgslType.Texture2d, WgslType.Texture2dArray, WgslType.Texture3d, WgslType.TextureCube, WgslType.TextureCubeArray, WgslType.TextureMultisampled2d];
+export const WgslTypeDepthTextures: Array<WgslTypeDepthTexture> = [WgslType.TextureDepth2d, WgslType.TextureDepth2dArray, WgslType.TextureDepthCube, WgslType.TextureDepthCube, WgslType.TextureDepthMultisampled2d];
+
+// Enum collections.
+export const WgslTypeAccessModes: Array<WgslTypeAccessMode> = [WgslEnum.AccessModeRead, WgslEnum.AccessModeWrite, WgslEnum.AccessModeReadWrite];
+export const WgslTypeAddressSpaces: Array<WgslTypeAddressSpace> = [WgslEnum.AddressSpaceFunction, WgslEnum.AddressSpacePrivate, WgslEnum.AddressSpaceStorage, WgslEnum.AddressSpaceUniform, WgslEnum.AddressSpaceWorkgroup];
+export const WgslTypeTexelFormats: Array<WgslTypeTexelFormat> = [
+    WgslEnum.TexelFormatRgba8unorm, WgslEnum.TexelFormatRgba8snorm, WgslEnum.TexelFormatRgba8uint, WgslEnum.TexelFormatRgba8sint,
+    WgslEnum.TexelFormatRgba16uint, WgslEnum.TexelFormatRgba16sint, WgslEnum.TexelFormatRgba16float, WgslEnum.TexelFormatR32uint,
+    WgslEnum.TexelFormatR32sint, WgslEnum.TexelFormatR32float, WgslEnum.TexelFormatRg32uint, WgslEnum.TexelFormatRg32sint,
+    WgslEnum.TexelFormatRg32float, WgslEnum.TexelFormatRgba32uint, WgslEnum.TexelFormatRgba32sint, WgslEnum.TexelFormatRgba32float, WgslEnum.TexelFormatBgra8unorm
+];
 
 export const WgslTypeRestrictions: Dictionary<WgslType, WgslTypeInformation> = (() => {
     const lTypeList: Dictionary<WgslType, WgslTypeInformation> = new Dictionary<WgslType, WgslTypeInformation>();
@@ -17,12 +30,7 @@ export const WgslTypeRestrictions: Dictionary<WgslType, WgslTypeInformation> = (
 
     const lNumeric32TypeList: Array<WgslType> = [WgslType.Integer32, WgslType.UnsignedInteger32, WgslType.Float32];
     const lAnyType: Array<WgslType> = [WgslType.Any];
-    const lTexelFormatTypeList: Array<WgslType> = [
-        WgslType.TexelFormatRgba8unorm, WgslType.TexelFormatRgba8snorm, WgslType.TexelFormatRgba8uint, WgslType.TexelFormatRgba8sint,
-        WgslType.TexelFormatRgba16uint, WgslType.TexelFormatRgba16sint, WgslType.TexelFormatRgba16float, WgslType.TexelFormatR32uint,
-        WgslType.TexelFormatR32sint, WgslType.TexelFormatR32float, WgslType.TexelFormatRg32uint, WgslType.TexelFormatRg32sint,
-        WgslType.TexelFormatRg32float, WgslType.TexelFormatRgba32uint, WgslType.TexelFormatRgba32sint, WgslType.TexelFormatRgba32float, WgslType.TexelFormatBgra8unorm
-    ];
+
 
     lAddType({ type: WgslType.Any, generic: true, genericTypes: [] });
 
@@ -73,10 +81,10 @@ export const WgslTypeRestrictions: Dictionary<WgslType, WgslTypeInformation> = (
     lAddType({ type: WgslType.TextureDepthMultisampled2d, generic: false, genericTypes: [] });
 
     // Storage textures.
-    lAddType({ type: WgslType.TextureStorage1d, generic: true, genericTypes: [lTexelFormatTypeList, WgslTypeAccessModes] });
-    lAddType({ type: WgslType.TextureStorage2d, generic: true, genericTypes: [lTexelFormatTypeList, WgslTypeAccessModes] });
-    lAddType({ type: WgslType.TextureStorage2dArray, generic: true, genericTypes: [lTexelFormatTypeList, WgslTypeAccessModes] });
-    lAddType({ type: WgslType.TextureStorage3d, generic: true, genericTypes: [lTexelFormatTypeList, WgslTypeAccessModes] });
+    lAddType({ type: WgslType.TextureStorage1d, generic: true, genericTypes: [WgslTypeTexelFormats, WgslTypeAccessModes] });
+    lAddType({ type: WgslType.TextureStorage2d, generic: true, genericTypes: [WgslTypeTexelFormats, WgslTypeAccessModes] });
+    lAddType({ type: WgslType.TextureStorage2dArray, generic: true, genericTypes: [WgslTypeTexelFormats, WgslTypeAccessModes] });
+    lAddType({ type: WgslType.TextureStorage3d, generic: true, genericTypes: [WgslTypeTexelFormats, WgslTypeAccessModes] });
 
     // Sampler.
     lAddType({ type: WgslType.Sampler, generic: false, genericTypes: [] });
@@ -88,5 +96,23 @@ export const WgslTypeRestrictions: Dictionary<WgslType, WgslTypeInformation> = (
 export type WgslTypeInformation = {
     type: WgslType,
     generic: boolean,
-    genericTypes: Array<Array<WgslType>>;
+    genericTypes: Array<Array<WgslType | WgslEnum>>;
 };
+
+// Type type collections.
+export type WgslTypeFloatNumber = WgslType.Float16 | WgslType.Float32;
+export type WgslTypeIntegerNumber = WgslType.Integer32 | WgslType.UnsignedInteger32;
+export type WgslTypeNumber = WgslTypeFloatNumber | WgslTypeIntegerNumber;
+export type WgslTypeVector = WgslType.Vector2 | WgslType.Vector3 | WgslType.Vector4;
+export type WgslTypeMatrix = WgslType.Matrix22 | WgslType.Matrix23 | WgslType.Matrix24 | WgslType.Matrix32 | WgslType.Matrix33 | WgslType.Matrix34 | WgslType.Matrix42 | WgslType.Matrix43 | WgslType.Matrix44;
+export type WgslTypeStorageTexture = WgslType.TextureStorage1d | WgslType.TextureStorage2d | WgslType.TextureStorage2dArray | WgslType.TextureStorage3d;
+export type WgslTypeTexture = WgslType.Texture1d | WgslType.Texture2d | WgslType.Texture2dArray | WgslType.Texture3d | WgslType.TextureCube | WgslType.TextureCubeArray | WgslType.TextureMultisampled2d;
+export type WgslTypeDepthTexture = WgslType.TextureDepth2d | WgslType.TextureDepth2dArray | WgslType.TextureDepthCube | WgslType.TextureDepthCube | WgslType.TextureDepthMultisampled2d;
+
+// Enum type collections.
+export type WgslTypeAccessMode = WgslEnum.AccessModeRead | WgslEnum.AccessModeWrite | WgslEnum.AccessModeReadWrite;
+export type WgslTypeAddressSpace = WgslEnum.AddressSpaceFunction | WgslEnum.AddressSpacePrivate | WgslEnum.AddressSpaceStorage | WgslEnum.AddressSpaceUniform | WgslEnum.AddressSpaceWorkgroup;
+export type WgslTypeTexelFormat = WgslEnum.TexelFormatRgba8unorm | WgslEnum.TexelFormatRgba8snorm | WgslEnum.TexelFormatRgba8uint | WgslEnum.TexelFormatRgba8sint |
+    WgslEnum.TexelFormatRgba16uint | WgslEnum.TexelFormatRgba16sint | WgslEnum.TexelFormatRgba16float | WgslEnum.TexelFormatR32uint |
+    WgslEnum.TexelFormatR32sint | WgslEnum.TexelFormatR32float | WgslEnum.TexelFormatRg32uint | WgslEnum.TexelFormatRg32sint |
+    WgslEnum.TexelFormatRg32float | WgslEnum.TexelFormatRgba32uint | WgslEnum.TexelFormatRgba32sint | WgslEnum.TexelFormatRgba32float | WgslEnum.TexelFormatBgra8unorm;

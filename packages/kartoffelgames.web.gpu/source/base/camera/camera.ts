202f90c95fbad0685adcea8d25c4fd1db284c1d4
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-04-15 21:42:12 +0200

    Cach camera projection view matrix.

--- packages/kartoffelgames.web.gpu/source/base/camera/camera.ts
+++ packages/kartoffelgames.web.gpu/source/base/camera/camera.ts
@@ -4,6 +4,9 @@ import { Quaternion } from '../../math/quaternion';
 import { IProjection } from './projection/i-projection.interface';
 
 export class Camera {
+    private mCacheProjection: Matrix | null;
+    private mCacheView: Matrix | null;
+    private mCacheViewProjection: Matrix | null;
     private readonly mPivot: Matrix;
     private readonly mProjection: IProjection;
     private mRotation: Quaternion;
@@ -16,6 +19,9 @@ export class Camera {
         return this.mPivot.data[0][3];
     } set pivotX(pValue: number) {
         this.mPivot.data[0][3] = pValue;
+        
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
     /**
@@ -25,6 +31,9 @@ export class Camera {
         return this.mPivot.data[1][3];
     } set pivotY(pValue: number) {
         this.mPivot.data[1][3] = pValue;
+        
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
     /**
@@ -34,6 +43,9 @@ export class Camera {
         return this.mPivot.data[2][3];
     } set pivotZ(pValue: number) {
         this.mPivot.data[2][3] = pValue;
+        
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
     /**
@@ -50,6 +62,9 @@ export class Camera {
         return this.mTranslation.data[0][3];
     } set translationX(pValue: number) {
         this.mTranslation.data[0][3] = pValue;
+        
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
     /**
@@ -59,6 +74,9 @@ export class Camera {
         return this.mTranslation.data[1][3];
     } set translationY(pValue: number) {
         this.mTranslation.data[1][3] = pValue;
+        
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
     /**
@@ -68,14 +86,36 @@ export class Camera {
         return this.mTranslation.data[2][3];
     } set translationZ(pValue: number) {
         this.mTranslation.data[2][3] = pValue;
+  
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
+    /**
+     * Get cameras projection view matrix.
+     * Caches calculated matrix.
+     */
     public get viewProjectionMatrix(): Matrix {
-        // TODO: Cache.
-        const lViewMatrix: Matrix = this.mTranslation.mult(this.mRotation.asMatrix()).inverse();
-        const lProjectionMatrix: Matrix = this.mProjection.projectionMatrix;
+        let lCacheChanged: boolean = false;
 
-        return lViewMatrix.mult(lProjectionMatrix);
+        // Validate view matrix,
+        if(this.mCacheView === null) {
+            this.mCacheView = this.mTranslation.mult(this.mRotation.asMatrix()).inverse();
+            lCacheChanged = true;
+        }
+
+        // Check for projection changes.
+        if(this.mCacheProjection !== this.mProjection.projectionMatrix){
+            this.mCacheProjection = this.mProjection.projectionMatrix;
+            lCacheChanged = true;
+        }
+        
+        // Recalculate projection view matrix on cache change.
+        if(lCacheChanged || this.mCacheViewProjection === null){
+            this.mCacheViewProjection = this.mCacheProjection.mult(this.mCacheView);
+        }
+
+        return this.mCacheViewProjection;
     }
 
     /**
@@ -87,6 +127,11 @@ export class Camera {
         this.mRotation = Quaternion.identity();
         this.mTranslation = Matrix.identity(4);
         this.mPivot = Matrix.identity(4);
+
+        // Caches.
+        this.mCacheView = null;
+        this.mCacheProjection = null;
+        this.mCacheViewProjection = null;
     }
 
     /**
@@ -97,6 +142,9 @@ export class Camera {
      */
     public rotate(pRoll: number, pPitch: number, pYaw: number): void {
         this.mRotation = this.mRotation.addEulerRotation(pRoll, pPitch, pYaw);
+
+        // Clear view cache.
+        this.mCacheView = null;
     }
 
     /**
@@ -107,5 +155,8 @@ export class Camera {
      */
     public setRotation(pRoll: number, pPitch: number, pYaw: number): void {
         this.mRotation = Quaternion.fromRotation(pRoll, pPitch, pYaw);
+        
+        // Clear view cache.
+        this.mCacheView = null;
     }
 }

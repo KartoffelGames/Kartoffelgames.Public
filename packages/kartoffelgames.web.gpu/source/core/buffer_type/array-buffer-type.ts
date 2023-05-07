ea0e7410d8d050987d9274a6fd0d6ac27fa10c41
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-05-07 23:37:24 +0200

    Type of buffer representation.

---
+++ packages/kartoffelgames.web.gpu/source/core/buffer_type/array-buffer-type.ts
@@ -0,0 +1,60 @@
+import { WgslAccessMode } from '../shader/enum/wgsl-access-mode.enum';
+import { WgslBindingType } from '../shader/enum/wgsl-binding-type.enum';
+import { WgslType } from '../shader/enum/wgsl-type.enum';
+import { BufferType } from './buffer-type';
+
+export class ArrayBufferType extends BufferType {
+    private readonly mArraySize: number;
+    private readonly mInnerType: BufferType;
+
+    /**
+     * Alignment of type.
+     */
+    public get alignment(): number {
+        return this.mInnerType.alignment;
+    }
+
+    /**
+     * Array size.
+     */
+    public get arraySize(): number {
+        return this.mArraySize;
+    }
+
+    /**
+     * Array type.
+     */
+    public get innerType(): BufferType {
+        return this.mInnerType;
+    }
+
+    /**
+     * Type size in byte.
+     */
+    public get size(): number {
+        if (this.mArraySize === -1) {
+            return -1;
+        }
+
+        return this.mInnerType.size * this.mArraySize;
+    }
+
+    /**
+     * Wgsl type.
+     */
+    public get type(): WgslType {
+        return WgslType.Array;
+    }
+
+    /**
+     * Constructor.
+     * @param pInnerType - Type of array.
+     * @param pSize - Optional array size.
+     */
+    public constructor(pInnerType: BufferType, pSize?: number, pAccessMode?: WgslAccessMode, pBindType?: WgslBindingType) {
+        super(pAccessMode, pBindType);
+        
+        this.mInnerType = pInnerType;
+        this.mArraySize = pSize ?? -1;
+    }
+}

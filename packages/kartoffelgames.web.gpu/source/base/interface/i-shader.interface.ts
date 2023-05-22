55f6e1efe105cdee82be5c26db7ba567b28b77c3
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-05-22 23:46:07 +0200

    Bind group interfaces.

--- packages/kartoffelgames.web.gpu/source/base/interface/i-shader.interface.ts
+++ packages/kartoffelgames.web.gpu/source/base/interface/i-shader.interface.ts
@@ -1,6 +1,7 @@
-import { IBindGroups } from './bind_group/i-bind-groups.interface';
+
+import { IBindGroupLayout } from './bind_group/i-bind-group-layout.interface';
 import { IGpuObject } from './i-gpu-object.interface';
 
 export interface IShader extends IGpuObject {
-    bindGroups: IBindGroups;
+    bindGroups: IBindGroupLayout;
 }

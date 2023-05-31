981f12d90bdc480e96dde15368520a69b5a3ab2f
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-05-31 03:13:28 +0200

    Mini texture format enum.

---
+++ packages/kartoffelgames.web.gpu/source/base/constant/texture-format.enum.ts
@@ -0,0 +1,12 @@
+export enum TextureFormat {
+    Red = 'r8unorm',
+    RedInteger = 'r8uint',
+    RedGreen = 'rg8unorm',
+    RedGreenInteger = 'rg8uint',
+    RedGreenBlueAlpha = 'rgba8unorm',
+    RedGreenBlueAlphaInteger = 'rgba8uint',
+    BlueRedGreenAlpha = 'bgra8unorm',
+    Depth = 'depth24plus',
+    Stencil = 'stencil8',
+    DepthStencil = 'depth24plus-stencil8',
+}

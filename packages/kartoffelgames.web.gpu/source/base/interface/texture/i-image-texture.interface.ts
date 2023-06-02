d1496f054ed01cc7429976703cd3b7a23620a65f
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-06-02 03:33:14 +0200

    Base image texture.

---
+++ packages/kartoffelgames.web.gpu/source/base/interface/texture/i-image-texture.interface.ts
@@ -0,0 +1,41 @@
+import { TextureFormat } from '../../constant/texture-format.enum';
+import { TextureUsage } from '../../constant/texture-usage.enum';
+
+export interface IImageTexture {
+    /**
+     * Texture height.
+     */
+    readonly height: number;
+
+    /**
+     * Loaded html image element list.
+     */
+    readonly images: Array<HTMLImageElement>;
+
+    /**
+     * Texture width.
+     */
+    readonly width: number;
+
+    /**
+     * Texture depth.
+     */
+    readonly depth: number;
+
+    /**
+     * Texture format.
+     */
+    readonly format: TextureFormat;
+
+    /**
+     * Texture usage.
+     */
+    readonly usage: TextureUsage
+
+    /**
+     * Load image into texture.
+     * Images needs to have the same dimensions.
+     * @param pSorceList - Source for each depth layer.
+     */
+    load(...pSorceList: Array<string>): Promise<void>;
+}

c2791b5eeef1b7dc329b0e95a740012bf462262e
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-06-04 03:31:24 +0200

    Sampler and texture  web gpu memory layout

---
+++ packages/kartoffelgames.web.gpu/source/base/webgpu/memory_layout/sampler-memory-layout.ts
@@ -0,0 +1,6 @@
+import { Base } from '../../base/export.';
+import { ISamplerMemoryLayout } from '../../interface/memory_layout/i-sampler-memory-layout.interface';
+
+export class SamplerMemoryLayout extends Base.SamplerMemoryLayout implements ISamplerMemoryLayout {
+    
+}

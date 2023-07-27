082533c189bf5b52ed3eb925dacdb2c7e9e91cb0
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-07-28 00:22:33 +0200

    Bunde cube resources into own directory.

--- packages/kartoffelgames.web.gpu/page/source/index.ts
+++ packages/kartoffelgames.web.gpu/page/source/index.ts
@@ -21,6 +21,7 @@ import { OrthographicProjection } from '../../source/something_better/view_proje
 import { PerspectiveProjection } from '../../source/something_better/view_projection/projection/perspective-projection';
 import { CameraMatrix, ViewProjection } from '../../source/something_better/view_projection/view-projection';
 import shader from './shader.wgsl';
+import { CubeVertexIndices, CubeVertexNormalData, CubeVertexPositionData, CubeVertexUvData } from './cube/cube';
 
 const gHeight: number = 10;
 const gWidth: number = 10;
@@ -294,149 +295,11 @@ const gDepth: number = 10;
     // Setup Sampler.
     const lCubeSampler: WebGpuTextureSampler = new WebGpuTextureSampler(lGpu);
 
-    // Create attributes data.
-    const lVertexPositionData: Array<number> = [ // 4x Position
-        // Back
-        -1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0,
-        1.0, -1.0, 1.0, 1.0,
-        -1.0, -1.0, 1.0, 1.0,
-
-        // Front
-        -1.0, 1.0, -1.0, 1.0,
-        1.0, 1.0, -1.0, 1.0,
-        1.0, -1.0, -1.0, 1.0,
-        -1.0, -1.0, -1.0, 1.0
-    ];
-    const lVertexUvData: Array<number> = [ // 4x Position
-        // Front 4,5,6
-        0.33333, 0.25,
-        0.66666, 0.25,
-        0.66666, 0.50,
-        // Front 4,6,7
-        0.33333, 0.25,
-        0.66666, 0.50,
-        0.33333, 0.50,
-
-        // Back 1,0,3
-        0.66666, 1,
-        0.33333, 1,
-        0.33333, 0.75,
-        // Back 1,3,2
-        0.66666, 1,
-        0.33333, 0.75,
-        0.66666, 0.75,
-
-        // Left 0,4,7
-        0, 0.25,
-        0.33333, 0.25,
-        0.33333, 0.50,
-        // Left 0,7,3
-        0, 0.25,
-        0.33333, 0.50,
-        0, 0.50,
-
-        // Right 5,1,2
-        0.66666, 0.25,
-        1, 0.25,
-        1, 0.50,
-        // Right 5,2,6
-        0.66666, 0.25,
-        1, 0.50,
-        0.66666, 0.50,
-
-        // Top 0,1,5
-        0.33333, 0,
-        0.66666, 0,
-        0.66666, 0.25,
-        // Top 0,5,4
-        0.33333, 0,
-        0.66666, 0.25,
-        0.33333, 0.25,
-
-        // Bottom 7,6,2
-        0.33333, 0.50,
-        0.66666, 0.50,
-        0.66666, 0.75,
-        // Bottom 7,2,3
-        0.33333, 0.50,
-        0.66666, 0.75,
-        0.33333, 0.75,
-    ];
-    const lVertexNormalData: Array<number> = [ // 4x Position
-        // Front
-        0, 0, -1, 0,
-        0, 0, -1, 0,
-        0, 0, -1, 0,
-        0, 0, -1, 0,
-        0, 0, -1, 0,
-        0, 0, -1, 0,
-
-        // Back 1,0,3
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-        0, 0, 1, 0,
-
-        // Left 0,4,7
-        -1, 0, 0, 0,
-        -1, 0, 0, 0,
-        -1, 0, 0, 0,
-        -1, 0, 0, 0,
-        -1, 0, 0, 0,
-        -1, 0, 0, 0,
-
-        // Right 5,1,2
-        1, 0, 0, 0,
-        1, 0, 0, 0,
-        1, 0, 0, 0,
-        1, 0, 0, 0,
-        1, 0, 0, 0,
-        1, 0, 0, 0,
-
-        // Top 0,1,5
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-        0, 1, 0, 0,
-
-        // Bottom 7,6,2
-        0, -1, 0, 0,
-        0, -1, 0, 0,
-        0, -1, 0, 0,
-        0, -1, 0, 0,
-        0, -1, 0, 0,
-        0, -1, 0, 0,
-    ];
-
     // Create mesh.
-    const lMesh = new RenderParameter(lGpu, [
-        // Front
-        4, 5, 6,
-        4, 6, 7,
-        // Back
-        1, 0, 3,
-        1, 3, 2,
-        // Left
-        0, 4, 7,
-        0, 7, 3,
-        // Right
-        5, 1, 2,
-        5, 2, 6,
-        // Top
-        0, 1, 5,
-        0, 5, 4,
-        // Bottom
-        7, 6, 2,
-        7, 2, 3
-    ]);
-    lMesh.setVertexData('vertex.position', lVertexPositionData, 4);
-    lMesh.setIndexData('vertex.uv', lVertexUvData, 2);
-    lMesh.setIndexData('vertex.normal', lVertexNormalData, 4);
+    const lMesh = new RenderParameter(lGpu, CubeVertexIndices);
+    lMesh.setVertexData('vertex.position', CubeVertexPositionData, 4);
+    lMesh.setIndexData('vertex.uv', CubeVertexUvData, 2);
+    lMesh.setIndexData('vertex.normal', CubeVertexNormalData, 4);
 
     // Setup renderer.
     const lInstructionExecutioner: InstructionExecuter = new InstructionExecuter(lGpu);

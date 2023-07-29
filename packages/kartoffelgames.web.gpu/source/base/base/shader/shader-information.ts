649bc97c2cd7e57e92b73be49966b7464947c6de
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-07-29 04:17:50 +0200

    Type definitions of native shader types inside shader information.

--- packages/kartoffelgames.web.gpu/source/base/base/shader/shader-information.ts
+++ packages/kartoffelgames.web.gpu/source/base/base/shader/shader-information.ts
@@ -1,4 +1,4 @@
-import { Dictionary } from '@kartoffelgames/core.data';
+import { Dictionary, Exception } from '@kartoffelgames/core.data';
 import { ComputeStage } from '../../constant/compute-stage.enum';
 import { GpuDependent } from '../gpu/gpu-dependent';
 import { GpuTypes } from '../gpu/gpu-device';
@@ -7,6 +7,9 @@ export abstract class ShaderInformation<TGpuTypes extends GpuTypes> extends GpuD
     private readonly mBindings: Dictionary<number, Array<TGpuTypes['memoryLayout']>>;
     private readonly mEntryPoints: Dictionary<ComputeStage, ShaderFunction<TGpuTypes>>;
     private readonly mShaderFunctions: Dictionary<string, ShaderFunction<TGpuTypes>>;
+    private readonly mShaderStructDefinitions: Dictionary<string, ShaderStructDefinition<TGpuTypes>>;
+    private readonly mShaderTypeAliases: Dictionary<string, ShaderTypeAlias>;
+    private readonly mShaderTypes: Dictionary<string, ShaderTypeDefinition>;
     private readonly mShaderValue: Dictionary<string, ShaderValue<TGpuTypes>>;
     private readonly mSourceCode: string;
 
@@ -40,23 +43,104 @@ export abstract class ShaderInformation<TGpuTypes extends GpuTypes> extends GpuD
 
         this.mSourceCode = pSourceCode;
 
+        // Setup all shader types.
+        this.mShaderTypes = new Dictionary<string, ShaderTypeDefinition>();
+        this.mShaderTypeAliases = new Dictionary<string, ShaderTypeAlias>();
+        this.setupShaderTypes((pType: ShaderTypeDefinition) => {
+            this.mShaderTypes.set(pType.name, pType);
+
+            // Map all aliases of type.
+            for (const lVariant of pType.variants) {
+                for (const lAlias of lVariant.aliases) {
+                    this.mShaderTypeAliases.set(lAlias, {
+                        type: pType.name,
+                        generics: lVariant.generic
+                    });
+                }
+            }
+        });
+
+        // Read defintions.
+        const lShaderFunctionDefinitionList: Array<ShaderFunctionDefintion<TGpuTypes>> = this.fetchFunctionDefinitions(pSourceCode);
+        const lShaderValueDefinitionList: Array<ShaderValueDefinition<TGpuTypes>> = this.fetchValueDefinitions(pSourceCode);
+        const lShaderStructDefinitionList: Array<ShaderStructDefinition<TGpuTypes>> = this.fetchStructDefinitions(pSourceCode);
+
+        // Map shader structs.
+        this.mShaderStructDefinitions = new Dictionary<string, ShaderStructDefinition<TGpuTypes>>();
+        for (const lStructDefinition of lShaderStructDefinitionList) {
+            this.mShaderStructDefinitions.set(lStructDefinition.name, lStructDefinition);
+        }
+
         // Meta data storages placeholders.
-        this.mShaderFunctions = new Dictionary<string, ShaderFunction<TGpuTypes>>();
-        this.mShaderValue = new Dictionary<string, ShaderValue<TGpuTypes>>();
-
-        // Get all shader functions.
-        for (const lShaderFunction of this.fetchFunctions(pSourceCode)) {
-            this.mShaderFunctions.set(lShaderFunction.name, lShaderFunction);
-        }
-
-        // Get all shader values.
-        for (const lShaderValue of this.fetchGlobalValues(pSourceCode)) {
-            this.mShaderValue.set(lShaderValue.name, lShaderValue);
-        }
+        this.mShaderFunctions = this.convertFunctions(lShaderFunctionDefinitionList);
+        this.mShaderValue = this.convertValues(lShaderValueDefinitionList);
 
         // Set entry point and bindings.
         this.mEntryPoints = this.readEntryPoints();
         this.mBindings = this.readBindings();
+    }
+
+    /**
+     * Create struct from value definition.
+     * @param pValueDefinition - value definition.
+     */
+    protected structFromDefinition(pStructDefinition: ShaderStructDefinition<TGpuTypes>): ShaderStruct<TGpuTypes> {
+        const lShaderStruct: ShaderStruct<TGpuTypes> = {
+            name: pStructDefinition.name,
+            properties: new Array<ShaderValue<TGpuTypes>>()
+        };
+
+        // Convert all properties to struct values.
+        for (const lProperty of pStructDefinition.properies) {
+            lShaderStruct.properties.push(this.valueFromDefinition(lProperty));
+        }
+
+        return lShaderStruct;
+    }
+
+    /**
+     * Get type of type, alias or struct name
+     * @param pTypeName - Type, alias or struct name.
+     * @param pGenericNames - Generics of type. Only valid on type names.
+     */
+    protected typeFor(pTypeName: string, pGenericNames: Array<string> = []): ShaderType<TGpuTypes> {
+        // Search for regular type.
+        if (this.mShaderTypes.has(pTypeName)) {
+            const lRegularType: ShaderTypeDefinition = this.mShaderTypes.get(pTypeName)!;
+            for (const lVariant of lRegularType.variants) {
+                if (lVariant.generic.toString() === pGenericNames.toString()) {
+                    return {
+                        typeName: lRegularType.name,
+                        type: 'buildIn',
+                        size: lVariant.size,
+                        align: lVariant.align
+                    };
+                }
+            }
+        }
+
+        // No gernics allows after this point.
+        if (pGenericNames.length > 0) {
+            throw new Exception(`No generics allowed for struct or alias types. Regular type "${pTypeName}<${pGenericNames.toString()}>" not found.`, this);
+        }
+
+        // Search alias type.
+        if (this.mShaderTypeAliases.has(pTypeName)) {
+            const lAliasType: ShaderTypeAlias = this.mShaderTypeAliases.get(pTypeName)!;
+            return this.typeFor(lAliasType.type, lAliasType.generics);
+        }
+
+        // Search for struct.
+        if (this.mShaderStructDefinitions.has(pTypeName)) {
+            const lStructDefinition: ShaderStructDefinition<TGpuTypes> = this.mShaderStructDefinitions.get(pTypeName)!;
+            return {
+                type: 'struct',
+                struct: this.structFromDefinition(lStructDefinition)
+            };
+        }
+
+        // Nothing found.
+        throw new Exception(`Type "${pTypeName}" not found.`, this);
     }
 
     /**
@@ -71,6 +155,34 @@ export abstract class ShaderInformation<TGpuTypes extends GpuTypes> extends GpuD
         }
 
         return lComputeStage;
+    }
+
+    /**
+     * Get all functions.
+     * @param pSourceCode - Source code of shader.
+     */
+    private convertFunctions(pFunctionDefinitions: Array<ShaderFunctionDefintion<TGpuTypes>>): Dictionary<string, ShaderFunction<TGpuTypes>> {
+        const lShaderFunctions: Dictionary<string, ShaderFunction<TGpuTypes>> = new Dictionary<string, ShaderFunction<TGpuTypes>>();
+        for (const lDefnition of pFunctionDefinitions) {
+            const lShaderFunction: ShaderFunction<TGpuTypes> = this.functionFromDefinition(lDefnition);
+            lShaderFunctions.set(lShaderFunction.name, lShaderFunction);
+        }
+
+        return lShaderFunctions;
+    }
+
+    /**
+     * Get all global values.
+     * @param pSourceCode - Source code of shader.
+     */
+    private convertValues(pValueDefinitions: Array<ShaderValueDefinition<TGpuTypes>>): Dictionary<string, ShaderValue<TGpuTypes>> {
+        const lShaderValues: Dictionary<string, ShaderValue<TGpuTypes>> = new Dictionary<string, ShaderValue<TGpuTypes>>();
+        for (const lDefnition of pValueDefinitions) {
+            const lShaderValue: ShaderValue<TGpuTypes> = this.valueFromDefinition(lDefnition);
+            lShaderValues.set(lShaderValue.name, lShaderValue);
+        }
+
+        return lShaderValues;
     }
 
     /**
@@ -153,19 +265,79 @@ export abstract class ShaderInformation<TGpuTypes extends GpuTypes> extends GpuD
     }
 
     /**
-     * Get all functions.
+     * Read all function definitions.
      * @param pSourceCode - Source code of shader.
      */
-    protected abstract fetchFunctions(pSourceCode: string): Array<ShaderFunction<TGpuTypes>>;
+    protected abstract fetchFunctionDefinitions(pSourceCode: string): Array<ShaderFunctionDefintion<TGpuTypes>>;
 
     /**
-     * Get all global shader values.
+     * Read all global shader values.
      * @param pSourceCode - Source code of shader.
      */
-    protected abstract fetchGlobalValues(pSourceCode: string): Array<ShaderValue<TGpuTypes>>;
+    protected abstract fetchStructDefinitions(pSourceCode: string): Array<ShaderStructDefinition<TGpuTypes>>;
+
+    /**
+     * Read all global shader values.
+     * @param pSourceCode - Source code of shader.
+     */
+    protected abstract fetchValueDefinitions(pSourceCode: string): Array<ShaderValueDefinition<TGpuTypes>>;
+
+    /**
+     * Convert function definition.
+     * @param pDefinition - Function definition.
+     */
+    protected abstract functionFromDefinition(pDefinition: ShaderFunctionDefintion<TGpuTypes>): ShaderFunction<TGpuTypes>;
+
+    /**
+     * Setup all shader types.
+     * @param pAddType - Add shader type callback.
+     */
+    protected abstract setupShaderTypes(pAddType: (pType: ShaderTypeDefinition) => void): void;
+
+    /**
+     * Create memory layout from value definition.
+     * @param pValueDefinition - value definition.
+     */
+    protected abstract valueFromDefinition(pValueDefinition: ShaderValueDefinition<TGpuTypes>): ShaderValue<TGpuTypes>;
 }
 
-export type ShaderGlobal<TGpuTypes extends GpuTypes> = ShaderValue<TGpuTypes> | ShaderFunction<TGpuTypes>;
+/*
+ * Definitions.
+ */
+
+export type ShaderValueDefinition<TGpuTypes extends GpuTypes> = {
+    name: string;
+    type: ShaderType<TGpuTypes>;
+    attachments: Record<string, string>;
+};
+
+export type ShaderFunctionDefintion<TGpuTypes extends GpuTypes> = {
+    attachments: Record<string, string>;
+    name: string;
+    returnType: ShaderValueDefinition<TGpuTypes>;
+    parameter: Array<ShaderValueDefinition<TGpuTypes>>;
+    declaredVariableNames: Array<string>,
+    usedVariableNames: Array<string>;
+};
+
+export type ShaderStructDefinition<TGpuTypes extends GpuTypes> = {
+    name: string,
+    properies: Array<ShaderValueDefinition<TGpuTypes>>;
+};
+
+export type ShaderTypeDefinition = {
+    name: string,
+    variants: Array<{
+        aliases: Array<string>;
+        size: number;
+        align: number;
+        generic: Array<string>;
+    }>;
+};
+
+/*
+ * Values.
+ */
 
 export type ShaderValue<TGpuTypes extends GpuTypes> = {
     name: string,
@@ -179,4 +351,28 @@ export type ShaderFunction<TGpuTypes extends GpuTypes> = {
     name: string;
     parameter: Array<TGpuTypes['memoryLayout']>;
     return: TGpuTypes['memoryLayout'] | null;
+};
+
+export type ShaderStruct<TGpuTypes extends GpuTypes> = {
+    name: string;
+    properties: Array<ShaderValue<TGpuTypes>>;
+};
+
+/*
+ * Types.
+ */
+
+export type ShaderType<TGpuTypes extends GpuTypes> = {
+    type: 'buildIn';
+    typeName: string;
+    size: number;
+    align: number;
+} | {
+    type: 'struct';
+    struct: ShaderStruct<TGpuTypes>;
+};
+
+type ShaderTypeAlias = {
+    type: string;
+    generics: Array<string>;
 };

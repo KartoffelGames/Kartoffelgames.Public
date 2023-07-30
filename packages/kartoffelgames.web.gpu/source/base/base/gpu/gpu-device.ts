49fd1bd08158ce0aaf28e9cff771dd4c2950447a
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-07-30 08:26:07 +0200

    No more self creations of memory layouts.

--- packages/kartoffelgames.web.gpu/source/base/base/gpu/gpu-device.ts
+++ packages/kartoffelgames.web.gpu/source/base/base/gpu/gpu-device.ts
@@ -1,48 +1,37 @@
 import { TypedArray } from '@kartoffelgames/core.data';
+import { BindGroup } from '../binding/bind-group';
+import { BindGroupLayout } from '../binding/bind-group-layout';
+import { PipelineLayout } from '../binding/pipeline-layout';
 import { Buffer } from '../buffer/buffer';
-import { ArrayBufferMemoryLayout, ArrayBufferMemoryLayoutParameter } from '../memory_layout/buffer/array-buffer-memory-layout';
-import { LinearBufferMemoryLayout, LinearBufferMemoryLayoutParameter } from '../memory_layout/buffer/linear-buffer-memory-layout';
-import { StructBufferMemoryLayout, StructBufferMemoryLayoutParameter } from '../memory_layout/buffer/struct-buffer-memory-layout';
-import { SamplerMemoryLayout, SamplerMemoryLayoutParameter } from '../memory_layout/sampler-memory-layout';
-import { TextureMemoryLayout, TextureMemoryLayoutParameter } from '../memory_layout/texture-memory-layout';
+import { ArrayBufferMemoryLayout } from '../memory_layout/buffer/array-buffer-memory-layout';
+import { LinearBufferMemoryLayout } from '../memory_layout/buffer/linear-buffer-memory-layout';
+import { StructBufferMemoryLayout } from '../memory_layout/buffer/struct-buffer-memory-layout';
+import { SamplerMemoryLayout } from '../memory_layout/sampler-memory-layout';
+import { TextureMemoryLayout } from '../memory_layout/texture-memory-layout';
+import { Shader } from '../shader/shader';
+import { ShaderInformation } from '../shader/shader-information';
 import { FrameBufferTexture } from '../texture/frame-buffer-texture';
 import { ImageTexture } from '../texture/image-texture';
 import { TextureSampler } from '../texture/texture-sampler';
 import { VideoTexture } from '../texture/video-texture';
-import { BindGroupLayout } from '../binding/bind-group-layout';
-import { PipelineLayout } from '../binding/pipeline-layout';
-import { BindGroup } from '../binding/bind-group';
 
 export abstract class GpuDevice<TGpuTypes extends GpuTypes = GpuTypes> {
     /**
-     * Create array buffer memory layout.
-     * @param pParameter - Memory layout parameter.
+     * Generate empty bind group layout.
      */
-    public abstract arrayMemoryLayout(pParameter: ArrayBufferMemoryLayoutParameter<TGpuTypes>): TGpuTypes['arrayBufferMemoryLayout'];
+    public abstract bindGroupLayout(): TGpuTypes['bindGroupLayout'];
 
     /**
-     * Create array buffer memory layout.
-     * @param pParameter - Memory layout parameter.
+     * Generate empty pipeline layout.
      */
-    public abstract linearMemoryLayout(pParameter: LinearBufferMemoryLayoutParameter): TGpuTypes['linearBufferMemoryLayout'];
+    public abstract pipelineLayout(): TGpuTypes['pipelineLayout'];
 
     /**
-     * Create sampler memory layout.
-     * @param pParameter - Memory layout parameter.
+     * Create shader.
+     * @param pSource - Shader source.
      */
-    public abstract samplerMemoryLayout(pParameter: SamplerMemoryLayoutParameter): TGpuTypes['samplerMemoryLayout'];
+    public abstract shader(pSource: string): TGpuTypes['shader'];
 
-    /**
-     * Create struct buffer memory layout.
-     * @param pParameter - Memory layout parameter.
-     */
-    public abstract structMemoryLayout(pParameter: StructBufferMemoryLayoutParameter): TGpuTypes['structBufferMemoryLayout'];
-
-    /**
-     * Create texture memory layout.
-     * @param pParameter - Memory layout parameter.
-     */
-    public abstract textureMemoryLayout(pParameter: TextureMemoryLayoutParameter): TGpuTypes['textureMemoryLayout'];
 }
 
 export interface GpuTypes {
@@ -76,4 +65,8 @@ export interface GpuTypes {
     bindGroupLayout: BindGroupLayout;
     pipelineLayout: PipelineLayout;
     bindGroup: BindGroup;
+
+    // Shader.
+    shader: Shader;
+    shaderInformation: ShaderInformation;
 }

ddb44d7e3db809663c916efb755f0f40aaf83e37
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-08-05 04:22:12 +0200

    Move update callbacking into gpu dependent.

--- packages/kartoffelgames.web.gpu/source/base/base/gpu/gpu-dependent.ts
+++ packages/kartoffelgames.web.gpu/source/base/base/gpu/gpu-dependent.ts
@@ -1,7 +1,18 @@
 import { GpuTypes } from './gpu-device';
 
 export class GpuDependent<TGpuTypes extends GpuTypes> {
+    private mAutoUpdate: boolean;
     private readonly mDevice: TGpuTypes['gpuDevice'];
+    private readonly mUpdateListenerList: Set<GpuObjectUpdateListener>;
+
+    /**
+     * Enable or disable auto update.
+     */
+    public get autoUpdate(): boolean {
+        return this.mAutoUpdate;
+    } set autoUpdate(pValue: boolean) {
+        this.mAutoUpdate = pValue;
+    }
 
     /**
      * Gpu Device.
@@ -15,6 +26,46 @@ export class GpuDependent<TGpuTypes extends GpuTypes> {
      * @param pDevice - Gpu device.
      */
     public constructor(pDevice: TGpuTypes['gpuDevice']) {
+        this.mAutoUpdate = true;
         this.mDevice = pDevice;
+        this.mUpdateListenerList = new Set<GpuObjectUpdateListener>();
+    }
+
+    /**
+     * Add update listener.
+     * @param pListener - Listener.
+     */
+    public addUpdateListener(pListener: GpuObjectUpdateListener): void {
+        this.mUpdateListenerList.add(pListener);
+    }
+
+    /**
+     * Add update listener.
+     * @param pListener - Listener.
+     */
+    public removeUpdateListener(pListener: GpuObjectUpdateListener): void {
+        this.mUpdateListenerList.delete(pListener);
+    }
+
+    /**
+    * Update gpu object.
+    */
+    public update(): void {
+        // Call parent update listerner.
+        for (const lUpdateListener of this.mUpdateListenerList) {
+            lUpdateListener();
+        }
+    }
+
+    /**
+     * Trigger auto update.
+     * Does nothing on disabled auto update.
+     */
+    protected triggerAutoUpdate(): void {
+        if (this.mAutoUpdate) {
+            this.update();
+        }
     }
 }
+
+export type GpuObjectUpdateListener = () => void;

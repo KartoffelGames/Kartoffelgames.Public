178c3858f631755ba623560d5daacf5d8558ffaf
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-08-06 05:32:49 +0200

    Create default texture layouts for new render targets.

--- packages/kartoffelgames.web.gpu/source/base/implementation/webgpu/pipeline/web-gpu-render-targets.ts
+++ packages/kartoffelgames.web.gpu/source/base/implementation/webgpu/pipeline/web-gpu-render-targets.ts
@@ -1,4 +1,58 @@
 import { RenderTargets } from '../../../base/pipeline/render-targets';
+import { AccessMode } from '../../../constant/access-mode.enum';
+import { ComputeStage } from '../../../constant/compute-stage.enum';
+import { TextureBindType } from '../../../constant/texture-bind-type.enum';
+import { TextureDimension } from '../../../constant/texture-dimension.enum';
+import { TextureFormat } from '../../../constant/texture-format.enum';
+import { WebGpuTextureMemoryLayout } from '../memory_layout/web-gpu-texture-memory-layout';
 import { WebGpuTypes } from '../web-gpu-device';
 
-export class WebGpuRenderTargets extends RenderTargets<WebGpuTypes> { }
+export class WebGpuRenderTargets extends RenderTargets<WebGpuTypes> {
+    /**
+     * Create layout for a canvas texture.
+     */
+    protected override createCanvasMemoryLayout(): WebGpuTextureMemoryLayout {
+        return new WebGpuTextureMemoryLayout(this.device, {
+            dimension: TextureDimension.ThreeDimension,
+            format: TextureFormat.RedGreenBlueAlpha,
+            bindType: TextureBindType.RenderTarget,
+            multisampled: this.multiSampleLevel > 1,
+            access: AccessMode.Write,
+            memoryIndex: null,
+            name: '',
+            visibility: ComputeStage.Fragment
+        });
+    }
+
+    /**
+     * Create layout for a color texture.
+     */
+    protected override createColorMemoryLayout(): WebGpuTextureMemoryLayout {
+        return new WebGpuTextureMemoryLayout(this.device, {
+            dimension: TextureDimension.ThreeDimension,
+            format: TextureFormat.RedGreenBlueAlpha,
+            bindType: TextureBindType.RenderTarget,
+            multisampled: this.multiSampleLevel > 1,
+            access: AccessMode.Write,
+            memoryIndex: null,
+            name: '',
+            visibility: ComputeStage.Fragment
+        });
+    }
+
+    /**
+     * Create layout for a depth texture.
+     */
+    protected override createDepthMemoryLayout(): WebGpuTextureMemoryLayout {
+        return new WebGpuTextureMemoryLayout(this.device, {
+            dimension: TextureDimension.ThreeDimension,
+            format: TextureFormat.Depth,
+            bindType: TextureBindType.RenderTarget,
+            multisampled: this.multiSampleLevel > 1,
+            access: AccessMode.Write,
+            memoryIndex: null,
+            name: '',
+            visibility: ComputeStage.Fragment
+        });
+    }
+}

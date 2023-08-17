b04015764b4b6fb7c804c975dcd0ba0f2031f10e
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-08-17 23:49:19 +0200

    Web gpu parameter layout.

---
+++ packages/kartoffelgames.web.gpu/source/base/implementation/webgpu/pipeline/parameter/web-gpu-parameter-layout.ts
@@ -0,0 +1,6 @@
+import { ParameterLayout } from '../../../../base/pipeline/parameter/parameter-layout';
+import { WebGpuTypes } from '../../web-gpu-device';
+
+export class WebGpuParameterLayout extends ParameterLayout<WebGpuTypes> {
+
+}

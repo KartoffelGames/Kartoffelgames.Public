90be98fa20661f195762a6a3d5a4e598c116c9e8
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-09-16 02:31:06 +0200

    Web gpu buffer generator.

--- packages/kartoffelgames.web.gpu/source/base/base/implementation/web_gpu/native-generator/web-gpu-gpu-buffer-generator.ts
+++ packages/kartoffelgames.web.gpu/source/base/base/implementation/web_gpu/native-generator/web-gpu-gpu-buffer-generator.ts
@@ -1,32 +1,24 @@
 import { TypedArray } from '@kartoffelgames/core.data';
-import { GpuBuffer } from '../../../base/buffer/gpu-buffer';
-import { BufferBindType } from '../../../constant/buffer-bind-type.enum';
-import { MemoryCopyType } from '../../../constant/memory-copy-type.enum';
-import { WebGpuDevice, WebGpuTypes } from '../web-gpu-device';
+import { BaseNativeBufferGenerator } from '../../../generator/base-native-buffer-generator';
+import { NativeObjectLifeTime } from '../../../generator/base-native-generator';
+import { NativeWebGpuMap, WebGpuGeneratorFactory } from '../web-gpu-generator-factory';
+import { GpuBuffer } from '../../../buffer/gpu-buffer';
+import { BufferBindType } from '../../../../constant/buffer-bind-type.enum';
+import { MemoryCopyType } from '../../../../constant/memory-copy-type.enum';
 
-export class WebGpuBuffer<T extends TypedArray> extends GpuBuffer<T, WebGpuTypes, GPUBuffer>  {
-    private readonly mDataType: BufferDataType<T>;
-
+export class WebGpuGpuBufferGenerator extends BaseNativeBufferGenerator<NativeWebGpuMap, 'gpuBuffer'> {
     private readonly mReadyBufferList: Array<GPUBuffer>;
     private readonly mWavingBufferList: Array<GPUBuffer>;
 
     /**
-     * Buffer size in bytes aligned to 4 bytes.
+     * Set life time of generated native.
      */
-    public get size(): number {
-        return ((this.initialData.length * this.mDataType.BYTES_PER_ELEMENT) + 3) & ~3;
+    protected override get nativeLifeTime(): NativeObjectLifeTime {
+        return NativeObjectLifeTime.Persistent;
     }
 
-    /**
-     * Constructor.
-     * @param pDevice - GPU.
-     * @param pLayout - Buffer layout.
-     * @param pInitialData  - Inital data. Can be empty.
-     */
-    public constructor(pDevice: WebGpuDevice, pLayout: WebGpuTypes['bufferMemoryLayout'], pInitialData: T) {
-        super(pDevice, pLayout, pInitialData);
-
-        this.mDataType = <BufferDataType<T>>pInitialData.constructor;
+    public constructor(pFactory: WebGpuGeneratorFactory, pBaseObject: GpuBuffer<TypedArray>) {
+        super(pFactory, pBaseObject);
 
         // Waving buffer list.
         this.mReadyBufferList = new Array<GPUBuffer>();
@@ -34,33 +26,18 @@ export class WebGpuBuffer<T extends TypedArray> extends GpuBuffer<T, WebGpuTypes
     }
 
     /**
-     * Read data raw without layout.
-     * @param pOffset - Data offset.
-     * @param pSize - Data size.
-     */
-    public async readRaw(pOffset?: number, pSize?: number): Promise<T> {
-        // Get buffer and map data.
-        const lBuffer: GPUBuffer = this.native;
-        await lBuffer.mapAsync(GPUMapMode.READ, pOffset, pSize);
-
-        // Get mapped data and force it into typed array.
-        const lData = new this.mDataType(lBuffer.getMappedRange());
-        return lData;
-    }
-
-    /**
-     * Write data raw without layout.
+     * Write data raw.
      * @param pData - Data.
      * @param pOffset - Data offset.
      * @param pSize - Data size.
      */
-    public async writeRaw(pData: ArrayLike<number>, pOffset?: number, pSize?: number): Promise<void> {
+    public override async writeRaw(pData: ArrayLike<number>, pOffset?: number | undefined, pSize?: number | undefined): Promise<void> {
         // Create new buffer when no mapped buffer is available. 
         let lStagingBuffer: GPUBuffer;
         if (this.mReadyBufferList.length === 0) {
-            lStagingBuffer = this.device.reference.createBuffer({
+            lStagingBuffer = this.factory.gpu.createBuffer({
                 label: `RingBuffer-WaveBuffer-${this.mWavingBufferList.length}`,
-                size: this.size,
+                size: this.gpuObject.size,
                 usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.COPY_SRC,
                 mappedAtCreation: true,
             });
@@ -72,16 +49,16 @@ export class WebGpuBuffer<T extends TypedArray> extends GpuBuffer<T, WebGpuTypes
         }
 
         // Execute write operations.
-        const lBufferArray: T = new this.mDataType(lStagingBuffer.getMappedRange(pOffset, pSize));
+        const lBufferArray: TypedArray = new this.gpuObject.dataType(lStagingBuffer.getMappedRange(pOffset, pSize));
         lBufferArray.set(pData);
 
         // Unmap for copying data.
         lStagingBuffer.unmap();
 
         // Copy buffer data from staging into wavig buffer.
-        const lCommandDecoder: GPUCommandEncoder = this.device.reference.createCommandEncoder();
-        lCommandDecoder.copyBufferToBuffer(lStagingBuffer, 0, this.native, 0, this.size);
-        this.device.reference.queue.submit([lCommandDecoder.finish()]);
+        const lCommandDecoder: GPUCommandEncoder = this.factory.gpu.createCommandEncoder();
+        lCommandDecoder.copyBufferToBuffer(lStagingBuffer, 0, this.create(), 0, this.gpuObject.size);
+        this.factory.gpu.queue.submit([lCommandDecoder.finish()]);
 
         // Shedule staging buffer remaping.
         lStagingBuffer.mapAsync(GPUMapMode.WRITE).then(() => {
@@ -90,9 +67,9 @@ export class WebGpuBuffer<T extends TypedArray> extends GpuBuffer<T, WebGpuTypes
     }
 
     /**
-     * Destroy buffer object.
+     * Destroy wave and ready buffer.
      */
-    protected destroyNative(pNativeObject: GPUBuffer): void {
+    protected override destroy(pNativeObject: GPUBuffer): void {
         pNativeObject.destroy();
 
         // Destroy all wave buffer and clear list.
@@ -102,18 +79,19 @@ export class WebGpuBuffer<T extends TypedArray> extends GpuBuffer<T, WebGpuTypes
 
         // Clear ready buffer list.
         for (let lCount: number = 0; this.mReadyBufferList.length < lCount; lCount++) {
+            // No need to destroy. All buffers have already destroyed.
             this.mReadyBufferList.pop();
         }
     }
 
     /**
-     * Generate native. Concat buffer usage bits from bind type and buffer usage.
+     * Generate buffer. Write local gpu object data as initial native buffer data.
      */
     protected override generate(): GPUBuffer {
         let lUsage: number = 0;
 
         // Append usage type from abstract bind type.
-        switch (this.memoryLayout.bindType) {
+        switch (this.gpuObject.memoryLayout.bindType) {
             case BufferBindType.Undefined: {
                 // Just an layout indicator. Does nothing to usage type.
                 break;
@@ -137,33 +115,28 @@ export class WebGpuBuffer<T extends TypedArray> extends GpuBuffer<T, WebGpuTypes
         }
 
         // Append usage type from abstract usage type.
-        if ((this.memoryLayout.memoryType & MemoryCopyType.CopyDestination) !== 0) {
+        if ((this.gpuObject.memoryLayout.memoryType & MemoryCopyType.CopyDestination) !== 0) {
             lUsage |= GPUBufferUsage.COPY_DST;
         }
-        if ((this.memoryLayout.memoryType & MemoryCopyType.CopySource) !== 0) {
+        if ((this.gpuObject.memoryLayout.memoryType & MemoryCopyType.CopySource) !== 0) {
             lUsage |= GPUBufferUsage.COPY_SRC;
         }
 
         // Create gpu buffer mapped
-        const lBuffer: GPUBuffer = this.device.reference.createBuffer({
+        const lBuffer: GPUBuffer = this.factory.gpu.createBuffer({
             label: 'Ring-Buffer-Static-Buffer',
-            size: this.size,
+            size: this.gpuObject.size,
             usage: lUsage,
             mappedAtCreation: true // Map data when buffer would receive initial data.
         });
 
-        const lData = new this.mDataType(lBuffer.getMappedRange());
-        lData.set(this.initialData, 0);
+        const lData = new this.gpuObject.dataType(lBuffer.getMappedRange());
+        lData.set(this.gpuObject.localData, 0);
 
         // unmap buffer.
         lBuffer.unmap();
 
         return lBuffer;
     }
-}
 
-type BufferDataType<T extends TypedArray> = {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    BYTES_PER_ELEMENT: number;
-    new(pInitValues: ArrayBuffer | number | TypedArray): T;
-};
+}

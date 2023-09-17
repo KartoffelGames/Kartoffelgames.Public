7ddbe9a513c78f40892735e2783e5f8c9cf4fdc3
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-09-17 23:04:12 +0200

    Web gpu frame buffer texture generator.

---
+++ packages/kartoffelgames.web.gpu/source/base/base/implementation/web_gpu/native-generator/web-gpu-frame-buffer-texture-generator.ts
@@ -0,0 +1,57 @@
+import { BaseNativeGenerator, NativeObjectLifeTime } from '../../../generator/base-native-generator';
+import { GpuObjectReason } from '../../../gpu/gpu-object-reason';
+import { FrameBufferTexture } from '../../../texture/frame-buffer-texture';
+import { NativeWebGpuMap, WebGpuGeneratorFactory } from '../web-gpu-generator-factory';
+
+export class WebGpuFramebufferTextureGenerator extends BaseNativeGenerator<NativeWebGpuMap, 'frameBufferTexture'> {
+    private mTexture: GPUTexture | null;
+
+    /**
+     * Set life time of generated native.
+     */
+    protected override get nativeLifeTime(): NativeObjectLifeTime {
+        return NativeObjectLifeTime.Frame;
+    }
+
+    /**
+     * Constructor.
+     * @param pBaseObject - Base object containing all values.
+     * @param pGeneratorFactory - Generator factory.
+     */
+    public constructor(pFactory: WebGpuGeneratorFactory, pBaseObject: FrameBufferTexture) {
+        super(pFactory, pBaseObject);
+
+        this.mTexture = null;
+    }
+
+    /**
+     * Destory texture object.
+     * @param _pNativeObject - Native canvas texture.
+     * @param pDestroyReason - Reason why the native should be destroyed.
+     */
+    protected override destroy(_pNativeObject: GPUTextureView, _pDestoryReason: GpuObjectReason): void {
+        this.mTexture?.destroy();
+        this.mTexture = null;
+    }
+
+    /**
+     * Generate native canvas texture view.
+     */
+    protected override generate(): GPUTextureView {
+        // Configure context.
+        if (!this.mTexture) {
+            // Create and configure canvas context.
+            this.mTexture = this.factory.gpu.createTexture({
+                label: 'Frame-Buffer-Texture',
+                size: [this.gpuObject.width, this.gpuObject.height, this.gpuObject.depth],
+                format: this.factory.formatFromLayout(this.gpuObject.memoryLayout),
+                usage: this.factory.usageFromLayout(this.gpuObject.memoryLayout),
+                dimension: this.factory.dimensionFromLayout(this.gpuObject.memoryLayout),
+                sampleCount: this.gpuObject.multiSampleLevel
+            });
+        }
+
+        // TODO: View descriptor.
+        return this.mTexture.createView();
+    }
+}

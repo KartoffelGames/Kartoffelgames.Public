9949ce24bf54d04be500ec4fc3f84ccafa4a9de1
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-09-18 00:06:00 +0200

    Web gpu texture sampler. Clean no longer needed legacy files.

--- packages/kartoffelgames.web.gpu/source/abstraction_layer/webgpu/texture_resource/texture/web-gpu-texture-usage.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum WebGpuTextureUsage {
-    CopySource = GPUTextureUsage.COPY_SRC,
-    CopyDestination = GPUTextureUsage.COPY_DST,
-    TextureBinding = GPUTextureUsage.TEXTURE_BINDING,
-    StorageBinding = GPUTextureUsage.STORAGE_BINDING,
-    RenderAttachment = GPUTextureUsage.RENDER_ATTACHMENT
-}

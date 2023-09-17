17f4b6371153350bd4eeba7ed9ef49b6b1d60292
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-09-17 23:28:38 +0200

    Web gpu image texture generator.

---
+++ packages/kartoffelgames.web.gpu/source/base/base/implementation/web_gpu/native-generator/web-gpu-image-texture-generator.ts
@@ -0,0 +1,65 @@
+import { BaseNativeGenerator, NativeObjectLifeTime } from '../../../generator/base-native-generator';
+import { GpuObjectReason } from '../../../gpu/gpu-object-reason';
+import { ImageTexture } from '../../../texture/image-texture';
+import { NativeWebGpuMap, WebGpuGeneratorFactory } from '../web-gpu-generator-factory';
+
+export class WebGpuImageTextureGenerator extends BaseNativeGenerator<NativeWebGpuMap, 'imageTexture'> {
+    private mTexture: GPUTexture | null;
+
+    /**
+     * Set life time of generated native.
+     */
+    protected override get nativeLifeTime(): NativeObjectLifeTime {
+        return NativeObjectLifeTime.Persistent;
+    }
+
+    /**
+     * Constructor.
+     * @param pBaseObject - Base object containing all values.
+     * @param pGeneratorFactory - Generator factory.
+     */
+    public constructor(pFactory: WebGpuGeneratorFactory, pBaseObject: ImageTexture) {
+        super(pFactory, pBaseObject);
+
+        this.mTexture = null;
+    }
+
+    /**
+     * Destory texture object.
+     * @param _pNativeObject - Native canvas texture.
+     * @param pDestroyReason - Reason why the native should be destroyed.
+     */
+    protected override destroy(_pNativeObject: GPUTextureView, _pDestoryReason: GpuObjectReason): void {
+        this.mTexture?.destroy();
+        this.mTexture = null;
+    }
+
+    /**
+     * Generate native canvas texture view.
+     */
+    protected override generate(): GPUTextureView {
+        // Create texture with set size, format and usage. Save it for destorying later.
+        this.mTexture = this.factory.gpu.createTexture({
+            label: 'Frame-Buffer-Texture',
+            size: [this.gpuObject.width, this.gpuObject.height, this.gpuObject.depth],
+            format: this.factory.formatFromLayout(this.gpuObject.memoryLayout),
+            usage: this.factory.usageFromLayout(this.gpuObject.memoryLayout),
+            dimension: this.factory.dimensionFromLayout(this.gpuObject.memoryLayout),
+        });
+
+        // Load images into texture.
+        for (let lImageIndex: number = 0; lImageIndex < this.gpuObject.images.length; lImageIndex++) {
+            const lBitmap: ImageBitmap = this.gpuObject.images[lImageIndex];
+
+            // Copy image into depth layer.
+            this.factory.gpu.queue.copyExternalImageToTexture(
+                { source: lBitmap },
+                { texture: this.mTexture, origin: [0, 0, lImageIndex] },
+                [lBitmap.width, lBitmap.height]
+            );
+        }
+
+        // TODO: View descriptor.
+        return this.mTexture.createView();
+    }
+}

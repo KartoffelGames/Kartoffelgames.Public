fa24ada535d4193f91bc306d5534e536bbc94518
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-09-28 23:50:54 +0200

    Vertex fragment pipeline.

---
+++ packages/kartoffelgames.web.gpu/source/base/base/pipeline/vertex-fragment-pipeline.ts
@@ -0,0 +1,116 @@
+import { CompareFunction } from '../../constant/compare-function.enum';
+import { PrimitiveCullMode } from '../../constant/primitive-cullmode';
+import { PrimitiveFrontFace } from '../../constant/primitive-front-face';
+import { PrimitiveTopology } from '../../constant/primitive-topology';
+import { GpuDevice } from '../gpu/gpu-device';
+import { GpuObject } from '../gpu/gpu-object';
+import { UpdateReason } from '../gpu/gpu-object-update-reason';
+import { RenderShader } from '../shader/render-shader';
+import { RenderTargets } from './target/render-targets';
+
+export class VertexFragmentPipeline extends GpuObject<'vertexFragmentPipeline'> {
+    private mDepthCompare: CompareFunction;
+    private mDepthWriteEnabled: boolean;
+    private mPrimitiveCullMode: PrimitiveCullMode;
+    private mPrimitiveFrontFace: PrimitiveFrontFace;
+    private mPrimitiveTopology: PrimitiveTopology;
+    private readonly mRenderTargets: RenderTargets;
+    private readonly mShader: RenderShader;
+    
+    /**
+     * Set depth compare function.
+     */
+    public get depthCompare(): CompareFunction {
+        return this.mDepthCompare;
+    } set depthCompare(pValue: CompareFunction) {
+        this.mDepthCompare = pValue;
+
+        // Set data changed flag.
+        this.triggerAutoUpdate(UpdateReason.Setting);
+    }
+
+    /**
+     * Defines which polygon orientation will be culled.
+     */
+    public get primitiveCullMode(): PrimitiveCullMode {
+        return this.mPrimitiveCullMode;
+    } set primitiveCullMode(pValue: PrimitiveCullMode) {
+        this.mPrimitiveCullMode = pValue;
+
+        // Set data changed flag.
+        this.triggerAutoUpdate(UpdateReason.Setting);
+    }
+
+    /**
+     * Defines which polygons are considered front-facing.
+     */
+    public get primitiveFrontFace(): PrimitiveFrontFace {
+        return this.mPrimitiveFrontFace;
+    } set primitiveFrontFace(pValue: PrimitiveFrontFace) {
+        this.mPrimitiveFrontFace = pValue;
+
+        // Set data changed flag.
+        this.triggerAutoUpdate(UpdateReason.Setting);
+    }
+
+    /**
+     * The type of primitive to be constructed from the vertex inputs.
+     */
+    public get primitiveTopology(): PrimitiveTopology {
+        return this.mPrimitiveTopology;
+    } set primitiveTopology(pValue: PrimitiveTopology) {
+        this.mPrimitiveTopology = pValue;
+
+        // Set data changed flag.
+        this.triggerAutoUpdate(UpdateReason.Setting);
+    }
+
+    /**
+     * Render targets.
+     */
+    public get renderTargets(): RenderTargets {
+        return this.mRenderTargets;
+    }
+
+    /**
+     * Pipeline shader.
+     */
+    public get shader(): RenderShader {
+        return this.mShader;
+    }
+
+    /**
+     * Set depth write enabled / disabled.
+     */
+    public get writeDepth(): boolean {
+        return this.mDepthWriteEnabled;
+    } set writeDepth(pValue: boolean) {
+        this.mDepthWriteEnabled = pValue;
+
+        // Set data changed flag.
+        this.triggerAutoUpdate(UpdateReason.Setting);
+    }
+
+    /**
+     * Constructor.
+     * Set default data.
+     * @param pDevice - Device.
+     * @param pShader - Pipeline shader.
+     */
+    public constructor(pDevice: GpuDevice, pShader: RenderShader, pRenderTargets: RenderTargets) {
+        super(pDevice);
+        this.mShader = pShader;
+        this.mRenderTargets = pRenderTargets;
+
+        // TODO: Listen for render target and shader changes.
+
+        // Depth default settings.
+        this.mDepthCompare = CompareFunction.Less;
+        this.mDepthWriteEnabled = true;
+
+        // Primitive default settings.
+        this.mPrimitiveTopology = PrimitiveTopology.TriangleList;
+        this.mPrimitiveCullMode = PrimitiveCullMode.Back;
+        this.mPrimitiveFrontFace = PrimitiveFrontFace.ClockWise;
+    }
+}

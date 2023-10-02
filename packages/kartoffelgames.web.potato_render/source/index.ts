4b5302b5cca7df5704ee32c6dbde43ceb528eb6d
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-03 01:20:10 +0200

    Move engine stuff into new package.

---
+++ packages/kartoffelgames.web.potato_render/source/index.ts
@@ -0,0 +1,29 @@
+// TODO: Exports.
+
+// Material
+    // Shader
+        // Typ. Deferred, Forward, Raytrace??? Or is this
+        // Module => [Light, Normal ...]
+        // Source
+    // VBOs - definition. Dictated by shader.
+    // Uniforms
+    // Settings => Transparent.
+
+// GameObject.
+    // Transformation
+    // Meshes
+        // Mesh ?? Verticles, UV
+        // Transformation
+        // Material
+        
+// Scene
+    // GameObject-List
+    // Lights
+        // Ambient.
+        // Point
+        // Spot
+        // Directional
+    // Camera(s)?
+    // Skybox
+
+// Renderer

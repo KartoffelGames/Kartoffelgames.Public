55f50f714e2269a7812924ccd63d05bf110b541e
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-05 21:37:20 +0200

    Remove last legacy files.

--- packages/kartoffelgames.web.gpu/source/abstraction_layer/webgpu/execution/instruction_set/i-instruction-set.ts
+++
@@ -1,7 +0,0 @@
-export interface IInstructionSet {
-    /**
-     * Execute instruction set.
-     * @param pCommandEncoder - Command encoder.
-     */
-    execute(pCommandEncoder: GPUCommandEncoder): void
-}

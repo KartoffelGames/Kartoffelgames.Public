eed4842923bc6d19362ee6eb1e0b6ffcd2c2657c
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-05 17:13:35 +0200

    Base Instructions for render and compute.

--- packages/kartoffelgames.web.gpu/source/base/base/execution/instruction/compute-instruction.ts
+++ packages/kartoffelgames.web.gpu/source/base/base/execution/instruction/compute-instruction.ts
@@ -1,5 +1,63 @@
+import { Exception } from '@kartoffelgames/core.data';
+import { BindDataGroup } from '../../binding/bind-data-group';
+import { GpuDevice } from '../../gpu/gpu-device';
 import { GpuObject } from '../../gpu/gpu-object';
+import { ComputePipeline } from '../../pipeline/compute-pipeline';
 
 export class ComputeInstruction extends GpuObject<'computeInstruction'> {
-    
+    private readonly mStepList: Array<ComputeInstructionStep>;
+
+    /**
+     * Constructor.
+     * @param pDevice - Device reference.
+     */
+    public constructor(pDevice: GpuDevice) {
+        super(pDevice);
+
+        this.mStepList = new Array<ComputeInstructionStep>();
+    }
+
+    /**
+     * Add instruction step.
+     * @param pPipeline - Pipeline.
+     * @param pBindData -  Pipeline bind data.
+     */
+    public addStep(pPipeline: ComputePipeline, pBindData: Record<number, BindDataGroup>): void {
+        const lStep: ComputeInstructionStep = {
+            pipeline: pPipeline,
+            bindData: new Array<BindDataGroup | null>()
+        };
+
+        // Fill in data groups.
+        for (const lGroup of pPipeline.shader.pipelineLayout.groups) {
+            const lBindDataGroup: BindDataGroup | undefined = pBindData[lGroup];
+
+            // Validate bind data group.
+            if (!lBindDataGroup) {
+                throw new Exception('Defined bind data group not set.', this);
+            }
+
+            // Validate same layout bind layout.
+            const lBindGroupLayout = pPipeline.shader.pipelineLayout.getGroupLayout(lGroup);
+            if (lBindDataGroup.layout.identifier !== lBindGroupLayout.identifier) {
+                throw new Exception('Source bind group layout does not match target layout.', this);
+            }
+
+            lStep.bindData[lGroup] = pBindData[lGroup];
+        }
+
+        this.mStepList.push(lStep);
+    }
+
+    /**
+     * Execute instruction.
+     */
+    public execute(): void {
+        this.device.generator.request<'computeInstruction'>(this).execute();
+    }
 }
+
+type ComputeInstructionStep = {
+    pipeline: ComputePipeline;
+    bindData: Array<BindDataGroup | null>;
+};
--- packages/kartoffelgames.web.gpu/source/base/base/execution/instruction/vertex-fragment-instruction.ts
+++ packages/kartoffelgames.web.gpu/source/base/base/execution/instruction/vertex-fragment-instruction.ts
@@ -1,5 +1,88 @@
+import { Exception } from '@kartoffelgames/core.data';
+import { BindDataGroup } from '../../binding/bind-data-group';
+import { GpuDevice } from '../../gpu/gpu-device';
 import { GpuObject } from '../../gpu/gpu-object';
+import { VertexParameter } from '../../pipeline/parameter/vertex-parameter';
+import { RenderTargets } from '../../pipeline/target/render-targets';
+import { VertexFragmentPipeline } from '../../pipeline/vertex-fragment-pipeline';
 
 export class VertexFragmentInstruction extends GpuObject<'vertexFragmentInstruction'> {
+    private readonly mRenderTargets: RenderTargets;
+    private readonly mStepList: Array<VertexFragmentInstructionStep>;
 
+    /**
+     * Get instruction render target.
+     */
+    public get renderTargets(): RenderTargets {
+        return this.mRenderTargets;
+    }
+
+    /**
+     * Get all instruction steps.
+     */
+    public get steps(): Array<VertexFragmentInstructionStep> {
+        return this.mStepList;
+    }
+
+    /**
+     * Constructor.
+     * @param pDevice - Device reference.
+     * @param pRenderTargets - Render targets. 
+     */
+    public constructor(pDevice: GpuDevice, pRenderTargets: RenderTargets) {
+        super(pDevice);
+
+        this.mStepList = new Array<VertexFragmentInstructionStep>();
+        this.mRenderTargets = pRenderTargets;
+    }
+
+    /**
+     * Add instruction step.
+     * @param pPipeline - Pipeline.
+     * @param pParameter - Pipeline parameter.
+     * @param pBindData - Pipline bind data groups.
+     * @param pInstanceCount - Instance count.
+     */
+    public addStep(pPipeline: VertexFragmentPipeline, pParameter: VertexParameter, pBindData: Record<number, BindDataGroup>, pInstanceCount: number = 1): void {
+        const lStep: VertexFragmentInstructionStep = {
+            pipeline: pPipeline,
+            parameter: pParameter,
+            instanceCount: pInstanceCount,
+            bindData: new Array<BindDataGroup | null>()
+        };
+
+        // Fill in data groups.
+        for (const lGroup of pPipeline.shader.pipelineLayout.groups) {
+            const lBindDataGroup: BindDataGroup | undefined = pBindData[lGroup];
+
+            // Validate bind data group.
+            if(!lBindDataGroup) {
+                throw new Exception('Defined bind data group not set.', this);
+            }
+
+            // Validate same layout bind layout.
+            const lBindGroupLayout = pPipeline.shader.pipelineLayout.getGroupLayout(lGroup);
+            if(lBindDataGroup.layout.identifier !== lBindGroupLayout.identifier){
+                throw new Exception('Source bind group layout does not match target layout.', this);
+            }
+
+            lStep.bindData[lGroup] = pBindData[lGroup];
+        }
+
+        this.mStepList.push(lStep);
+    }
+
+    /**
+     * Execute instruction.
+     */
+    public execute(): void {
+        this.device.generator.request<'vertexFragmentInstruction'>(this).execute();
+    }
 }
+
+type VertexFragmentInstructionStep = {
+    pipeline: VertexFragmentPipeline;
+    parameter: VertexParameter;
+    instanceCount: number;
+    bindData: Array<BindDataGroup | null>;
+};

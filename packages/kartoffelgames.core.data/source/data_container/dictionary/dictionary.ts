b6b821699b76339da5372d41d909b5375f04afd7
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-11 23:40:25 +0200

    Extend dictionary documentation. Prepare for auto generation.

--- packages/kartoffelgames.core.data/source/data_container/dictionary/dictionary.ts
+++ packages/kartoffelgames.core.data/source/data_container/dictionary/dictionary.ts
@@ -2,13 +2,33 @@
 import { Exception } from '../../exception/exception';
 
 /**
- * Default dictionary.
+ * Wrapper for {@link Map}.
+ * Extended by {@link Dictionary.add}, {@link Dictionary.getAllKeysOfValue}, {@link Dictionary.getOrDefault} and {@link Dictionary.map}.
+ * 
+ * @typeParam TKey - Type of objects defined for keys. 
+ * @typeParam TValue - Type of objects defined for values. 
+ * 
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map}
+ * 
+ * @public
  */
 export class Dictionary<TKey, TValue> extends Map<TKey, TValue> {
     /**
-     * Add value and key to dictionary.
+     * Add value and key to dictionary. 
+     * Throws {@link Exception}  for any added dublicate key. 
+     * 
      * @param pKey - Key of item.
      * @param pValue - value of item.
+     * 
+     * @throws
+     * On any dublicate key set,
+     * 
+     * @example
+     * ```TypeScript
+     * const dictionary = new Dictionary<string, number>();
+     * dictionary.add('a', 4); // => OK
+     * dictionary.add('a', 4); // => Fail: Dublicate key.
+     * ```
      */
     public add(pKey: TKey, pValue: TValue): void {
         // Add value and key to containers.
@@ -21,11 +41,24 @@ export class Dictionary<TKey, TValue> extends Map<TKey, TValue> {
 
     /**
      * Get all keys that have the set value.
+     * 
      * @param pValue - Value.
+     * 
+     * @returns all keys that hold the specified value.
+     * 
+     * @example
+     * ```TypeScript
+     * const dictionary = new Dictionary<string, number>();
+     * dictionary.set('a', 1);
+     * dictionary.set('b', 2);
+     * dictionary.set('c', 1);
+     * 
+     * const keys = dictionary.getAllKeysOfValue(1); // => ['a', 'c']
+     * ```
      */
     public getAllKeysOfValue(pValue: TValue): Array<TKey> {
         // Add entires iterator to list and filter for pValue = Value
-        const lKeyValuesWithValue: Array<[TKey, TValue]> = List.newListWith(...this.entries()).filter((pItem: [TKey, TValue]) => {
+        const lKeyValuesWithValue: Array<[TKey, TValue]> = [...this.entries()].filter((pItem: [TKey, TValue]) => {
             return pItem[1] === pValue;
         });
 
@@ -38,23 +71,46 @@ export class Dictionary<TKey, TValue> extends Map<TKey, TValue> {
     }
 
     /**
-     * Get item of dictionary. If key does not exists the default value gets returned.
-     * @param pKey - key of item.
+     * Get item. If the key does not exists the default value gets returned.
+     * @param pKey - Key of item.
      * @param pDefault - Default value if key was not found.
+     * 
+     * @returns value of the key. If the key does not exists the default value gets returned.
+     * 
+     * @example
+     * ```TypeScript
+     * const dictionary = new Dictionary<string, number>();
+     * dictionary.set('a', 1);
+     * 
+     * const keyA = dictionary.getAllKeysOfValue('a', 22); // => 1
+     * const keyZ = dictionary.getAllKeysOfValue('z', 22); // => 22
+     * ```
      */
     public getOrDefault(pKey: TKey, pDefault: TValue): TValue {
         const lValue: TValue | undefined = this.get(pKey);
-        
         if (typeof lValue !== 'undefined') {
             return lValue;
-        } else {
-            return pDefault;
         }
+
+        return pDefault;
     }
 
     /**
      * Maps information into new list.
      * @param pFunction - Mapping funktion.
+     * 
+     * @typeParam T - Result type of mapping resolver function.
+     * 
+     * @returns mapped data for each item.
+     * 
+     * @example
+     * ```TypeScript
+     * const dictionary = new Dictionary<string, number>();
+     * dictionary.set('a', 1);
+     * dictionary.set('b', 2);
+     * 
+     * const list = dictionary.map((key, value) => value + 1); //  => [2, 3]
+     * ```
      */
     public map<T>(pFunction: (pKey: TKey, pValue: TValue) => T): Array<T> {
         const lResultList: List<T> = new List<T>();

e216b48011f38ca0a0a2396187253a292bacc90f
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-12 00:15:13 +0200

    Extend list documentation.

--- packages/kartoffelgames.core.data/readme.md
+++ packages/kartoffelgames.core.data/readme.md
@@ -5,44 +5,8 @@ This package consists mostly of helper classes and storage types, aimed at reduc
 ---
 
 * [Installation](#installation)
-* [Data storage](#data-storage)
-    * [Dictionary](#dictionary)
 
 ## Installation
 ```
 npm install @kartoffelgames/core.data
-```
-
-## Data Storage
-Hold generic data but mostly extends existing javascript types by new data access methods.
-
-### Dictionary
-Wrapper for a JavaScript [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map). 
-Extended by `add`, `getAllKeysOfValue`, `getOrDefault` and `map`.
-```TypeScript
-const dictionary = new Dictionary<string, number>();
-
-// Set example data.
-dictionary.set('a', 1);
-dictionary.set('b', 2);
-dictionary.set('c', 2);
-
-// add. Fails by adding dublicate keys.
-dictionary.add('d', 4); // => OK
-dictionary.add('a', 4); // => Fail: Dublicate key.
-
-// getAllKeysOfValue
-const keys = dictionary.getAllKeysOfValue(1);
-console.log(keys); => // Output => ['a', 'c']
-
-// getOrDefault
-const keyA = dictionary.getAllKeysOfValue('a', 22);
-console.log(keyA);  // Output => 1
-
-const keyZ = dictionary.getAllKeysOfValue('z', 22);
-console.log(keyZ);  // Output => 22
-
-// Map
-const list = dictionary.map((key, value) => value + 1);
-console.log(list);  // Output => [2, 3, 3]
 ```
--- packages/kartoffelgames.core.data/source/data_container/list/list.ts
+++ packages/kartoffelgames.core.data/source/data_container/list/list.ts
@@ -1,12 +1,26 @@
 import { ICloneable } from '../../interface/i-cloneable';
 
 /**
- * Extended array functionality.
+ * Wrapper for {@link Array}.
+ * 
+ * @typeParam T - Type of items of list. 
+ * 
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array}
+ * 
+ * @public
  */
 export class List<T> extends Array<T> implements ICloneable<List<T>> {
     /**
      * Create list and add items.
+     * Prevents spread operator with number arrays to initialize array with length instead of item.
+     * 
      * @param pItemList - Items.
+     * 
+     * @example
+     * ```TypeScript
+     * const newList = List.newListWith<number>(...[3]); // => [3] 
+     * const newListWrong = new List<number>(...[3]);    // => [undefined, undefined, undefined] 
+     * ```
      */
     public static newListWith<T>(...pItemList: Array<T>): List<T> {
         const lNewList: List<T> = new List<T>();
@@ -16,36 +30,71 @@ export class List<T> extends Array<T> implements ICloneable<List<T>> {
     }
 
     /**
-     * Clear list.
+     * Remove every item.
+     * 
+     * @example
+     * ```TypeScript
+     * const list = List.newListWith<number>(1, 2, 3);
+     * list.clear(); 
+     * 
+     * console.log(list.length); // => 0
+     * ```
      */
     public clear(): void {
         this.splice(0, this.length);
     }
 
     /**
-     * Copy first layer of object.
+     * Create new list and add same items.
+     * @see {@link ICloneable.clone}
+     * 
+     * @returns cloned list with shallow copied item refernces.
+     * 
+     * @example
+     * ```TypeScript
+     * const list = List.newListWith<object>(new Object());
+     * 
+     * const clone = list.clone();
+     * 
+     * const areSame = list === clone; // => False
+     * const itemSame = list[0] === list[0]; // => True
+     * ```
      */
     public clone(): List<T> {
         return List.newListWith(...this);
     }
 
     /**
-     * Distinct values inside list.
+     * Copy distinct values into new list.
+     * 
+     * @returns new list instance with only distinct values.
+     * 
+     * @example
+     * ```TypeScript
+     * const listWithDublicates = List.newListWith<number>(1, 1, 2, 3, 3);
+     * const distinctList = listWithDublicates.distinct(); // => [1, 2, 3]
+     * ```
      */
     public distinct(): List<T> {
-        const lSelf: this = this;
-
-        // Get all values where index is same index as first index of first appearance.
-        const lDistinctArray: Array<T> = this.filter((pValue, pIndex) => {
-            return lSelf.indexOf(pValue) === pIndex;
-        });
-
-        return List.newListWith(...lDistinctArray);
+        return List.newListWith(...new Set(this));
     }
 
     /**
-     * Check if arrays are the same.
-     * @param pArray - Array.
+     * Compares this array with the specified one.
+     * Compares length and every item by reference and order.
+     * Does only shallow compare item references.
+     * 
+     * @param pArray - Array to compare.
+     * 
+     * @returns true for equality.
+     * 
+     * @example
+     * ```TypeScript
+     * const list = List.newListWith<number>(1, 3, 2);
+     * 
+     * const isEqual = list.equals([1, 3, 2]) // => True
+     * const isUnequal = list.equals([1, 2, 3]) // => False
+     * ```
      */
     public equals(pArray: Array<unknown>): boolean {
         // Check if array are same, dont null and have same length.
@@ -66,8 +115,19 @@ export class List<T> extends Array<T> implements ICloneable<List<T>> {
     }
 
     /**
-     * Removes the first appearence of value.
-     * @param pValue - value requested for removement.
+     * Removes the first appearence of a value.
+     * 
+     * @param pValue - Target value to remove.
+     * 
+     * @returns removed element. When no element was removed, undefined is returned instead.
+     * 
+     * @example 
+     * ```TypeScript
+     * const list = List.newListWith<number>(1, 3, 2);
+     * 
+     * const removedElement = list.remove(1); // => 1
+     * const noneExistingElement = list.remove(4); // => undefined
+     * ```
      */
     public remove(pValue: T): T | undefined {
         const lFoundIndex: number = this.indexOf(pValue);
@@ -82,8 +142,21 @@ export class List<T> extends Array<T> implements ICloneable<List<T>> {
 
     /**
      * Replace first appearence of value.
+     * 
      * @param pOldValue - Target value to replace.
      * @param pNewValue - Replacement value.
+     * 
+     * @returns replaced element. When no element was replaced, undefined is returned instead.
+     * 
+     * @example 
+     * ```TypeScript
+     * const list = List.newListWith<number>(1, 5, 3);
+     * 
+     * const removedElement = list.replace(5, 2); // => 5
+     * const noneExistingElement = list.replace(4, 3); // => undefined
+     * 
+     * console.log(list); // => [1, 2, 3]
+     * ```
      */
     public replace(pOldValue: T, pNewValue: T): T | undefined {
         const lFoundIndex: number = this.indexOf(pOldValue);
@@ -101,7 +174,16 @@ export class List<T> extends Array<T> implements ICloneable<List<T>> {
     }
 
     /**
-     * List to string.
+     * Returns a string representation of this list.
+     * @override base objects {@link Array.toString}
+     * 
+     * @returns string representation for this list.
+     * 
+     * @example 
+     * ```TypeScript
+     * const list = List.newListWith<number>(1, 2, 3);
+     * console.log(list.toString()); // => [1, 2, 3]
+     * ```
      */
     public override toString(): string {
         return `[${super.join(', ')}]`;

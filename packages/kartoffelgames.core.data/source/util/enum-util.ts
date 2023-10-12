9ec818c95df7f239db13423e7c7176a96feb460b
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-12 10:10:43 +0200

    Extended documentation for enum util.

--- packages/kartoffelgames.core.data/source/util/enum-util.ts
+++ packages/kartoffelgames.core.data/source/util/enum-util.ts
@@ -1,25 +1,55 @@
 /**
- * Enumaration helper.
+ * Static helper type to read data from enum objects.
+ * 
+ * @public
  */
 export class EnumUtil {
     /**
      * Return all keys of an enum as array.
+     * 
      * @param pEnum - typeof Enum object.
+     * 
+     * @returns All enum key as array in defined order.
+     * 
+     * @example Read enum names from custom enum object.
+     * ``` Typescript
+     * enum MyEnum {
+     *     Entry1 = 1,
+     *     Entry2 = 2
+     * }
+     * 
+     * const enumNames = EnumUtil.enumNamesToArray(MyEnum); // => ['Entry1', 'Entry2']
+     * ```
      */
-    public static enumNamesToArray(pEnum: object): Array<string> {
+    public static namesOf(pEnum: object): Array<string> {
         // Convert enum to key array.
         return Object.keys(pEnum).filter((pKey) => isNaN(Number(pKey)));
     }
 
     /**
      * Return all values of an enum as array.
+     * 
      * @param pEnum - typeof Enum object.
+     * 
+     * @typeParam T - Enum value type.
+     * 
+     * @returns All enum values as array in defined order.
+     * 
+     * @example Read enum names from custom enum object.
+     * ``` Typescript
+     * enum MyEnum {
+     *     Entry1 = 1,
+     *     Entry2 = 2
+     * }
+     * 
+     * const enumValues = EnumUtil.enumValuesToArray(MyEnum); // => [1, 2]
+     * ```
      */
-    public static enumValuesToArray<T>(pEnum: object): Array<T> {
+    public static valuesOf<T>(pEnum: object): Array<T> {
         const lEnumValues: Array<T> = new Array<T>();
 
         // Convert enum to vaue array by iterating over all keys.
-        for (const lKey of EnumUtil.enumNamesToArray(pEnum)) {
+        for (const lKey of EnumUtil.namesOf(pEnum)) {
             lEnumValues.push((<{ [key: string]: T; }>pEnum)[lKey]);
         }
 

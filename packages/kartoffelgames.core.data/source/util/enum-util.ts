20019f1b125234ca0d60796cc7181a98b82a9380
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-12 11:06:35 +0200

    Add cast and exists to enum util.

--- packages/kartoffelgames.core.data/source/util/enum-util.ts
+++ packages/kartoffelgames.core.data/source/util/enum-util.ts
@@ -5,6 +5,62 @@
  */
 export class EnumUtil {
     /**
+     * Infers value into enum type.
+     * When the values can not be infered into the specified enum, undefined is returned.
+     * 
+     * @param pEnum - typeof Enum object.
+     * @param pValue - Value of enum.
+     * 
+     * @typeParam T - Enum type the value should be infered into.
+     * 
+     * @returns Infered `pValue` parameter or undefined when the value does not exists as the enums value.
+     * 
+     * @example Try to cast two possible enum values.
+     * ``` Typescript
+     * enum MyEnum {
+     *     Entry1 = 1,
+     *     Entry2 = 2
+     * }
+     * 
+     * const existingValue = EnumUtil.cast<MyEnum>(MyEnum, 1); // => MyEnum.Entry1
+     * const noneExistingValue = EnumUtil.cast<MyEnum>(MyEnum, 5); // => undefined
+     * ```
+     */
+    public static cast<T>(pEnum: object, pValue: any): T | undefined {
+        // Thats it... :)
+        if (EnumUtil.exists<T>(pEnum, pValue)) {
+            return pValue;
+        } else {
+            return undefined;
+        }
+    }
+
+    /**
+     * Check value existence on a enum object.
+     * Infers `pValue` parameter as enum type.
+     * @param pEnum - typeof Enum object.
+     * @param pValue - Value of enum.
+     * 
+     * @typeParam T - Enum type the value should be infered into.
+     * 
+     * @returns True when the value can be casted into enum.
+     * 
+     * @example Check existence of one two possible enum values.
+     * ``` Typescript
+     * enum MyEnum {
+     *     Entry1 = 1,
+     *     Entry2 = 2
+     * }
+     * 
+     * const existingValue = EnumUtil.exists(MyEnum, 1); // => True
+     * const noneExistingValue = EnumUtil.exists(MyEnum, 5); // => False
+     * ```
+     */
+    public static exists<T>(pEnum: object, pValue: any): pValue is T {
+        return EnumUtil.valuesOf(pEnum).includes(pValue);
+    }
+
+    /**
      * Return all keys of an enum as array.
      * 
      * @param pEnum - typeof Enum object.
@@ -21,10 +77,8 @@ export class EnumUtil {
      *     Entry2 = 2
      * }
      * 
-     * const enumNames = EnumUtil.enumNamesToArray(MyEnum); // => ['Entry1', 'Entry2']
+     * const enumNames = EnumUtil.namesOf(MyEnum); // => ['Entry1', 'Entry2']
      * ```
-     * 
-     * @experimental @alpha
      */
     public static namesOf(pEnum: object): Array<string> {
         // Convert enum to key array.
@@ -40,20 +94,15 @@ export class EnumUtil {
      * 
      * @returns All enum values as array in defined order.
      * 
-     * @remarks
-     * Does only work for number enums and should fail for mixed or string enums.
-     * 
-     * @example Read enum names from custom enum object.
+     * @example Read enum values from custom enum object.
      * ``` Typescript
      * enum MyEnum {
      *     Entry1 = 1,
      *     Entry2 = 2
      * }
      * 
-     * const enumValues = EnumUtil.enumValuesToArray(MyEnum); // => [1, 2]
+     * const enumValues = EnumUtil.valuesOf(MyEnum); // => [1, 2]
      * ```
-     * 
-     * @experimental @alpha
      */
     public static valuesOf<T>(pEnum: object): Array<T> {
         const lEnumValues: Array<T> = new Array<T>();

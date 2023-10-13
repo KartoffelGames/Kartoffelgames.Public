c26c5dc9d2a28628ba1d240348aecb15698cfd9a
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-14 00:31:44 +0200

    Add code parser root node.

--- packages/kartoffelgames.core.parser/source/code-parser.ts
+++ packages/kartoffelgames.core.parser/source/code-parser.ts
@@ -1,7 +1,19 @@
+import { GrammarRootNode } from './graph/grammar-root-node';
 import { Lexer } from './lexer';
 
-export class CodeParser<TTokenType> {
+export class CodeParser<TTokenType, TParseResult> {
     private readonly mLexer: Lexer<TTokenType>;
+    private readonly mRootNode: GrammarRootNode<TTokenType>;
+
+    /**
+     * Get parser grammar root node.
+     * 
+     * @remarks
+     * Any graph resets to this point after reaching an end node.
+     */
+    public get rootNode(): GrammarRootNode<TTokenType>{
+        return this.mRootNode;
+    }
 
     /**
      * Constructor.
@@ -9,5 +21,16 @@ export class CodeParser<TTokenType> {
      */
     public constructor(pLexer: Lexer<TTokenType>){
         this.mLexer = pLexer;
+        this.mRootNode = new GrammarRootNode<TTokenType>();
+    }
+
+    public parse(pCodeText: string): TParseResult {
+        let lCurrentNode = this.rootNode;
+
+        for(const lNextToken of this.mLexer.tokenize(pCodeText)){
+            // TODO: Process token.
+        }
+
+        return <any>null;
     }
 }

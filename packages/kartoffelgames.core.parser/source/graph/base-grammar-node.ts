4f9da8293b667006e276671700c69905c42cfde9
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-10-18 00:03:42 +0200

    Set placeholder result types for chain methods.

--- packages/kartoffelgames.core.parser/source/graph/base-grammar-node.ts
+++ packages/kartoffelgames.core.parser/source/graph/base-grammar-node.ts
@@ -1,3 +1,9 @@
+import { GrammarEndNode } from './chain_nodes/grammar-end-node';
+import { GrammarLoopNode } from './chain_nodes/grammar-loop-node';
+import { GrammarOptionalNode } from './chain_nodes/grammar-optional-node';
+import { GrammarOrNode } from './chain_nodes/grammar-or-node';
+import { GrammarThenNode } from './chain_nodes/grammar-then-node';
+import { GrammarTrunkNode } from './chain_nodes/grammar-trunk-node';
 import { GrammarNodeType } from './grammar-node-type.enum';
 
 export abstract class BaseGrammarNode<TTokenType> {
@@ -26,27 +32,27 @@ export abstract class BaseGrammarNode<TTokenType> {
         this.mChainedNode = null;
     }
 
-    public loop(pPath?: SingleTokenPath<TTokenType>) {
+    public loop(pPath?: SingleTokenPath<TTokenType>): GrammarLoopNode<TTokenType> {
 
     }
 
-    public optional(pPath?: SingleTokenPath<TTokenType>) {
+    public optional(pPath?: SingleTokenPath<TTokenType>): GrammarOptionalNode<TTokenType> {
 
     }
 
-    public or(pPath: MultiTokenPath<TTokenType>) {
+    public or(pPath: MultiTokenPath<TTokenType>): GrammarOrNode<TTokenType> {
 
     }
 
-    public then(pPath?: SingleTokenPath<TTokenType>) {
+    public then(pPath?: SingleTokenPath<TTokenType>): GrammarThenNode<TTokenType> {
 
     }
 
-    public end(pResultName: string, pResultCollector: any) {
+    public end(pPath?: SingleTokenPath<TTokenType>, pResultName: string, pResultCollector: any): GrammarEndNode<TTokenType> {
 
     }
 
-    public trunk() {
+    public trunk(): GrammarTrunkNode<TTokenType> {
 
     }
 

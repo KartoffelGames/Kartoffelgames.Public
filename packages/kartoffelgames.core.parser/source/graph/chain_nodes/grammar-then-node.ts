90ae349c578e751ca8454dee5af81ea49a62d1b0
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2023-10-18 00:04:18 +0200

    Placeholder then node logic.

--- packages/kartoffelgames.core.parser/source/graph/chain_nodes/grammar-then-node.ts
+++ packages/kartoffelgames.core.parser/source/graph/chain_nodes/grammar-then-node.ts
@@ -8,4 +8,26 @@ export class GrammarThenNode<TTokenType> extends BaseGrammarNode<TTokenType>{
     public get type(): GrammarNodeType {
         return GrammarNodeType.Then;
     }
+
+    /**
+     * Retrieve chained node but only when it has the specified token type. 
+     * 
+     * @param pToken - The token type the next node should have.
+     * 
+     * @returns The next chained node with the specified node type. 
+     * When nothing is changed or the node type does not match, null is returned.
+     */
+    public override retrieveNextFor(pToken: TTokenType): BaseGrammarNode<TTokenType> | null {
+        // No chained set.
+        if(!this.chainedNode) {
+            return null;
+        }
+
+        // Validate chained node type.
+        if(this.chainedNode.tokenType !== pToken){
+            return null;
+        }
+
+        return this.chainedNode;
+    }
 }

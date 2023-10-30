a609e8b087488eaaa1278e0e7d91c54ab69cc166
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-10-30 02:09:26 +0100

    Single node prototype with token type as value.

--- packages/kartoffelgames.core.parser/source/graph/grammer-single-node.ts
+++ packages/kartoffelgames.core.parser/source/graph/grammer-single-node.ts
@@ -1,95 +1,31 @@
-import { Stack } from '@kartoffelgames/core.data';
-import { BaseGrammarNode } from './base-grammar-node';
-import { GraphPartReference } from './graph-part-reference';
-
-// TODO: VERY BIG. Is there a way to prevent stack cloning. FUgging slow.
+import { Exception } from '@kartoffelgames/core.data';
+import { BaseGrammarNode, GrammarGrapthValue } from './base-grammar-node';
 
 export class GrammarSingleNode<TTokenType extends string> extends BaseGrammarNode<TTokenType> {
-    private readonly mIdentifier: string | null;
-    private readonly mNodeValue: TTokenType | GraphPartReference<TTokenType>;
-    private readonly mOptional: boolean;
+    private readonly mNodeValue: TTokenType;
 
-    public constructor(pNodeValue: TTokenType | GraphPartReference<TTokenType>, pOptional: boolean, pIdentifier: string | null) {
-        super();
+    
+    public constructor(pPreviousNode: BaseGrammarNode<TTokenType> | null, pNodeValue: TTokenType, pOptional: boolean, pIdentifier: string | null) {
+        super(pPreviousNode, pOptional, pIdentifier);
 
-        this.mOptional = pOptional;
         this.mNodeValue = pNodeValue;
-        this.mIdentifier = pIdentifier;
     }
 
-    public override retrieveNext(pToken: TTokenType, pParentStack: Stack<BaseGrammarNode<TTokenType>>, pRequestingNode: BaseGrammarNode<TTokenType> | null): BaseGrammarNode<TTokenType>[] {
-        throw new Error('Method not implemented.');
+    public override next(pRevisited: boolean): Array<GrammarGrapthValue<TTokenType>> {
+        // Restrict revisiting.
+        if (pRevisited) {
+            throw new Exception('Single node can not be visited again. Node holds no inner branch.', this);
+        }
 
-        // TODO: Get all nodes that are available after this node. NextNode, ChildGraph... and check if pToken is available for next token::validTokens
-        // TODO: How to skip optional or void nodes???
+        return [this.nextNode];
     }
 
     /**
-     * Get valid tokens for this node. Takes the current route into account.\
-     * 
-     * When the last node of a child graph requests:\
-     * This node is skipped and the valid types of the {@link GrammarSingleNode.nextNode} are returned.\
-     * 
-     * When {@link GrammarSingleNode.previousNode} or {@link pParentNode} requests:\
-     * The current node value or when the node value is a {@link GraphPartReference}, then the valid token of the child graph is returned. 
-     * 
-     * When this node is an optional node:\
-     * All valid types for this node and all types of {@link GrammarSingleNode.nextNode} are returned.\
-     * 
-     * @param pParentStack - Parent nodes that host this nodes as node data. Stack includes parent of parent node. Not the nodes that is chained before this node. 
-     * @param pRequestingNode - Node that requests this information. 
+     * Get valid tokens for this node.
      * 
      * @returns All valid token types for this node.
      */
-    public override validTokens(pParentStack: Stack<BaseGrammarNode<TTokenType>>, pRequestingNode: BaseGrammarNode<TTokenType> | null): Array<TTokenType> {
-        // When requesting node is part of mNodeValue::GraphPartReference return only from nextNode.
-        if (typeof this.mNodeValue !== 'string' && pRequestingNode && this.mNodeValue.graph().branchRoot === pRequestingNode.branchRoot) {
-            return this.nextNodeValidToken(this.nextNode, pParentStack);
-        }
-
-        const lValidTokenList: Array<TTokenType> = new Array<TTokenType>();
-
-        // Get starting token type of child graph..
-        if (typeof this.mNodeValue !== 'string') {
-            // Clone parent stack, to prevent altering original data, and add current node as new parent layer.
-            const lLocalParentStack: Stack<BaseGrammarNode<TTokenType>> = pParentStack.clone();
-            lLocalParentStack.push(this);
-
-            lValidTokenList.push(...this.mNodeValue.graph().validTokens(lLocalParentStack, this));
-        } else {
-            // Get token type this node.
-            lValidTokenList.push(this.mNodeValue);
-        }
-
-        // Add next nodes types when token is optional.
-        if (this.mOptional) {
-            lValidTokenList.push(...this.nextNodeValidToken(this.nextNode, pParentStack));
-        }
-
-        return lValidTokenList;
-    }
-
-    /**
-     * Get all valid types of the {@link GrammarSingleNode.nextNode}  or inner {@link GraphPartReference}.
-     * 
-     * @param pNextNode - Value of {@link GrammarSingleNode.nextNode} 
-     * @param pParentStack - Parent nodes that host this nodes as node data. Stack includes parent of parent node. Not the nodes that is chained before this node. 
-     * 
-     * @returns All valid types of the {@link GrammarSingleNode.nextNode}  or inner {@link GraphPartReference}
-     */
-    private nextNodeValidToken(pNextNode: BaseGrammarNode<TTokenType> | null, pParentStack: Stack<BaseGrammarNode<TTokenType>>): Array<TTokenType> {
-        const lValidTokenList: Array<TTokenType> = new Array<TTokenType>();
-
-        if (pNextNode) {
-            lValidTokenList.push(...pNextNode.validTokens(pParentStack, this));
-        } else if (pParentStack.top) {
-            // Clone parent stack, to prevent altering original data, and pop current parent.
-            const lLocalParentStack: Stack<BaseGrammarNode<TTokenType>> = pParentStack.clone();
-            const lParentNode: BaseGrammarNode<TTokenType> = lLocalParentStack.pop()!;
-
-            lParentNode.validTokens(lLocalParentStack, this);
-        }
-
-        return lValidTokenList;
+    public override validTokens(): Array<TTokenType> {
+        return [this.mNodeValue];
     }
 }

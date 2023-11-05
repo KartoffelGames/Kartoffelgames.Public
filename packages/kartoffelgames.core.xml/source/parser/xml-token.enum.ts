1435766ac64f884b0461f5348214fe1a533a7fda
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2023-11-05 04:21:20 +0100

    Rework base xml parser with the code parser. Does not work at the moment.

---
+++ packages/kartoffelgames.core.xml/source/parser/xml-token.enum.ts
@@ -0,0 +1,11 @@
+export enum XmlToken {
+    OpenBracket = 'Open braket',
+    CloseBracket = 'Close braket',
+    Comment = 'Comment',
+    OpenClosingBracket = 'Open closing braket',
+    CloseClosingBracket = 'Close closing braket',
+    Identifier = 'Identifier',
+    Value = 'Value',
+    Assignment = 'Assignment',
+    NamespaceDelimiter = 'Namespace delimiter'
+}

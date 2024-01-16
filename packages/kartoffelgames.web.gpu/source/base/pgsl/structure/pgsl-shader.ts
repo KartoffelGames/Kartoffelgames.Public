78fceebc96b1156eb35597e6f9e354b7bea3f55c
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-01-17 00:11:19 +0100

    Pgsl shader root structures.

--- packages/kartoffelgames.web.gpu/source/base/pgsl/structure/pgsl-shader.ts
+++ packages/kartoffelgames.web.gpu/source/base/pgsl/structure/pgsl-shader.ts
@@ -1,6 +1,13 @@
 import { Dictionary } from '@kartoffelgames/core.data';
+import { PgslFunction } from './pgsl-function';
+import { PgslVariable } from './pgsl-variable';
 import { PgslStruct } from './type/pgsl-struct';
 
 export class PgslShader {
-    public readonly mStructs: Dictionary<string, PgslStruct>;
-}
+    // Values
+    private readonly mGlobals: Dictionary<string, PgslVariable>;
+    private readonly mStructs: Dictionary<string, PgslStruct>;
+    private readonly mFunctions: Dictionary<string, PgslFunction>;
+
+    
+}       

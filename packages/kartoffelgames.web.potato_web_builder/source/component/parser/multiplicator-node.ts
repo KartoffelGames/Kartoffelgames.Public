f8936455f369fcbc730f8c4975315894b389f060
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-02-26 22:45:46 +0100

    Multiplicator node from xmlElement but wihout working namespaces.

--- packages/kartoffelgames.web.potato_web_builder/source/component/parser/multiplicator-node.ts
+++ packages/kartoffelgames.web.potato_web_builder/source/component/parser/multiplicator-node.ts
@@ -1,4 +1,4 @@
-import { BaseXmlNode } from '@kartoffelgames/core.xml';
+import { XmlElement } from '@kartoffelgames/core.xml';
 
 /**
  * Multiplicator node.
@@ -12,82 +12,22 @@ import { BaseXmlNode } from '@kartoffelgames/core.xml';
  * [/MultiType]
  * ```
  */
-export class MultiplicatorNode extends BaseXmlNode {
-    private mInstruction: string;
-    private mTypeName: string;
-    
+export class MultiplicatorNode extends XmlElement {
     /**
-     * Get default namespace.
+     * Get namespace prefix of xml node.
+     * Does allways return null and does nothing on set.
      */
-    public get defaultNamespace(): string | null {
-        // Get parent mapping.
+    public override get namespacePrefix(): string | null {
+        return null;
+    } override set namespacePrefix(_pNamespacePrefix: string | null) {
+        // Does nothing.
+    }
+
+    /**
+     * Namespaces not supported for this xml element.
+     * Does allways return parent namespace or null when no parent is provided.
+     */
+    public override getNamespace(_pPrefix: string | null = null): string | null {
         return this.parent?.defaultNamespace ?? null;
     }
-
-    /**
-     * Multiplicator nodes instruction.
-     */
-    public get instruction(): string {
-        return this.mInstruction;
-    } set instruction(pValue: string) {
-        this.mInstruction = pValue;
-    }
-
-    /**
-     * Nodes type.
-     */
-    public get typeName(): string {
-        return this.mTypeName;
-    } set typeName(pValue: string) {
-        this.mTypeName = pValue;
-    }
-
-    /**
-     * Constructor.
-     */
-    public constructor() {
-        super();
-
-        this.mTypeName = '';
-        this.mInstruction = '';
-    }
-
-    /**
-     * Clone multiplicator node.
-     * 
-     * @returns new instance of current node with the same data.
-     */
-    override clone(): BaseXmlNode {
-        const lClone: MultiplicatorNode = new MultiplicatorNode();
-        lClone.instruction = this.instruction;
-        lClone.typeName = this.typeName;
-
-        return lClone;
-    }
-
-    /**
-     * Compare current node with another one.
-     * @param pBaseNode - Base xml node.
-     * 
-     * @returns if the set node is equal to this node.
-     */
-    override equals(pBaseNode: BaseXmlNode): boolean {
-        // Check type, tagname, namespace and namespace prefix.
-        if (!(pBaseNode instanceof MultiplicatorNode)) {
-            return false;
-        }
-
-        // Same base type.
-        if (this.mTypeName !== pBaseNode.mTypeName) {
-            return false;
-        }
-
-        // Same instruction set.
-        if (this.mInstruction !== pBaseNode.mInstruction) {
-            return false;
-        }
-
-        return true;
-    }
-
 }

bafd823e71c0240bde78d8e9fb1611df06812775
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-05-25 02:28:44 +0200

    Update interaction zone type exports.

--- packages/kartoffelgames.web.change_detection/source/index.ts
+++ packages/kartoffelgames.web.change_detection/source/index.ts
@@ -1,8 +1,11 @@
 /* istanbul ignore file */
 
 export { CompareHandler } from './comparison/compare-handler';
+
+// Interaction zone.
 export { InteractionZone } from './change_detection/interaction-zone';
 export { InteractionReason } from './change_detection/interaction-reason';
+export { InteractionResponseType } from './change_detection/enum/interaction-response-type.enum';
 
 // Difference search
 export { MyersDiff, HistoryItem, ChangeState } from './comparison/myers-diff';

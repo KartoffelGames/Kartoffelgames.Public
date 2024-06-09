4aaa0fb40c1160b812358fc2bded4b67f64a216f
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-06-09 14:47:25 +0200

    Export IgnoreInteractionDetection decorator.

--- packages/kartoffelgames.web.change_detection/source/index.ts
+++ packages/kartoffelgames.web.change_detection/source/index.ts
@@ -1,11 +1,14 @@
 /* istanbul ignore file */
 
+
+
 export { CompareHandler } from './comparison/compare-handler';
 
 // Interaction zone.
 export { InteractionZone } from './change_detection/interaction-zone';
 export { InteractionReason } from './change_detection/interaction-reason';
 export { InteractionResponseType } from './change_detection/enum/interaction-response-type.enum';
+export { IgnoreInteractionDetection } from './change_detection/synchron_tracker/ignore-interaction-detection.decorator';
 
 // Difference search
 export { MyersDiff, HistoryItem, ChangeState } from './comparison/myers-diff';

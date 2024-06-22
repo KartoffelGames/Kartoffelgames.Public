6506fbe63d619559423dc356f82cf502f6f4ddda
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-06-22 03:33:26 +0200

    Remove ModuleTargetNodeReference from instruction modules.

--- packages/kartoffelgames.web.potato_web_builder/source/core/module/instruction_module/instruction-module.ts
+++ packages/kartoffelgames.web.potato_web_builder/source/core/module/instruction_module/instruction-module.ts
@@ -1,10 +1,8 @@
 import { UpdateTrigger } from '../../../enum/update-trigger.enum';
-import { ElementCreator } from '../../component/element-creator';
 import { PwbTemplateInstructionNode } from '../../component/template/nodes/pwb-template-instruction-node';
 import { LayerValues } from '../../component/values/layer-values';
 import { ModuleKeyReference } from '../../injection-reference/module/module-key-reference';
 import { ModuleLayerValuesReference } from '../../injection-reference/module/module-layer-values-reference';
-import { ModuleTargetNodeReference } from '../../injection-reference/module/module-target-node-reference';
 import { ModuleTemplateReference } from '../../injection-reference/module/module-template-reference';
 import { ModuleValueReference } from '../../injection-reference/module/module-value-reference';
 import { BaseModule, BaseModuleConstructorParameter, IPwbModuleProcessor, IPwbModuleProcessorConstructor } from '../base-module';
@@ -33,7 +31,6 @@ export class InstructionModule extends BaseModule<IPwbInstructionModuleProcessor
 
         // Set processor attribute values from injection template.
         this.setProcessorAttributes(ModuleTemplateReference, pParameter.targetTemplate.clone());
-        this.setProcessorAttributes(ModuleTargetNodeReference, ElementCreator.createComment('')); // TODO: Remove after restriction set.
         this.setProcessorAttributes(ModuleLayerValuesReference, pParameter.values);
         this.setProcessorAttributes(ModuleKeyReference, pParameter.targetTemplate.instructionType);
         this.setProcessorAttributes(ModuleValueReference, pParameter.targetTemplate.instruction);

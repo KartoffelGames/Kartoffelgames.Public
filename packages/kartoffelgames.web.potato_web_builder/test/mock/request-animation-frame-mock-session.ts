61e58fd5061408f6331a75d707dfeb3422edcbb9
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-06-24 20:27:18 +0200

    Update animation frame mock.

--- packages/kartoffelgames.web.potato_web_builder/test/mock/request-animation-frame-mock-session.ts
+++ packages/kartoffelgames.web.potato_web_builder/test/mock/request-animation-frame-mock-session.ts
@@ -1,11 +1,36 @@
 class RequestAnimationFrameMockSession {
-    cancelAnimationFrame(pHandle: any) {
-        globalThis.clearTimeout(pHandle);
+    private mCurrentList: Map<number, FrameRequestCallback> = new Map<number, FrameRequestCallback>();
+    private mCurrentTimer: number | null = null;
+
+    cancelAnimationFrame(pIdentifier: number) {
+        this.mCurrentList.delete(pIdentifier);
     }
-    requestAnimationFrame(pCallback: any): number {
-        return <any>globalThis.setTimeout(() => {
-            pCallback();
-        }, 1);
+
+    requestAnimationFrame(pCallback: FrameRequestCallback): number {
+        const lIdentifier: number = Math.random();
+
+        // Add animation frame.
+        this.mCurrentList.set(lIdentifier, pCallback);
+
+        // Create new "frame" timer when not already set. 
+        if (this.mCurrentTimer === null) {
+            this.mCurrentTimer = <any>globalThis.setTimeout(() => {
+                // Save current "frame"-Handler and create a new list for the next "frame".
+                const lActiveList = this.mCurrentList;
+                this.mCurrentList = new Map<number, FrameRequestCallback>();
+
+                // Setup new timer.
+                this.mCurrentTimer = null;
+
+                const lCurrentTime: number = Date.now();
+
+                for (const lFunction of lActiveList.values()) {
+                    lFunction(lCurrentTime);
+                }
+            }, 1);
+        }
+
+        return lIdentifier;
     }
 }
 

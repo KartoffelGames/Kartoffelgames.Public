a873286b1e8259390cb2720f5f0ce8a1fc9b0c2c
Kartoffeleintopf <tetrodotoxinpotato@gmail.com>
2024-07-12 03:46:58 +0200

    Test multiple one way binding to same module.

--- packages/kartoffelgames.web.potato_web_builder/test/integration/module/one-way-binding-attribute-module.test.ts
+++ packages/kartoffelgames.web.potato_web_builder/test/integration/module/one-way-binding-attribute-module.test.ts
@@ -62,4 +62,79 @@ describe('OneWayBindingAttribute', () => {
         // Evaluation.
         expect(lViewValue).to.equal(lNewValue);
     });
+
+    it('Exchange value to child component', async () => {
+        // Setup. Define values.
+        const lValueOne: string = 'NEW__VALUE';
+
+        // Setup. Define component.
+        const lChildComponentSelector: string = TestUtil.randomSelector();
+        @PwbComponent({
+            selector: lChildComponentSelector
+        })
+        class TestChildComponent extends Processor {
+            @PwbExport
+            public valueOne!: string;
+        }
+
+        // Setup. Define component.
+        @PwbComponent({
+            selector: TestUtil.randomSelector(),
+            template: `<${lChildComponentSelector} [valueOne]="this.valueOneExternal"/>`
+        })
+        class TestComponent extends Processor {
+            public valueOneExternal: string = lValueOne;
+        }
+
+        // Setup. Create element.
+        const lComponent: HTMLElement & TestComponent = await <any>TestUtil.createComponent(TestComponent);
+        const lChildComponent: HTMLElement & TestChildComponent = TestUtil.getComponentNode(lComponent, lChildComponentSelector);
+
+        // Process.
+        const lExportOne: string = lChildComponent.valueOne;
+
+        // Evaluation.
+        expect(lExportOne).to.equal(lValueOne);
+    });
+
+    it('Exchange two values to to same component ', async () => {
+        // Setup. Define values.
+        const lValueOne: string = 'NEW__VALUE';
+        const lValueTwo: string = 'NEW__VALUE';
+
+        // Setup. Define component.
+        const lChildComponentSelector: string = TestUtil.randomSelector();
+        @PwbComponent({
+            selector: lChildComponentSelector
+        })
+        class TestChildComponent extends Processor {
+            @PwbExport
+            public valueOne!: string;
+
+            @PwbExport
+            public valueTwo!: string;
+        }
+
+        // Setup. Define component.
+        @PwbComponent({
+            selector: TestUtil.randomSelector(),
+            template: `<${lChildComponentSelector} [valueOne]="this.valueOneExternal" [valueTwo]="this.valueTwoExternal"/>`
+        })
+        class TestComponent extends Processor {
+            public valueOneExternal: string = lValueOne;
+            public valueTwoExternal: string = lValueTwo;
+        }
+
+        // Setup. Create element.
+        const lComponent: HTMLElement & TestComponent = await <any>TestUtil.createComponent(TestComponent);
+        const lChildComponent: HTMLElement & TestChildComponent = TestUtil.getComponentNode(lComponent, lChildComponentSelector);
+
+        // Process.
+        const lExportOne: string = lChildComponent.valueOne;
+        const lExportTwo: string = lChildComponent.valueTwo;
+
+        // Evaluation.
+        expect(lExportOne).to.equal(lValueOne);
+        expect(lExportTwo).to.equal(lValueTwo);
+    });
 });

6b496696eed1cd9629b8e0c1229c6725971b5f22
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-07-19 00:14:19 +0200

    Try to implement own version of w3 Template list discovery algorithm.

--- packages/kartoffelgames.web.gpu/source/pgsl/parser/pgsl-lexer.ts
+++ packages/kartoffelgames.web.gpu/source/pgsl/parser/pgsl-lexer.ts
@@ -1,5 +1,6 @@
 import { Lexer, LexerToken } from '@kartoffelgames/core.parser';
 import { PgslToken } from './pgsl-token.enum';
+import { Stack } from '@kartoffelgames/core';
 
 export class PgslLexer extends Lexer<PgslToken> {
 
@@ -50,8 +51,113 @@ export class PgslLexer extends Lexer<PgslToken> {
         this.addTokenTemplate('TemplateList', {
             pattern: {
                 start: {
-                    regex: /<(?![<=])(?=[^;:{}]*>)/, // TODO: a<b || b>c will be detected.
-                    type: PgslToken.TemplateListEnd
+                    regex: /(?<=([_\p{XID_Start}][\p{XID_Continue}]+)|([\p{XID_Start}]))<(?![<=])/u, // TODO: a<b || b>c will be detected.
+                    type: PgslToken.TemplateListEnd,
+                    validator: (pToken: LexerToken<PgslToken>, pText: string, pIndex: number): boolean => {
+                        // Init nexting stack.
+                        const lNestingStack: Stack<string> = new Stack<string>();
+
+                        const lPermittedCodePoints: Set<string> = new Set<string>([';', ':', '{', '}']);
+                        const lPermittedClosingCodePoint: Set<string> = new Set<string>(['>', '=']);
+                        const lNestingOpenCodePoints: Set<string> = new Set<string>(['[', '(']);
+                        const lNestingCloseCodePoints: Set<string> = new Set<string>([']', ')']);
+                        const lShortCircuits: Set<string> = new Set<string>(['&', '|']);
+                        const lTemplateListOpeningRegex: RegExp = /(?<=([_\p{XID_Start}][\p{XID_Continue}]+)|([\p{XID_Start}]))<(?![<=])/u;
+
+                        // Iterate each code point.
+                        let lCurrentTextIndex: number = pIndex + pToken.value.length;
+                        while (lCurrentTextIndex < pText.length) {
+                            const lCurrentCodePoint: string = pText[lCurrentTextIndex];
+
+                            // Branch for special code points..
+                            switch (true) {
+                                // When any ;:{ or } is between the closing > return false.
+                                case (lPermittedCodePoints.has(lCurrentCodePoint)): {
+                                    return false;
+                                }
+
+                                // Push nestings [, ( 
+                                case (lNestingOpenCodePoints.has(lCurrentCodePoint)): {
+                                    lNestingStack.push(lCurrentCodePoint);
+                                    break;
+                                }
+
+                                // Pop nested [, ( 
+                                case (lNestingCloseCodePoints.has(lCurrentCodePoint)): {
+                                    const lClosedNesting: string | undefined = lNestingStack.pop();
+                                    if (!lClosedNesting) {
+                                        return false;
+                                    }
+
+                                    // When closed nesting does not correspond the current pending nesting. 
+                                    if (lCurrentCodePoint === ')' && lClosedNesting !== '(' || lCurrentCodePoint === ']' && lClosedNesting !== '[') {
+                                        return false;
+                                    }
+
+                                    // No error. Continue search for closing.
+                                    break;
+                                }
+
+                                // Potential nested template list.
+                                case (lCurrentCodePoint === '<'): {
+                                    // TODO: We need nesting :( vec<array<i32>> Triggers early closing.
+
+                                    lTemplateListOpeningRegex.lastIndex = lCurrentTextIndex;
+                                    if(lTemplateListOpeningRegex.exec(pText)) {
+                                        // TODO: Test for something ??
+                                    }
+
+                                    break;
+                                }
+
+                                // Potential template list closing.
+                                case (lCurrentCodePoint === '>'): {
+
+                                    // Ignore it when it is not on the root nesting level.
+                                    if (lNestingStack.size > 0) {
+                                        break;
+                                    }
+
+                                    // When current  code point interferes with the next, advance this code point and continue searching. 
+                                    const lNextCodePoint: string = pText[lCurrentTextIndex + 1];
+                                    if (lPermittedClosingCodePoint.has(lNextCodePoint)) {
+                                        lCurrentTextIndex++;
+                                        break;
+                                    }
+
+                                    // Closing of current template list is found.
+                                    return true;
+                                }
+
+                                // Token can be a comparison.
+                                case (lShortCircuits.has(lCurrentCodePoint)): {
+                                    // Ignore when next code point is not the same char, as it is not a short circuit.
+                                    const lNextCodePoint: string = pText[lCurrentTextIndex + 1];
+                                    if (lNextCodePoint !== lCurrentCodePoint) {
+                                        break;
+                                    }
+
+                                    // When currently nested, it does not affect the root nesting.
+                                    if (lNestingStack.size > 0) {
+                                        // When current nesting is a template list, pop nesting, as the used template start code point was a comparison.
+                                        if (lNestingStack.top === '<') {
+                                            lNestingStack.pop();
+                                        }
+
+                                        break;
+                                    }
+
+                                    // The found template list start token is a comparison.
+                                    return false;
+                                }
+                            }
+
+                            // When nothing special happened, check next code point.
+                            lCurrentTextIndex++;
+                        }
+
+                        return true;
+                    }
                 },
                 end: {
                     regex: />/,

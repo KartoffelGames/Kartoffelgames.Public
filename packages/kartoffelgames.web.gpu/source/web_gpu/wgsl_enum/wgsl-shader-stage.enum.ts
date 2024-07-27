599a132d788041a6df739d4ae702e9031542952e
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-07-27 23:13:09 +0200

    Make it runable somehow.

--- packages/kartoffelgames.web.gpu/source/web_gpu/wgsl_enum/wgsl-shader-stage.enum.ts
+++ packages/kartoffelgames.web.gpu/source/web_gpu/wgsl_enum/wgsl-shader-stage.enum.ts
@@ -1,5 +1,5 @@
-export enum WgslShaderStage {
-    Fragment = GPUShaderStage.FRAGMENT,
-    Vertex = GPUShaderStage.VERTEX,
-    Compute = GPUShaderStage.COMPUTE,
+export enum WgslShaderStage { // TODO:
+    Fragment = 1, //GPUShaderStage.FRAGMENT,
+    Vertex = 2, //GPUShaderStage.VERTEX,
+    Compute = 3 //GPUShaderStage.COMPUTE,
 }

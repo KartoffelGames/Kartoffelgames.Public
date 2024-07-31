28a15ff400b796ef878669e72f8e441422f0070a
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-07-31 23:14:31 +0200

    Write pgsl declaration tests for all module scope variable declarations.

--- packages/kartoffelgames.web.gpu/test/pgsl/parser/pgsl-parser.test.ts
+++ packages/kartoffelgames.web.gpu/test/pgsl/parser/pgsl-parser.test.ts
@@ -1,3 +1,4 @@
+import { ParserException } from '@kartoffelgames/core.parser';
 import { expect } from 'chai';
 import { PgslParser } from 'packages/kartoffelgames.web.gpu/source/pgsl/parser/pgsl-parser';
 import { PgslDocument } from 'packages/kartoffelgames.web.gpu/source/pgsl/pgsl-document';
@@ -6,8 +7,6 @@ describe('PsglLexer', () => {
     const lPgslParser: PgslParser = new PgslParser();
 
     describe('-- Modulescope variable declarations', () => {
-        // TODO: Check if they can have a initial value.
-
         describe('-- const declaration', () => {
             it('-- Declaration with const value', () => {
                 // Setup.
@@ -34,6 +33,36 @@ describe('PsglLexer', () => {
 
                 // Evaluation. // TODO:
                 expect(lResult).to.be.true;
+            });
+
+            it('-- Reject const declaration without assignment', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    const myInt: int;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
+
+            it('-- Reject const declaration with attributes', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @attribute() const myInt: int = 1;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
             });
         });
 
@@ -78,10 +107,40 @@ describe('PsglLexer', () => {
                 // Evaluation. // TODO:
                 expect(lResult).to.be.true;
             });
+
+            it('-- Reject param declaration without assignment', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    param myInt: int;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
+
+            it('-- Reject param declaration with attributes', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @attribute() param myInt: int = 1;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
         });
 
-        describe('-- private declaration', () => { 
-            it('-- Declaration with const value', () => {
+        describe('-- private declaration', () => {
+            it('-- Declaration with const value assignment', () => {
                 // Setup.
                 const lSourceCode: string = `
                     private myInt: int = 10;
@@ -94,7 +153,20 @@ describe('PsglLexer', () => {
                 expect(lResult).to.be.true;
             });
 
-            it('-- Declaration with expression value', () => {
+            it('-- Declaration without assignment', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    private myInt: int;
+                `;
+
+                // Process.
+                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+
+                // Evaluation. // TODO:
+                expect(lResult).to.be.true;
+            });
+
+            it('-- Declaration with expression value assignment', () => {
                 // Setup.
                 const lSourceCode: string = `
                     private myInt: int = 10 + 11;
@@ -106,13 +178,28 @@ describe('PsglLexer', () => {
                 // Evaluation. // TODO:
                 expect(lResult).to.be.true;
             });
+
+            it('-- Reject private declaration with attributes', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @attribute() private myInt: int;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
         });
 
         describe('-- workgroup declaration', () => { // TODO: Better naming
-            it('-- Declaration with const value', () => {
+            it('-- Declaration with expression value', () => {
                 // Setup.
                 const lSourceCode: string = `
-                    workgroup myInt: int = 10;
+                    workgroup myInt: int;
                 `;
 
                 // Process.
@@ -122,17 +209,34 @@ describe('PsglLexer', () => {
                 expect(lResult).to.be.true;
             });
 
-            it('-- Declaration with expression value', () => {
+            it('-- Reject workgroup declaration with assignment', () => {
                 // Setup.
                 const lSourceCode: string = `
-                    workgroup myInt: int = 10 + 11;
+                    workgroup myInt: int = 10;
                 `;
 
                 // Process.
-                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
 
-                // Evaluation. // TODO:
-                expect(lResult).to.be.true;
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
+
+            it('-- Reject workgroup declaration with attributes', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @attribute() workgroup myInt: int;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
             });
         });
 
@@ -150,49 +254,123 @@ describe('PsglLexer', () => {
                 expect(lResult).to.be.true;
             });
 
-            it('-- Declaration with expression value', () => {
+            it('-- Reject uniform declaration without binding', () => {
                 // Setup.
                 const lSourceCode: string = `
-                    uniform myInt: int = 10 + 11;
+                    uniform myInt: int;
                 `;
 
                 // Process.
-                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
 
-                // Evaluation. // TODO:
-                expect(lResult).to.be.true;
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
             });
-         });
 
-        describe('-- storage declaration', () => { 
-            // TODO: How to do read write access?
-            it('-- Declaration with const value', () => {
+            it('-- Reject storage declaration with assignment', () => {
                 // Setup.
                 const lSourceCode: string = `
-                    storage myInt: int = 10;
+                    @groupBind(x, y) uniform myInt: int = 10;
                 `;
 
                 // Process.
-                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
 
-                // Evaluation. // TODO:
-                expect(lResult).to.be.true;
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
             });
 
-            it('-- Declaration with expression value', () => {
-                // Setup.
-                const lSourceCode: string = `
-                    storage myInt: int = 10 + 11;
-                `;
-
-                // Process.
-                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
-
-                // Evaluation. // TODO:
-                expect(lResult).to.be.true;
-            });
+            // TODO: sampler and texture values.
         });
 
-        // TODO: handle ??? How
+        describe('-- storage declaration', () => {
+            it('-- Declaration with read access', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @groupBind(x, y) @accessMode(AccessMode.Read) storage myInt: int;
+                `;
+
+                // Process.
+                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+
+                // Evaluation. // TODO:
+                expect(lResult).to.be.true;
+            });
+
+            it('-- Declaration  with write access', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @groupBind(x, y) @accessMode(AccessMode.Write) storage myInt: int;
+                `;
+
+                // Process.
+                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+
+                // Evaluation. // TODO:
+                expect(lResult).to.be.true;
+            });
+
+            it('-- Declaration with read write access', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @groupBind(x, y) @accessMode(AccessMode.ReadWrite) storage myInt: int;
+                `;
+
+                // Process.
+                const lResult: PgslDocument = lPgslParser.parse(lSourceCode);
+
+                // Evaluation. // TODO:
+                expect(lResult).to.be.true;
+            });
+
+            it('-- Reject storage declaration without binding', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @accessMode(AccessMode.ReadWrite) storage myInt: int;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
+
+            it('-- Reject storage declaration without access mode', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @groupBind(x, y) storage myInt: int;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
+
+            it('-- Reject storage declaration with assignment', () => {
+                // Setup.
+                const lSourceCode: string = `
+                    @groupBind(x, y) @accessMode(AccessMode.ReadWrite) storage myInt: int = 10;
+                `;
+
+                // Process.
+                const lErrorFunction = () => {
+                    lPgslParser.parse(lSourceCode);
+                };
+
+                // Evaluation.
+                expect(lErrorFunction).to.throw(ParserException);
+            });
+        });
     });
 });

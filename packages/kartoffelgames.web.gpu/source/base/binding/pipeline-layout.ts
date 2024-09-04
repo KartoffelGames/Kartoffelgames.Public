f45b34928a0edfff1519a5f1d3fc4103482198c2
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-09-05 00:18:23 +0200

    Replacement group validation.

--- packages/kartoffelgames.web.gpu/source/base/binding/pipeline-layout.ts
+++ packages/kartoffelgames.web.gpu/source/base/binding/pipeline-layout.ts
@@ -2,29 +2,43 @@ import { Dictionary, Exception } from '@kartoffelgames/core';
 import { GpuDevice } from '../gpu/gpu-device';
 import { GpuNativeObject, NativeObjectLifeTime } from '../gpu/gpu-native-object';
 import { UpdateReason } from '../gpu/gpu-object-update-reason';
-import { BindGroupLayout } from './bind-group-layout';
+import { BindGroupLayout, BindLayout } from './bind-group-layout';
 
 export class PipelineLayout extends GpuNativeObject<GPUPipelineLayout> {
+    private readonly mBindGroupNames: Dictionary<string, number>;
     private readonly mBindGroups: Dictionary<number, BindGroupLayout>;
-
+    private readonly mInitialBindGroups: Dictionary<number, BindGroupLayout>;
 
     /**
-     * Bind group count.
+     * Bind group names.
      */
-    public get groups(): Array<number> {
-        return [...this.mBindGroups.keys()];
+    public get groups(): Array<string> {
+        return [...this.mBindGroupNames.keys()];
     }
 
     /**
      * Constructor.
      * 
      * @param pDevice - Gpu Device reference.
+     * @param pInitialGroups - Initial groups.
      */
-    public constructor(pDevice: GpuDevice) {
+    public constructor(pDevice: GpuDevice, pInitialGroups: Dictionary<number, BindGroupLayout>) {
         super(pDevice, NativeObjectLifeTime.Persistent);
 
-        // Init storage.
+        // Init storages.
+        this.mBindGroupNames = new Dictionary<string, number>();
+        this.mInitialBindGroups = new Dictionary<number, BindGroupLayout>();
         this.mBindGroups = new Dictionary<number, BindGroupLayout>();
+
+        // Set initial work groups.
+        for (const [lGroupIndex, lGroup] of pInitialGroups) {
+            // Set name to index mapping.
+            this.mBindGroupNames.set(lGroup.name, lGroupIndex);
+
+            // Set bind groups to initial data and working bind group.
+            this.mInitialBindGroups.set(lGroupIndex, lGroup);
+            this.mBindGroups.set(lGroupIndex, lGroup);
+        }
 
         // TODO: Find a way to reuse groups instead of initializing a new every time.
         // TODO: Validate if replaced group meets at least the preset criteria. (Same, layout, visibility, bindings)
@@ -36,11 +50,83 @@ export class PipelineLayout extends GpuNativeObject<GPUPipelineLayout> {
     }
 
     /**
+     * Replace existing bind group.
+     * 
+     * @param pGroupName - Layout name that should be replaced.
+     * @param pBindGroup - Replacement bind group.
+     */
+    public replaceGroup(pGroupName: string, pBindGroup: BindGroupLayout): void {
+        const lGroupIndex: number | undefined = this.mBindGroupNames.get(pGroupName);
+
+        // Throw on unaccessable group.
+        if (typeof lGroupIndex === 'undefined') {
+            throw new Exception(`Bind group layout (${pGroupName}) does not exists.`, this);
+        }
+
+        // Read original bind group.
+        const lInitialGroup: BindGroupLayout | undefined = this.mInitialBindGroups.get(lGroupIndex);
+        if (!lInitialGroup) {
+            throw new Exception(`Only initial bind group layouts can be replaced.`, this);
+        }
+
+        const lInitialBindingList: Array<Readonly<BindLayout>> = pBindGroup.bindings;
+        const lReplacementBindingList: Array<Readonly<BindLayout>> = pBindGroup.bindings;
+
+        // Compare inital it with replacement to check compatibility.
+        if (lInitialBindingList.length !== lReplacementBindingList.length) {
+            throw new Exception(`Replacement group does not include all bindings.`, this);
+        }
+
+        // Compare all bindings.
+        for (let lBindingIndex: number = 0; lBindingIndex < lInitialBindingList.length; lBindingIndex++) {
+            const lInitialBinding: Readonly<BindLayout> | undefined = lInitialBindingList.at(lBindingIndex);
+            const lReplacementBinding: Readonly<BindLayout> | undefined = lReplacementBindingList.at(lBindingIndex);
+
+            // Continue on undefined or when bind layout is the same.
+            if (lInitialBinding === lReplacementBinding) {
+                continue;
+            }
+
+            // Can't set binding of something that is not there.
+            if (typeof lInitialBinding === 'undefined') {
+                throw new Exception(`Can't replace group binding with index "${lBindingIndex}". Layout binding does not exists in initial layout.`, this);
+            }
+
+            // Group must have the same bindings no binding can be missed.
+            if (typeof lReplacementBinding === 'undefined') {
+                throw new Exception(`Can't omit group binding with index "${lBindingIndex}".`, this);
+            }
+
+            // Same binding name.
+            if (lInitialBinding.name !== lReplacementBinding.name) {
+                throw new Exception(`Group binding replacement "${lReplacementBinding.name}" must be named "${lInitialBinding.name}"`, this);
+            }
+
+            // Must share the same access mode.
+            if ((lReplacementBinding.accessMode & lInitialBinding.accessMode) !== lReplacementBinding.accessMode) {
+                throw new Exception(`Group binding replacement "${lReplacementBinding.name}" must at least cover the initial access mode.`, this);
+            }
+
+            // Must share the same access mode.
+            if (lReplacementBinding.usage !== lInitialBinding.usage) {
+                throw new Exception(`Group binding replacement "${lReplacementBinding.name}" must be the initial usage.`, this);
+            }
+
+            // Must share the same visibility.
+            if ((lReplacementBinding.visibility & lInitialBinding.visibility) !== lReplacementBinding.visibility) {
+                throw new Exception(`Group binding replacement "${lReplacementBinding.name}" must at least cover the initial visibility.`, this);
+            }
+
+            // TODO: layout: BaseMemoryLayout;
+        }
+    }
+
+    /**
      * Create bind group.
      * @param pIndex - Group index.
      * @param pLayout - [Optional] Bind group Layout.
      */
-    public addGroupLayout(pIndex: number, pLayout: BindGroupLayout): void {
+    public setPlaceholderGroup(pIndex: number, pLayout: BindGroupLayout): void {
         this.mBindGroups.add(pIndex, pLayout);
 
         // Register change listener for layout changes.
@@ -53,39 +139,45 @@ export class PipelineLayout extends GpuNativeObject<GPUPipelineLayout> {
     }
 
     /**
-     * Get created bind group layout.
-     * @param pIndex - Group index.
+     * Get bind group layout by name.
+     * 
+     * @param pGroupName - Group name.
      */
-    public getGroupLayout(pIndex: number): BindGroupLayout {
+    public getGroupLayout(pGroupName: string): BindGroupLayout {
+        const lGroupIndex: number | undefined = this.mBindGroupNames.get(pGroupName);
+
         // Throw on unaccessable group.
-        if (!this.mBindGroups.has(pIndex)) {
-            throw new Exception(`Bind group layout (${pIndex}) does not exists.`, this);
+        if (typeof lGroupIndex === 'undefined') {
+            throw new Exception(`Bind group layout (${pGroupName}) does not exists.`, this);
         }
 
         // Bind group should allways exist.
-        return this.mBindGroups.get(pIndex)!;
+        return this.mBindGroups.get(lGroupIndex)!;
+    }
+
+    /**
+     * Destroy native object.
+     */
+    protected override destroy(): void {
+        // Nothing to destroy as it is only a descriptor object.
     }
 
     /**
      * Generate native gpu pipeline data layout.
      */
     protected override generate(): GPUPipelineLayout {
-        const lBindGoupIndices: Array<number> = this.gpuObject.groups;
-
         // Generate pipeline layout from bind group layouts.
         const lPipelineLayoutDescriptor = { bindGroupLayouts: new Array<GPUBindGroupLayout>() };
-        for (const lIndex of lBindGoupIndices) {
-            const lBindGroupLayout = this.gpuObject.getGroupLayout(lIndex);
-
-            lPipelineLayoutDescriptor.bindGroupLayouts[lIndex] = this.factory.request<'bindDataGroupLayout'>(lBindGroupLayout).create();
+        for (const [lGroupIndex, lBindGroupLayout] of this.mBindGroups) {
+            lPipelineLayoutDescriptor.bindGroupLayouts[lGroupIndex] = lBindGroupLayout.native;
         }
 
         // Validate continunity.
-        if (lBindGoupIndices.length !== lPipelineLayoutDescriptor.bindGroupLayouts.length) {
+        if (this.mBindGroups.size !== lPipelineLayoutDescriptor.bindGroupLayouts.length) {
             throw new Exception(`Bind group gap detected. Group not set.`, this);
         }
 
         // Generate pipeline layout from descriptor.
-        return this.factory.gpu.createPipelineLayout(lPipelineLayoutDescriptor);
+        return this.device.gpu.createPipelineLayout(lPipelineLayoutDescriptor);
     }
 }

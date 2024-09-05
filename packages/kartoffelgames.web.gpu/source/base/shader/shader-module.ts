444414574642ac1c3a61cc9889d46de259d74bd1
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-09-05 22:52:06 +0200

    Generate shader module pipeline initialisation values.

--- packages/kartoffelgames.web.gpu/source/base/shader/shader-module.ts
+++ packages/kartoffelgames.web.gpu/source/base/shader/shader-module.ts
@@ -1,14 +1,16 @@
-import { BindGroupLayout } from '../binding/bind-group-layout';
+import { Dictionary } from '@kartoffelgames/core';
+import { BindGroupLayout, BindLayout } from '../binding/bind-group-layout';
 import { PipelineLayout } from '../binding/pipeline-layout';
 import { GpuDevice } from '../gpu/gpu-device';
 import { GpuNativeObject, NativeObjectLifeTime } from '../gpu/gpu-native-object';
 import { ShaderLayout } from './shader-layout';
+import { PrimitiveBufferFormat } from '../memory_layout/buffer/enum/primitive-buffer-format.enum';
 
 export class ShaderModule extends GpuNativeObject<GPUShaderModule> {
+    private readonly mParameter: Dictionary<string, PrimitiveBufferFormat>;
     private readonly mPipelineLayout: PipelineLayout;
-    private readonly mShaderLayout: ShaderLayout;
     private readonly mSource: string;
-
+    
     /**
      * Shader pipeline layout.
      */
@@ -16,38 +18,57 @@ export class ShaderModule extends GpuNativeObject<GPUShaderModule> {
         return this.mPipelineLayout;
     }
 
-    // TODO: Entry points.
+    /**
+     * Shader pipeline parameters.
+     */
+    public get parameter(): Dictionary<string, PrimitiveBufferFormat> {
+        return this.mParameter;
+    }
 
-    // TODO: Parameter.
+    // TODO: Entry points.
 
     /**
      * Constructor.
      * @param pDevice - Gpu Device reference.
+     * @param pSource - Shader source as wgsl code.
+     * @param pLayout - Shader layout information.
      */
     public constructor(pDevice: GpuDevice, pSource: string, pLayout: ShaderLayout) {
         super(pDevice, NativeObjectLifeTime.Persistent);
 
         // Create shader information for source.
-        this.mShaderLayout = pLayout;
         this.mSource = pSource;
 
-        // Generate layout.
-        this.mPipelineLayout = new PipelineLayout(this.device);
-        for (const [lGroupIndex, lBindingList] of this.mShaderInformation.bindings) {
-            // Create group layout and add each binding.
-            let lGroupLayout: BindGroupLayout = new BindGroupLayout(this.device);
-            for (const lBinding of lBindingList) {
-                lGroupLayout.addBinding(lBinding, lBinding.name);
+        // Generate initial pipeline layout.
+        const lInitialPipelineLayout: Dictionary<number, BindGroupLayout> = new Dictionary<number, BindGroupLayout>();
+        for (const lGroupName of Object.keys(pLayout.groups)) {
+            const lGroup: ShaderLayout['groups'][string] = pLayout.groups[lGroupName];
+
+            // Generate each binding.
+            const lBindLayoutList: Array<BindLayout> = new Array<BindLayout>();
+            for (const lBindName of Object.keys(lGroup.bindings)) {
+                const lBind: ShaderLayout['groups'][string]['bindings'][string] = lGroup.bindings[lBindName];
+
+                // Generate and add bind layout to bind layout list.
+                lBindLayoutList.push({
+                    name: lBindName,
+                    index: lBind.index,
+                    layout: lBind.layout,
+                    visibility: lBind.visibility,
+                    accessMode: lBind.accessMode,
+                    usage: lBind.usage
+                });
             }
 
-            // Read from cache.
-            if (BaseShader.mBindGroupLayoutCache.has(lGroupLayout.identifier)) {
-                lGroupLayout = BaseShader.mBindGroupLayoutCache.get(lGroupLayout.identifier)!;
-            }
-
-            // Add group to pipeline.
-            this.mPipelineLayout.addGroupLayout(lGroupIndex, lGroupLayout);
+            // Set bind group layout with group index.
+            lInitialPipelineLayout.set(lGroup.index, new BindGroupLayout(this.device, lGroupName, lBindLayoutList));
         }
+
+        // Generate layout.
+        this.mPipelineLayout = new PipelineLayout(this.device, lInitialPipelineLayout);
+
+        // Save parameters.
+        this.mParameter = new Dictionary<string, PrimitiveBufferFormat>(Object.entries(pLayout.parameter));
     }
 
     /**
@@ -63,11 +84,11 @@ export class ShaderModule extends GpuNativeObject<GPUShaderModule> {
     protected override generate(): GPUShaderModule {
         // Create shader module use hints to speed up compilation on safari.
         return this.device.gpu.createShaderModule({
-            code: this.mSource, 
+            code: this.mSource,
             hints: {
                 WHAT: { layout: this.mPipelineLayout.native }
             },
-            //  sourceMap: undefined
+            // TODO: sourceMap: undefined
         });
     }
 }

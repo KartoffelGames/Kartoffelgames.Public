5887f3f4081be5eb25f77c9521bfe0c0e7488695
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2024-12-12 15:47:53 +0100

    Delete db.

--- packages/kartoffelgames.web.database/source/indexed_db/web-db.ts
+++ packages/kartoffelgames.web.database/source/indexed_db/web-db.ts
@@ -1,3 +1,55 @@
+import { Exception } from '@kartoffelgames/core';
+import { TableType } from './table/table-layout';
+
 export class WebDb {
-    
+    private readonly mDatabaseName: string;
+    private readonly mTableTypes: Array<TableType>;
+
+    /**
+     * Constructor.
+     * 
+     * @param pName - Database name.
+     * @param pTables - Database tables.
+     */
+    public constructor(pName: string, pTables: Array<TableType>) {
+        this.mDatabaseName = pName;
+        this.mTableTypes = [...pTables];
+    }
+
+    /**
+     * Delete database and resolve on success.
+     */
+    public async delete(): Promise<void> {
+        const lDeleteRequest: IDBOpenDBRequest = window.indexedDB.deleteDatabase(this.mDatabaseName);
+        return new Promise<void>((pResolve, pReject) => {
+            // Reject on error.
+            lDeleteRequest.onerror = () => {
+                pReject(new Exception('Error deleting database', this));
+            };
+
+            // Databse delete success.
+            lDeleteRequest.onsuccess = () => {
+                pResolve();
+            };
+        });
+
+    }
+
+    public async table(pType: TableType): Promise<void> {
+        // Create table object with attached opened db.
+    }
+
+    public transaction(pAction: () => void): void {
+        // Call action in a db transaction.
+    }
+
+    public async open(): Promise<void> {
+        // Open db.
+
+        // Init tables.
+
+        // Save open state.
+    }
+
+
 }

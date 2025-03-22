61981bad846746d98d0b54586eb2b3b1d0e494f8
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2025-03-22 02:56:03 +0100

    Shitup profile.

--- packages/kartoffelgames.core.xml/benchmark/profile.ts
+++ packages/kartoffelgames.core.xml/benchmark/profile.ts
@@ -2,15 +2,31 @@ import { XmlParser } from "../source/index.ts";
 
 // Load xml files.
 //const gFile: string = new TextDecoder("utf-8").decode(Deno.readFileSync(import.meta.dirname + "/basic-structure.xml"));
-const gFile: string = new TextDecoder("utf-8").decode(Deno.readFileSync(import.meta.dirname + "/complex-nested.xml"));
-//const gFile: string = new TextDecoder("utf-8").decode(Deno.readFileSync(import.meta.dirname + "/large-dataset.xml"));
+//const gFile: string = new TextDecoder("utf-8").decode(Deno.readFileSync(import.meta.dirname + "/complex-nested.xml"));
+const gFile: string = new TextDecoder("utf-8").decode(Deno.readFileSync(import.meta.dirname + "/large-dataset.xml"));
 
 const lXmlParser = new XmlParser();
-//const lResult = lXmlParser.parse(gFile, (pPosition: number, pLine: number, pColumn: number, pPercent: number) => {
-//    console.log(pPercent.toFixed(2).padStart(5, ' '), pPosition)
-//});
-const lResult = lXmlParser.parse(gFile);
 
-console.log(lResult)
+let lCurrentPosition: number = 0;
+let lCurrentLine: number = 0;
+let lCurrentColumn: number = 0;
+let lCurrentPercent: number = 0;
+let lLastLine: number = 0;
+let lLastTime: number = Date.now();
 
-console.log('READY')
+const lResult = lXmlParser.parse(gFile, (pPosition: number, pLine: number, pColumn: number, pPercent: number) => {
+    lCurrentPosition = pPosition;
+    lCurrentLine = pLine;
+    lCurrentColumn = pColumn;
+    lCurrentPercent = pPercent;
+
+    if (Date.now() - lLastTime > 1000) {
+        console.log(lCurrentPercent.toFixed(2).padStart(5, ' '), '%', 'P:' + lCurrentPosition, 'L:' + lCurrentLine, 'C:' + lCurrentColumn, 'L/s:', (lCurrentLine - lLastLine));
+        lLastLine = lCurrentLine;
+        lLastTime = Date.now();
+    }
+});
+
+console.log(lResult);
+
+console.log('READY');

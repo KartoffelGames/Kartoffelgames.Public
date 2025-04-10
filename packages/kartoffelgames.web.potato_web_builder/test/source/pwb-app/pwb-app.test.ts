9cfd8cb4d80121912a0bc33dba688bc756dd835a
Tetrodopotato <tetrodotoxinpotato@gmail.com>
2025-04-10 16:00:28 +0200

    Reworked pwb app test.

--- packages/kartoffelgames.web.potato_web_builder/test/source/pwb-app/pwb-app.test.ts
+++ packages/kartoffelgames.web.potato_web_builder/test/source/pwb-app/pwb-app.test.ts
@@ -1,17 +1,16 @@
 // Import mock at start of file.
 import { MOCK_WINDOW, TestUtil } from '../../utility/test-util.ts';
 
-// Funcitonal imports after mock.
+// Functional imports after mock.
 import { expect } from '@kartoffelgames/core-test';
-import { describe, it } from '@std/testing/bdd';
 import { PwbComponent } from '../../../source/core/component/pwb-component.decorator.ts';
 import { PwbTemplateXmlNode } from '../../../source/core/component/template/nodes/pwb-template-xml-node.ts';
 import { PwbTemplate } from '../../../source/core/component/template/nodes/pwb-template.ts';
 import { Processor } from '../../../source/core/core_entity/processor.ts';
 import { PwbApp } from '../../../source/pwb-app/pwb-app.ts';
 
-describe('PwbApp', () => {
-    it('Property: component', () => {
+Deno.test('PwbApp.component', async (pContext) => {
+    await pContext.step('Default', async () => {
         // Setup.
         const lApp: PwbApp = new PwbApp();
 
@@ -22,106 +21,107 @@ describe('PwbApp', () => {
         expect(lContent.tagName.toLowerCase()).toBe('pwb-app');
         expect(lContent.shadowRoot).not.toBeNull();
     });
+});
 
-    describe('Method: addContent', () => {
-        it('-- Correct component', async () => {
-            // Setup.
-            const lApp: PwbApp = new PwbApp();
-            const lSelector: string = TestUtil.randomSelector();
+Deno.test('PwbApp.addContent()', async (pContext) => {
+    await pContext.step('Correct component', async () => {
+        // Setup.
+        const lApp: PwbApp = new PwbApp();
+        const lSelector: string = TestUtil.randomSelector();
 
-            // Setup. Define component.
-            @PwbComponent({
-                selector: lSelector,
-                template: '<div/>'
-            })
+        // Setup. Define component.
+        @PwbComponent({
+            selector: lSelector,
+            template: '<div/>'
+        })
+        class TestComponent extends Processor { }
 
-            class TestComponent extends Processor { }
+        // Process. Add component and wait for update.
+        lApp.addContent(TestComponent);
+        await TestUtil.waitForUpdate(lApp.component);
 
-            // Process. Add component and wait for update.
-            lApp.addContent(TestComponent);
-            await TestUtil.waitForUpdate(lApp.component);
+        // Process. Read component.
+        const lContent: Element = <Element>lApp.component.shadowRoot!.querySelector(lSelector);
 
-            // Process. Read component.
-            const lContent: Element = <Element>lApp.component.shadowRoot!.querySelector(lSelector);
-
-            // Evaluation.
-            expect(lContent.tagName.toLowerCase()).toBe(lSelector);
-        });
-
-        it('-- Initialize component before appendTo', async () => {
-            // Setup.
-            const lSelector: string = TestUtil.randomSelector();
-            const lApp: PwbApp = new PwbApp();
-
-            // Setup. Define component.
-            @PwbComponent({
-                selector: lSelector,
-                template: '<div/>'
-            })
-            class TestComponent extends Processor { }
-
-            // Process. Add content and wait for rendering.
-            lApp.addContent(TestComponent);
-            await TestUtil.waitForUpdate(lApp.component);
-
-            // Process. Get component.
-            const lContent: HTMLElement = <HTMLElement>lApp.component.shadowRoot!.querySelector(lSelector);
-
-            // Evaluation.
-            expect(lContent).toBeComponentStructure(MOCK_WINDOW, [
-                MOCK_WINDOW.Comment,
-                MOCK_WINDOW.HTMLDivElement
-            ], true);
-        });
-
-        it('-- Wrong component type', () => {
-            // Setup.
-            const lApp: PwbApp = new PwbApp();
-
-            // Process.
-            const lErrorFunction = () => {
-                lApp.addContent(String);
-            };
-
-            // Evaluation.
-            expect(lErrorFunction).toThrow(`Constructor "${String.name}" is not a registered custom element`);
-        });
+        // Evaluation.
+        expect(lContent.tagName.toLowerCase()).toBe(lSelector);
     });
 
-    describe('Method: appendTo', () => {
-        it('-- Default', async () => {
-            // Setup.
-            const lDummyElement: HTMLDivElement = MOCK_WINDOW.document.createElement('div');
-            const lApp: PwbApp = new PwbApp();
-            lApp.setSplashScreen({ manual: true, animationTime: 0 });
+    await pContext.step('Initialize component before appendTo', async () => {
+        // Setup.
+        const lSelector: string = TestUtil.randomSelector();
+        const lApp: PwbApp = new PwbApp();
 
-            // Process.
-            lApp.appendTo(lDummyElement);
+        // Setup. Define component.
+        @PwbComponent({
+            selector: lSelector,
+            template: '<div/>'
+        })
+        class TestComponent extends Processor { }
 
-            // Evaluation.
-            expect(lDummyElement.childNodes[0]).toBe(lApp.component);
-        });
+        // Process. Add content and wait for rendering.
+        lApp.addContent(TestComponent);
+        await TestUtil.waitForUpdate(lApp.component);
 
-        it('-- Reappend to document.', async () => {
-            // Setup. Create app.
-            const lApp: PwbApp = new PwbApp();
+        // Process. Get component.
+        const lContent: HTMLElement = <HTMLElement>lApp.component.shadowRoot!.querySelector(lSelector);
 
-            // Process. Create splash screen.
-            lApp.setSplashScreen({
-                animationTime: 0
-            });
-
-            // Process. Append and wait for splash screen remove
-            lApp.appendTo(MOCK_WINDOW.document.body);
-            await TestUtil.waitForUpdate(lApp.component);
-
-            // Process
-            lApp.appendTo(MOCK_WINDOW.document.body);
-            await TestUtil.waitForUpdate(lApp.component);
-        });
+        // Evaluation.
+        expect(lContent).toBeComponentStructure(MOCK_WINDOW, [
+            MOCK_WINDOW.Comment,
+            MOCK_WINDOW.HTMLDivElement
+        ], true);
     });
 
-    it('Method: addErrorListener', async () => {
+    await pContext.step('Wrong component type', () => {
+        // Setup.
+        const lApp: PwbApp = new PwbApp();
+
+        // Process.
+        const lErrorFunction = () => {
+            lApp.addContent(String);
+        };
+
+        // Evaluation.
+        expect(lErrorFunction).toThrow(`Constructor "${String.name}" is not a registered custom element`);
+    });
+});
+
+Deno.test('PwbApp.appendTo()', async (pContext) => {
+    await pContext.step('Default', async () => {
+        // Setup.
+        const lDummyElement: HTMLDivElement = MOCK_WINDOW.document.createElement('div');
+        const lApp: PwbApp = new PwbApp();
+        lApp.setSplashScreen({ manual: true, animationTime: 0 });
+
+        // Process.
+        lApp.appendTo(lDummyElement);
+
+        // Evaluation.
+        expect(lDummyElement.childNodes[0]).toBe(lApp.component);
+    });
+
+    await pContext.step('Reappend to document.', async () => {
+        // Setup. Create app.
+        const lApp: PwbApp = new PwbApp();
+
+        // Process. Create splash screen.
+        lApp.setSplashScreen({
+            animationTime: 0
+        });
+
+        // Process. Append and wait for splash screen remove
+        lApp.appendTo(MOCK_WINDOW.document.body);
+        await TestUtil.waitForUpdate(lApp.component);
+
+        // Process
+        lApp.appendTo(MOCK_WINDOW.document.body);
+        await TestUtil.waitForUpdate(lApp.component);
+    });
+});
+
+Deno.test('PwbApp.addErrorListener()', async (pContext) => {
+    await pContext.step('Default', async () => {
         // Setup.
         const lErrorMessage: string = 'Custom Error';
         const lSelector: string = TestUtil.randomSelector();
@@ -131,7 +131,6 @@ describe('PwbApp', () => {
             selector: lSelector,
             template: '{{this.value}}'
         })
-
         class TestComponent extends Processor {
             public value: string = '';
 
@@ -147,7 +146,7 @@ describe('PwbApp', () => {
         lApp.setSplashScreen({ manual: true, animationTime: 0 });
         lApp.appendTo(MOCK_WINDOW.document.body);
 
-        // Process. Lof error.
+        // Process. Log error.
         let lErrorMessageResult: string | null = null;
         lApp.addErrorListener((pError: Error) => {
             lErrorMessageResult = pError.message;
@@ -169,50 +168,52 @@ describe('PwbApp', () => {
         // Evaluation.
         expect(lErrorMessageResult).toBe(lErrorMessage);
     });
+});
 
-    describe('Method: addStyle', () => {
-        it('-- Default', async () => {
-            // Setup.
-            const lStyleContent: string = 'Content';
+Deno.test('PwbApp.addStyle()', async (pContext) => {
+    await pContext.step('Default', async () => {
+        // Setup.
+        const lStyleContent: string = 'Content';
 
-            // Setup. Init app.
-            const lApp: PwbApp = new PwbApp();
-            lApp.appendTo(MOCK_WINDOW.document.body);
+        // Setup. Init app.
+        const lApp: PwbApp = new PwbApp();
+        lApp.appendTo(MOCK_WINDOW.document.body);
 
-            // Process.
-            lApp.addStyle(lStyleContent);
-            await TestUtil.waitForUpdate(lApp.component);
+        // Process.
+        lApp.addStyle(lStyleContent);
+        await TestUtil.waitForUpdate(lApp.component);
 
-            const lContent: HTMLStyleElement = <HTMLStyleElement>lApp.component.shadowRoot!.querySelectorAll('style')[1];
+        const lContent: HTMLStyleElement = <HTMLStyleElement>lApp.component.shadowRoot!.querySelectorAll('style')[1];
 
-            // Evaluation.
-            expect(lContent instanceof MOCK_WINDOW.HTMLStyleElement).toBeTruthy();
-            expect(lContent.textContent).toBe(lStyleContent);
-        });
-
-        it('-- Add style after append', async () => {
-            // Setup.
-            const lStyleContent: string = 'Content';
-
-            // Setup. Init app.
-            const lApp: PwbApp = new PwbApp();
-            lApp.setSplashScreen({ animationTime: 0 });
-            lApp.appendTo(MOCK_WINDOW.document.body);
-
-            // Process. Add style and wait.
-            lApp.addStyle(lStyleContent);
-            await TestUtil.waitForUpdate(lApp.component);
-
-            // Get style content.
-            const lContent: HTMLStyleElement = <HTMLStyleElement>lApp.component.shadowRoot!.querySelectorAll('style')[1];
-
-            // Evaluation.
-            expect(lContent instanceof MOCK_WINDOW.HTMLStyleElement).toBeTruthy();
-            expect(lContent.textContent).toBe(lStyleContent);
-        });
+        // Evaluation.
+        expect(lContent instanceof MOCK_WINDOW.HTMLStyleElement).toBeTruthy();
+        expect(lContent.textContent).toBe(lStyleContent);
     });
 
-    it('Method: removeSplashScreen', async () => {
+    await pContext.step('Add style after append', async () => {
+        // Setup.
+        const lStyleContent: string = 'Content';
+
+        // Setup. Init app.
+        const lApp: PwbApp = new PwbApp();
+        lApp.setSplashScreen({ animationTime: 0 });
+        lApp.appendTo(MOCK_WINDOW.document.body);
+
+        // Process. Add style and wait.
+        lApp.addStyle(lStyleContent);
+        await TestUtil.waitForUpdate(lApp.component);
+
+        // Get style content.
+        const lContent: HTMLStyleElement = <HTMLStyleElement>lApp.component.shadowRoot!.querySelectorAll('style')[1];
+
+        // Evaluation.
+        expect(lContent instanceof MOCK_WINDOW.HTMLStyleElement).toBeTruthy();
+        expect(lContent.textContent).toBe(lStyleContent);
+    });
+});
+
+Deno.test('PwbApp.removeSplashScreen()', async (pContext) => {
+    await pContext.step('Default', async () => {
         // Setup.
         const lApp: PwbApp = new PwbApp();
         lApp.setSplashScreen({ animationTime: 0 });
@@ -225,88 +226,88 @@ describe('PwbApp', () => {
         // Evaluation.
         expect(lContent).toBeNull();
     });
+});
 
-    describe('Method: setSplashScreen', () => {
-        it('-- Content', async () => {
-            // Setup. Content.
-            const lContentSpan = new PwbTemplateXmlNode();
-            lContentSpan.tagName = 'span';
-            lContentSpan.setAttribute('style').addValue('color: #fff;');
+Deno.test('PwbApp.setSplashScreen()', async (pContext) => {
+    await pContext.step('Content', async () => {
+        // Setup. Content.
+        const lContentSpan = new PwbTemplateXmlNode();
+        lContentSpan.tagName = 'span';
+        lContentSpan.setAttribute('style').addValue('color: #fff;');
 
-            // Setup. Add content to content template.
-            const lContent = new PwbTemplate();
-            lContent.appendChild(lContentSpan);
+        // Setup. Add content to content template.
+        const lContent = new PwbTemplate();
+        lContent.appendChild(lContentSpan);
 
-            // Setup. Create app.
-            const lApp: PwbApp = new PwbApp();
+        // Setup. Create app.
+        const lApp: PwbApp = new PwbApp();
 
-            // Process. Create splash screen.
-            lApp.setSplashScreen({
-                content: lContent,
-                animationTime: 0,
-                manual: true
-            });
-
-            // Setup. Add app to document.
-            await TestUtil.waitForUpdate(lApp.component);
-
-            // Process. Read splash screen data.
-            const lContentElement: HTMLElement = lApp.component.shadowRoot!.querySelector('span')!;
-
-            expect(lContentElement.getAttribute('style')).toBe('color: #fff;');
+        // Process. Create splash screen.
+        lApp.setSplashScreen({
+            content: lContent,
+            animationTime: 0,
+            manual: true
         });
 
-        it('-- Background', async () => {
-            // Setup.
-            const lBackground: string = 'red';
+        // Setup. Add app to document.
+        await TestUtil.waitForUpdate(lApp.component);
 
-            // Setup. Content.
-            const lContentSpan = new PwbTemplateXmlNode();
-            lContentSpan.tagName = 'span';
-            lContentSpan.setAttribute('style').addValue('color: #fff;');
+        // Process. Read splash screen data.
+        const lContentElement: HTMLElement = lApp.component.shadowRoot!.querySelector('span')!;
 
-            // Setup. Create app.
-            const lApp: PwbApp = new PwbApp();
-            lApp.appendTo(MOCK_WINDOW.document.body);
+        expect(lContentElement.getAttribute('style')).toBe('color: #fff;');
+    });
 
-            // Process. Create splash screen.
-            lApp.setSplashScreen({
-                background: lBackground,
-                animationTime: 0,
-                manual: true
-            });
-            await TestUtil.waitForUpdate(lApp.component);
+    await pContext.step('Background', async () => {
+        // Setup.
+        const lBackground: string = 'red';
 
-            // Process. Read splash screen data.
-            const lSplashScreen: HTMLElement = <HTMLElement>lApp.component.shadowRoot!.querySelector('.splashscreen');
+        // Setup. Content.
+        const lContentSpan = new PwbTemplateXmlNode();
+        lContentSpan.tagName = 'span';
+        lContentSpan.setAttribute('style').addValue('color: #fff;');
 
-            expect(lSplashScreen.style.getPropertyValue('--background')).toBe(lBackground);
+        // Setup. Create app.
+        const lApp: PwbApp = new PwbApp();
+        lApp.appendTo(MOCK_WINDOW.document.body);
+
+        // Process. Create splash screen.
+        lApp.setSplashScreen({
+            background: lBackground,
+            animationTime: 0,
+            manual: true
+        });
+        await TestUtil.waitForUpdate(lApp.component);
+
+        // Process. Read splash screen data.
+        const lSplashScreen: HTMLElement = <HTMLElement>lApp.component.shadowRoot!.querySelector('.splashscreen');
+
+        expect(lSplashScreen.style.getPropertyValue('--background')).toBe(lBackground);
+    });
+
+    await pContext.step('Manual splash screen', async () => {
+        // Setup. Create app.
+        const lApp: PwbApp = new PwbApp();
+        lApp.setSplashScreen({
+            animationTime: 0
         });
 
-        it('-- Manual splash screen', async () => {
-            // Setup. Create app.
-            const lApp: PwbApp = new PwbApp();
-            lApp.setSplashScreen({
-                animationTime: 0
-            });
-
-            // Process. Create splash screen.
-            lApp.setSplashScreen({
-                manual: true
-            });
-
-            // Process. Append and wait for splash screen remove
-            lApp.appendTo(MOCK_WINDOW.document.body);
-            const lBeforeRemoveState: boolean = !!lApp.component.shadowRoot?.querySelector('.splashscreen');
-
-            // Process
-            await lApp.removeSplashScreen();
-            await TestUtil.waitForUpdate(lApp.component);
-            const lAfterRemoveState: boolean = !!lApp.component.shadowRoot?.querySelector('.splashscreen');
-
-            // Evaluation.
-            expect(lBeforeRemoveState).toBeTruthy();
-            expect(lAfterRemoveState).toBeFalsy();
+        // Process. Create splash screen.
+        lApp.setSplashScreen({
+            manual: true
         });
+
+        // Process. Append and wait for splash screen remove
+        lApp.appendTo(MOCK_WINDOW.document.body);
+        const lBeforeRemoveState: boolean = !!lApp.component.shadowRoot?.querySelector('.splashscreen');
+
+        // Process
+        await lApp.removeSplashScreen();
+        await TestUtil.waitForUpdate(lApp.component);
+        const lAfterRemoveState: boolean = !!lApp.component.shadowRoot?.querySelector('.splashscreen');
+
+        // Evaluation.
+        expect(lBeforeRemoveState).toBeTruthy();
+        expect(lAfterRemoveState).toBeFalsy();
     });
 });

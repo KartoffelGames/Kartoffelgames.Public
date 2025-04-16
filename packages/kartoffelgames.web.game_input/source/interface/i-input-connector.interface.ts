c59f35cf311e8164addbb2a538207fba994d16df
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2025-04-16 21:40:41 +0200

    Remove complete bullshitting garbage of a project i really be ashamed of [[game_input]].

--- packages/kartoffelgames.web.game_input/source/interface/i-input-connector.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { InputDevices } from '../input-devices.ts';
-
-export interface IInputConnector {
-    /**
-     * Start connection cycle.
-     * @param pDevices - Device register.
-     */
-    init(pDevices: InputDevices): void;
-}
--- packages/kartoffelgames.web.game_input/source/types.ts
+++
@@ -1,5 +0,0 @@
-import type { KeyboardButton } from './enum/keyboard-button.enum.ts';
-import type { GamepadButton } from './enum/gamepad-button.enum.ts';
-import type { MouseButton } from './enum/mouse-button.enum.ts';
-
-export type InputButton = KeyboardButton | GamepadButton | MouseButton;

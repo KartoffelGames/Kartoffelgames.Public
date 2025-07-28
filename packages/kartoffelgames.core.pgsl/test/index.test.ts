2bc9a4881c9b6916de5c30428dcfb87626ff249e
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2025-07-29 00:13:43 +0200

    Move all pgsl dedicated stuff into pgsl package.

--- packages/kartoffelgames.core.pgsl/test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from '@std/testing/bdd';
-import { expect } from '@std/expect';
-
-describe("Main", () => {
-    it("Placeholder Test", () => {
-        const lResult = 2 + 3;
-        expect(lResult).toBe(5);
-    });
-});

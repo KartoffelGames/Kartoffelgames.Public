6b3b461dbe18eafdb4852101c9cd9ad2d726b8cd
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2025-11-05 00:05:08 +0100

    Sample type for binding result information.

--- packages/kartoffelgames.core.pgsl/source/parser_result/pgsl-parser-result-binding.ts
+++ packages/kartoffelgames.core.pgsl/source/parser_result/pgsl-parser-result-binding.ts
@@ -1,8 +1,23 @@
 import { Exception } from "@kartoffelgames/core";
 import { PgslDeclarationType } from "../enum/pgsl-declaration-type.enum.ts";
 import { PgslValueTrace } from "../trace/pgsl-value-trace.ts";
-import { PgslParserResultType } from "./type/pgsl-parser-result-type.ts";
+import { PgslParserResultType, PgslParserResultTypeAlignmentType } from "./type/pgsl-parser-result-type.ts";
 import { PgslType } from "../type/pgsl-type.ts";
+import { PgslNumericType } from "../type/pgsl-numeric-type.ts";
+import { PgslParserResultNumericType } from "./type/pgsl-parser-result-numeric-type.ts";
+import { PgslVectorType } from "../type/pgsl-vector-type.ts";
+import { PgslParserResultVectorType } from "./type/pgsl-parser-result-vector-type.ts";
+import { PgslMatrixType } from "../type/pgsl-matrix-type.ts";
+import { PgslParserResultMatrixType } from "./type/pgsl-parser-result-matrix-type.ts";
+import { PgslArrayType } from "../type/pgsl-array-type.ts";
+import { PgslParserResultArrayType } from "./type/pgsl-parser-result-array-type.ts";
+import { PgslStructType } from "../type/pgsl-struct-type.ts";
+import { PgslParserResultStructMember, PgslParserResultStructType } from "./type/pgsl-parser-result-struct-type.ts";
+import { PgslTrace } from "../trace/pgsl-trace.ts";
+import { PgslStructTrace } from "../trace/pgsl-struct-trace.ts";
+import { PgslStructPropertyTrace } from "../trace/pgsl-struct-property-trace.ts";
+import { PgslSamplerType } from "../type/pgsl-sampler-type.ts";
+import { PgslParserResultSamplerType } from "./type/pgsl-parser-result-sampler-type.ts";
 
 /**
  * Represents a binding result from PGSL parser with type and location information.
@@ -74,7 +89,7 @@ export class PgslParserResultBinding {
      *
      * @param pParameters - The constructor parameters containing all binding information.
      */
-    public constructor(pBinding: PgslValueTrace) {
+    public constructor(pBinding: PgslValueTrace, pTrace: PgslTrace) {
         // Convert binding type from trace.
         this.mBindingType = (() => {
             switch (pBinding.declarationType) {
@@ -96,11 +111,104 @@ export class PgslParserResultBinding {
         this.mBindLocationIndex = pBinding.bindingInformation.bindLocationIndex;
 
         // Convert type.
-        this.mType = this.convertType(pBinding.type);
+        this.mType = this.convertType(pBinding.type, pTrace);
     }
 
-    private convertType(pType: PgslType): PgslParserResultType {
-        // Conversion logic here...
+    /**
+     * Converts a PGSL traced type to a parser result type.
+     *
+     * @param pType - pgsl traced type.
+     *
+     * @returns The parser result type.
+     */
+    private convertType(pType: PgslType, pTrace: PgslTrace): PgslParserResultType {
+        // Convert binding type to alignment type.
+        const lAlignmentType: PgslParserResultTypeAlignmentType = (() => {
+            switch (this.mBindingType) {
+                case 'uniform': return 'uniform';
+                case 'storage': return 'storage';
+            }
+        })();
+
+        switch (true) {
+            // Numeric types.
+            case pType instanceof PgslNumericType: {
+                switch (pType.numericTypeName) {
+                    case PgslNumericType.typeName.float32:
+                        return new PgslParserResultNumericType('float', lAlignmentType);
+                    case PgslNumericType.typeName.signedInteger:
+                        return new PgslParserResultNumericType('integer', lAlignmentType);
+                    case PgslNumericType.typeName.unsignedInteger:
+                        return new PgslParserResultNumericType('unsigned-integer', lAlignmentType);
+                }
+                throw new Exception(`Unsupported numeric type in PgslNumericType: ${pType.numericTypeName}`, this);
+            }
+
+            // Vector types.
+            case pType instanceof PgslVectorType: {
+                const elementType = this.convertType(pType.innerType, pTrace) as PgslParserResultNumericType;
+                return new PgslParserResultVectorType(elementType, pType.dimension, lAlignmentType);
+            }
+
+            // Matrix types.
+            case pType instanceof PgslMatrixType: {
+                const elementType = this.convertType(pType.innerType, pTrace) as PgslParserResultNumericType;
+                return new PgslParserResultMatrixType(elementType, pType.rowCount, pType.columnCount, lAlignmentType);
+            }
+
+            // Array types.
+            case pType instanceof PgslArrayType: {
+                const elementType = this.convertType(pType.innerType, pTrace) as PgslParserResultNumericType;
+                return new PgslParserResultArrayType(elementType, pType.length, lAlignmentType);
+            }
+
+            // TODO: Texture
+
+            // Sampler type
+            case pType instanceof PgslSamplerType: {
+                // Samplers are always packed alignment.
+                return new PgslParserResultSamplerType(pType.comparison);
+            }
+
+            // Struct types.
+            case pType instanceof PgslStructType: {
+                const lStruct: PgslStructTrace | undefined = pTrace.getStruct(pType.structName);
+                if (!lStruct) {
+                    throw new Exception(`Struct trace not found for struct: ${pType.structName}`, this);
+                }
+
+                const lPropertyArray: Array<PgslParserResultStructMember> = new Array<PgslParserResultStructMember>();
+
+                // Convert property types.
+                for (const lStructPropertyDeclaration of lStruct.declaration.properties) {
+                    const lStructProperty: PgslStructPropertyTrace = pTrace.getStructProperty(lStructPropertyDeclaration); // TODO: THIS SHIT NEEDS TO BE CLEANED UP ASAP!!!
+
+                    // Create default property result.
+                    const lPropertyResult: PgslParserResultStructMember = {
+                        name: lStructProperty.name,
+                        type: this.convertType(lStructProperty.type, pTrace),
+                    }
+
+                    // Apply size override if present.
+                    if (typeof lStructProperty.meta.size === 'number') {
+                        lPropertyResult.sizeOverride = lStructProperty.meta.size;
+                    }
+
+                    // Apply alignment override if present.
+                    if (typeof lStructProperty.meta.alignment === 'number') {
+                        lPropertyResult.alignmentOverride = lStructProperty.meta.alignment;
+                    }
+
+                    // Add property result to array.
+                    lPropertyArray.push(lPropertyResult);
+                }
+
+                return new PgslParserResultStructType(lPropertyArray, lAlignmentType);
+            }
+
+            default:
+                throw new Exception(`Unsupported type in PgslParserResultBinding conversion.`, this);
+        }
     }
 }
 

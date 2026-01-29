bd80ade73ff54efb1fea027700c1bf6d2c563c40
henrikschauer <henrikschauer@aera-gmbh.de>
2026-01-29 09:53:19 +0100

    Remove unfinished projeckt files.

--- packages/kartoffelgames.web.potato_render/source/something_better/view_projection/projection/i-projection.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { Matrix } from '../../../math/matrix.ts';
-
-export interface IProjection {
-    aspectRatio: number;
-    far: number;
-    near: number;
-    projectionMatrix: Matrix;
-}

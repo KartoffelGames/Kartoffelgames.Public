825aba059e4a18ef9c5b12786f5531bf39288792
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2026-02-08 01:23:34 +0100

    Clean up system a bit.

--- packages/kartoffelgames.web.potato_engine/source/scene/environment.ts
+++ packages/kartoffelgames.web.potato_engine/source/scene/environment.ts
@@ -1,28 +1,223 @@
+import { EnvironmentStateChange, EnvironmentTransmission } from "./environment-transmittion.ts";
 import { Component } from "./component.ts";
-import { EnvironmentStateChange } from "./environment-transmittion.ts";
-import { GameObject } from "./game-object.ts";
 import { Scene } from "./scene.ts";
 import { System } from "./system.ts";
 
 export class Environment {
     private readonly mSystems: Array<System> = new Array<System>();
     private readonly mStateChangeQueue: Array<EnvironmentStateChange> = new Array<EnvironmentStateChange>();
+    private readonly mLoadedScenes: Set<Scene> = new Set();
 
+    /**
+     * Register a system with the environment.
+     * The system will be initialized and notified of component state changes.
+     *
+     * @param system - The system to register
+     */
     public registerSystem(system: System): void {
-        // TODO: Check dependencies of system.
+        // TODO: Check dependencies of system and provide the instance to the system.
 
         // TODO: Register system as a component initializer if it has set any.
 
+        // Add system to list
         this.mSystems.push(system);
     }
 
-    public loadScene(_scene: Scene): void {
-        // TODO: Load all game objects/ their components with an index of the scene.
-        // So a scene can be loaded and deloaded independently of another by maybe create independent buffers of each scene.
+    /**
+     * Load a scene into the environment.
+     * This establishes the environment connection for all game objects in the scene,
+     * allowing them to transmit component state changes.
+     *
+     * @param pScene - The scene to load
+     */
+    public loadScene(pScene: Scene): void {
+        // Mark scene as loaded
+        this.mLoadedScenes.add(pScene);
+
+        // Create a transmission object that queues state changes
+        const lTransmission = new EnvironmentTransmission((pStateChange: EnvironmentStateChange) => {
+            this.mStateChangeQueue.push(pStateChange);
+        });
+
+        // Establish environment connection for all root game objects in the scene
+        for (const lGameObject of pScene.gameObjects) {
+            lGameObject.establishEnvironmentConnection(lTransmission);
+        }
+
+        // TODO: Need to trigger an add event for all components. Somehow?
     }
 
-    public unloadScene(_scene: Scene): void {
-        // The same as loadScene, but in reverse.
+    /**
+     * Unload a scene from the environment.
+     * All components from the scene will be removed from tracking by systems.
+     *
+     * @param pScene - The scene to unload
+     */
+    public unloadScene(pScene: Scene): void {
+        // Mark scene as unloaded
+        this.mLoadedScenes.delete(pScene);
+
+        // TODO: Remove all game objects and components from this scene from system tracking
+        // TODO: Maybe disable them first and then remove them in a unpriorized way: Different list, that gets processed some time later?
+    }
+
+    /**
+     * Process all queued state changes and notify systems.
+     * Call this once per update cycle before calling executeUpdate.
+     *
+     * @internal
+     */
+    public processStateChanges(): void {
+        // Optimize and order the state change queue
+        const lOptimizedQueue = this.optimizeStateChangeQueue();
+
+        // Clear the original queue. Splice is half as fast as setting length to 0.
+        this.mStateChangeQueue.length = 0;
+
+        // Notify all systems about optimized state changes
+        for (const lStateChange of lOptimizedQueue) {
+            for (const lSystem of this.mSystems) {
+                lSystem.handleStateChange(lStateChange);
+            }
+        }
+    }
+
+    /**
+     * Optimizes the state change queue by consolidating redundant state changes.
+     * Reduces event notifications based on the following rules:
+     * - add + activate = send only add
+     * - deactivate + remove = send only remove
+     * - add + remove = send no events
+     * - activate + deactivate = send no events
+     * - deactivate + activate = send no events
+     *
+     * Update events are always included unless the component is being removed.
+     *
+     * @returns Optimized array of state changes to process.
+     *
+     * @internal
+     */
+    private optimizeStateChangeQueue(): Array<EnvironmentStateChange> {
+        // Track final lifecycle state and update status for each component
+        const lComponentStates = new Map<Component, {
+            lifecycleState: 'add' | 'remove' | 'activate' | 'deactivate' | null;
+            hasUpdate: boolean;
+        }>();
+
+        // Process each state change in order to determine final state
+        for (const lStateChange of this.mStateChangeQueue) {
+            const lComponent = lStateChange.component;
+
+            // Initialize tracking state if needed
+            if (!lComponentStates.has(lComponent)) {
+                lComponentStates.set(lComponent, {
+                    lifecycleState: null,
+                    hasUpdate: false
+                });
+            }
+
+            const lState = lComponentStates.get(lComponent)!;
+
+            // Update state based on new event and current state
+            switch (lStateChange.type) {
+                case 'add': {
+                    lState.lifecycleState = 'add';
+                    break;
+                }
+                case 'remove': {
+                    if (lState.lifecycleState === 'add') {
+                        // Rule: add + remove = send no events
+                        lState.lifecycleState = null;
+                    } else {
+                        // Rule: deactivate + remove = send only remove
+                        lState.lifecycleState = 'remove';
+                    }
+                    break;
+                }
+                case 'activate': {
+                    if (lState.lifecycleState === 'add') {
+                        // Rule: add + activate = send only add, suppress activate
+                        // State remains 'add'
+                    } else if (lState.lifecycleState === 'deactivate') {
+                        // Rule: deactivate + activate = send no events
+                        lState.lifecycleState = null;
+                    } else {
+                        lState.lifecycleState = 'activate';
+                    }
+                    break;
+                }
+                case 'deactivate': {
+                    if (lState.lifecycleState === 'activate') {
+                        // Rule: activate + deactivate = send no events
+                        lState.lifecycleState = null;
+                    } else if (lState.lifecycleState !== 'add') {
+                        // Only set deactivate if not just added
+                        lState.lifecycleState = 'deactivate';
+                    }
+                    break;
+                }
+                case 'update': {
+                    // Track that an update occurred
+                    lState.hasUpdate = true;
+                    break;
+                }
+            }
+        }
+
+        // Rebuild queue with only optimized state changes
+        const lOptimizedQueue: Array<EnvironmentStateChange> = [];
+
+        for (const [lComponent, lState] of lComponentStates) {
+            // Add lifecycle state change if present
+            if (lState.lifecycleState !== null) {
+                lOptimizedQueue.push({
+                    type: lState.lifecycleState,
+                    component: lComponent
+                });
+            }
+
+            // Add update event if there were updates and component is not being removed
+            if (lState.hasUpdate && lState.lifecycleState !== 'remove') {
+                lOptimizedQueue.push({
+                    type: 'update',
+                    component: lComponent
+                });
+            }
+        }
+
+        return lOptimizedQueue;
+    }
+
+    /**
+     * Execute the update cycle for all systems.
+     * This should be called once per frame.
+     *
+     * @internal
+     */
+    public executeUpdate(): void {
+        // Process all queued state changes first
+        this.processStateChanges();
+
+        // Call update on all systems
+        for (const lSystem of this.mSystems) {
+            lSystem.executeUpdate();
+        }
+    }
+
+    /**
+     * Execute the tick cycle for all systems.
+     * This should be called at a fixed timestep for physics.
+     *
+     * @internal
+     */
+    public executeTick(): void {
+        // Process any state changes that occurred during the tick
+        this.processStateChanges();
+
+        // Call tick on all systems
+        for (const lSystem of this.mSystems) {
+            lSystem.executeTick();
+        }
     }
 }
 
--- packages/kartoffelgames.web.potato_engine/source/scene/system.ts
+++ packages/kartoffelgames.web.potato_engine/source/scene/system.ts
@@ -1,9 +1,101 @@
-export abstract class System {
+import { Exception } from "@kartoffelgames/core";
+import { IAnyParameterConstructor } from "../../../kartoffelgames.core/source/interface/i-constructor.ts";
+import { GameComponentConstructor } from "./component.ts";
+import { EnvironmentStateChange } from "./environment-transmittion.ts";
 
-    // TODO: Create a "System"-Class that can be registered in the environment. The system can have other system dependencies, so a call order can be created.
-    // TODO: InitComponent() - Initializes components data somehow. Maybe with a init method of the component itself?
-    // TODO: Hold a list of all components of the system, so that it can update them in the update loop. This list should be updated whenever a component is added or removed from a game object.
-    // TODO: Load and deload scenes, it is possible to have multiple scenes loaded at the same time.
-    // TODO: Add a action queue to add, remove, activate or deactive components and game objects. At a set time the system can process the queue, so that it can update its lists of components and game objects.
-    // TODO: How to share data between systems? Maybe a expose of a shared buffer?
+// TODO: Create a "System"-Class that can be registered in the environment. The system can have other system dependencies, so a call order can be created.
+// TODO: InitComponent() - Initializes components data somehow. Maybe with a init method of the component itself?
+// TODO: Hold a list of all components of the system, so that it can update them in the update loop. This list should be updated whenever a component is added or removed from a game object.
+// TODO: Load and deload scenes, it is possible to have multiple scenes loaded at the same time.
+// TODO: Add a action queue to add, remove, activate or deactive components and game objects. At a set time the system can process the queue, so that it can update its lists of components and game objects.
+
+export abstract class System {
+    private readonly mDependencySystems: Map<SystemConstructor<System>, System>;
+
+    /**
+     * Define which component types this system is interested in.
+     * Override this method to return an array of component types this system handles.
+     */
+    public abstract readonly handledComponentTypes: Array<GameComponentConstructor>;
+
+    /**
+     * Constructor of the system.
+     */
+    public constructor() {
+        this.mDependencySystems = new Map<SystemConstructor<System>, System>();
+    }
+
+    /**
+     * Initialize the system with the interested component types.
+     * This is called by the environment during registration.
+     *
+     * @internal
+     */
+    public initialize(pDependencySystems: Array<System>): void {
+        // Store dependent systems in a map.
+        for (const lSystem of pDependencySystems) {
+            this.mDependencySystems.set(lSystem.constructor as SystemConstructor<System>, lSystem);
+        }
+    }
+
+    /**
+     * Call the update hook.
+     * This is called by the environment.
+     *
+     * @internal
+     */
+    public executeUpdate(pStateChanges: Array<EnvironmentStateChange>): void {
+        this.onUpdate(pStateChanges);
+    }
+
+    /**
+     * Call the tick hook.
+     * This is called by the environment.
+     *
+     * @internal
+     */
+    public executeTick(): void {
+        this.onTick();
+    }
+
+    /**
+     * Called when the system is created and registered to the environment.
+     */
+    protected abstract onCreate(): void;
+
+    /**
+     * Called once per update cycle.
+     * Used for resource managements based on component state changes.
+     */
+    protected abstract onUpdate(pStateChanges: Array<EnvironmentStateChange>): void;
+
+    /**
+     * Called once per frame.
+     */
+    protected abstract onFrame(): void;
+
+    /**
+     * Called once per tick (physics tick).
+     */
+    protected abstract onTick(): void;
+
+    /**
+     * Get a dependency system by its type.
+     * The requested system must be registered as a dependency of this system, otherwise an exception is thrown.
+     * 
+     * @param pSystemType - Type of the system marked as dependency.
+     * 
+     * @returns The instance of the requested system.
+     */
+    protected getDependency<T extends System>(pSystemType: SystemConstructor<T>): T {
+        // Check if the requested system is registered as a dependency of this system.
+        const lSystem = this.mDependencySystems.get(pSystemType);
+        if (!lSystem) {
+            throw new Exception(`System of type ${pSystemType.name} is not registered as a dependency.`, this);
+        }
+
+        return lSystem as T;
+    }
 }
+
+type SystemConstructor<T extends System> = IAnyParameterConstructor<T>;

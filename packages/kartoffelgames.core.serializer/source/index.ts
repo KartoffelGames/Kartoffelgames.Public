cadd39780672592742eac5b56a2f5ac83e91e2d9
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2026-02-16 11:00:44 +0100

    Remove complete serializer.

--- packages/kartoffelgames.core.serializer/source/index.ts
+++
@@ -1,6 +0,0 @@
-/* istanbul ignore file */
-
-export { StatefullSerializeable } from './statefull_serialize/decorator/statefull-serializeable.decorator.ts';
-export { StatefullDeserializer } from './statefull_serialize/statefull-deserializer.ts';
-export { StatefullSerializeableClasses } from './statefull_serialize/statefull-serializeable-classes.ts';
-export { StatefullSerializer } from './statefull_serialize/statefull-serializer.ts';
--- packages/kartoffelgames.core.serializer/source/type.ts
+++
@@ -1,2 +0,0 @@
-export type SerializeableConstructor = new (...pParameter: Array<any>) => any;
-export type SerializeableGuid = string;

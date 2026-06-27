d7436b61bd9a539243400af03a3ebd4ca450b0e6
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2026-06-27 03:49:30 +0200

    Initialize page files for core.

---
+++ packages/kartoffelgames.core/page/build/page.js
@@ -0,0 +1,2 @@
+(()=>{(()=>{let e=new WebSocket("ws://127.0.0.1:8088");e.addEventListener("open",()=>{console.log("Refresh connection established")}),e.addEventListener("message",o=>{console.log("Bundle finished. Start refresh"),o.data==="REFRESH"&&window.location.reload()})})();console.log("Hello World!!!");})();
+//# sourceMappingURL=page.js.map

33caf4700f74d8a21827314f8d3bc8ff9aa99895
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2026-09-03 21:29:06 +0200

    Move components into component dir.

--- packages/kartoffelgames.web.components/source/index.ts
+++ packages/kartoffelgames.web.components/source/index.ts
@@ -1,8 +1,8 @@
-export { KgButtonComponent, type KgButtonComponentType } from './kg-button/kg-button-component.ts';
-export { KgInformationComponent } from './kg-information/kg-information-component.ts';
-export { KgInputComponent } from './kg-input/kg-input-component.ts';
-export { KgListItemComponent } from './kg-list-item/kg-list-item-component.ts';
-export { KgPopupComponent } from './kg-popup/kg-popup-component.ts';
-export { KgResizeBoxComponent, KgResizeBoxComponentResize, KgResizeBoxComponentResizeDirection } from './kg-resize-box/kg-resize-box-component.ts';
-export { KgResizePanelComponent, KgResizePanelComponentResize, KgResizePanelComponentResizeDirection } from './kg-resize-panel/kg-resize-panel-component.ts';
-export { KgSelectComponent, type KgSelectComponentOption } from './kg-select/kg-select-component.ts';
+export { KgButtonComponent, type KgButtonComponentType } from './component/kg-button/kg-button-component.ts';
+export { KgInformationComponent } from './component/kg-information/kg-information-component.ts';
+export { KgInputComponent } from './component/kg-input/kg-input-component.ts';
+export { KgListItemComponent } from './component/kg-list-item/kg-list-item-component.ts';
+export { KgPopupComponent } from './component/kg-popup/kg-popup-component.ts';
+export { KgResizeBoxComponent, KgResizeBoxComponentResize, KgResizeBoxComponentResizeDirection } from './component/kg-resize-box/kg-resize-box-component.ts';
+export { KgResizePanelComponent, KgResizePanelComponentResize, KgResizePanelComponentResizeDirection } from './component/kg-resize-panel/kg-resize-panel-component.ts';
+export { KgSelectComponent, type KgSelectComponentOption } from './component/kg-select/kg-select-component.ts';
